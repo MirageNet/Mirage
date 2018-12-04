a880efbc2449ad1345668c5f321d9d52e7da6318
Paul Pacheco <paulpach@gmail.com>
2018-12-04 17:11:10 -0600

    Move telepathy inside mirror for better organization

---
+++ Telepathy/NetworkStreamExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+public static class NetworkStreamExtensions
+{
+    // helper function to read EXACTLY 'n' bytes
+    // -> default .Read reads up to 'n' bytes. this function reads exactly 'n'
+    //    bytes
+    // -> either return all the bytes requested or null if end of stream
+    public static async Task<byte[]> ReadExactlyAsync(this NetworkStream stream, int size)
+    {
+        byte[] data = new byte[size];
+
+        int offset = 0;
+
+        // keep reading until we fill up the buffer;
+        while (offset < size)
+        {
+            int received = await stream.ReadAsync(data, offset, size - offset);
+
+            // we just got disconnected
+            if (received == 0)
+            {
+                return null;
+            }
+
+            offset += received;
+        }
+
+        return data;
+    }
+
+}
