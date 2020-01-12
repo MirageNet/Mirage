946dfa4d165f171788169b084b88a76f66ba2a52
Paul Pacheco <paulpach@gmail.com>
2020-01-12 10:08:02 -0600

    Run git gc before filter-repo

--- publish_upm.sh
+++ publish_upm.sh
@@ -5,7 +5,7 @@ TAG=$2
 
 git subtree split --prefix=Assets/Mirror -b $BRANCH
 #git filter-branch --prune-empty --tree-filter 'rm -rf Tests' upm
-git repack
+git gc
 git filter-repo --invert-paths --path Tests --refs $BRANCH
 git tag $TAG $BRANCH
 git push -u origin $BRANCH --tags
