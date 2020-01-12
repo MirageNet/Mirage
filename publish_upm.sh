9f326ba926cb6a8d0a3efad26c33f05622d2a023
Paul Pacheco <paulpach@gmail.com>
2020-01-12 10:35:38 -0600

    What happened to origin?

--- publish_upm.sh
+++ publish_upm.sh
@@ -2,12 +2,26 @@
 
 BRANCH=$1
 TAG=$2
+echo "Showing remotes 1"
+git remote -v
 
 git subtree split --prefix=Assets/Mirror -b $BRANCH
+echo "Showing remotes 2"
+git remote -v
+
 #git filter-branch --prune-empty --tree-filter 'rm -rf Tests' upm
 git gc
+echo "Showing remotes 3"
+git remote -v
+
 git filter-repo --force --invert-paths --path Tests --refs $BRANCH
+
+echo "Showing remotes 4"
+git remote -v
+
 git filter-repo --force --path-rename "Examples:Samples~"
+
+echo "Showing remotes 5"
 git remote -v
 git tag $TAG $BRANCH
 git push origin $BRANCH --tags
