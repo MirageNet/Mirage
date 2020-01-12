9df1a2a0d449562d9a8123d1bc7167afda620399
Paul Pacheco <paulpach@gmail.com>
2020-01-12 10:29:26 -0600

    Show me my remotes

--- publish_upm.sh
+++ publish_upm.sh
@@ -8,5 +8,6 @@ git subtree split --prefix=Assets/Mirror -b $BRANCH
 git gc
 git filter-repo --force --invert-paths --path Tests --refs $BRANCH
 git filter-repo --force --path-rename "Examples:Samples~"
+git remote -v
 git tag $TAG $BRANCH
 git push origin $BRANCH --tags
