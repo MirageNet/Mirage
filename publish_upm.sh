1843a0cd817604b2bee6e6945831caa54afc1bda
Paul Pacheco <paulpach@gmail.com>
2020-01-12 10:21:52 -0600

    Move Examples to Samples~ for UPM

--- publish_upm.sh
+++ publish_upm.sh
@@ -7,5 +7,6 @@ git subtree split --prefix=Assets/Mirror -b $BRANCH
 #git filter-branch --prune-empty --tree-filter 'rm -rf Tests' upm
 git gc
 git filter-repo --force --invert-paths --path Tests --refs $BRANCH
+git filter-repo --force --path-rename "Examples:Samples~"
 git tag $TAG $BRANCH
 git push -u origin $BRANCH --tags
