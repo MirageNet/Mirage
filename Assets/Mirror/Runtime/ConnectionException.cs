cc9ee583fead802c0ddaf80f7d1895e3b594eb6d
Paul Pacheco <paulpach@gmail.com>
2020-03-15 11:18:26 -0500

    Add a connection exception

---
+++ Assets/Mirror/Runtime/ConnectionException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Mirror
+{
+    [Serializable]
+    public class ConnectionException : Exception
+    {
+        public readonly NetworkConnectionToClient Connection;
+
+        public ConnectionException()
+        {
+        }
+
+        public ConnectionException(string message) : base(message)
+        {
+        }
+
+        public ConnectionException(string message, NetworkConnectionToClient connection) : base(message)
+        {
+            this.Connection = connection;
+        }
+
+        public ConnectionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ConnectionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
