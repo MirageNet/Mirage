aefb075fb0bcc27424e8bb428f61e495729929bf
uwee <uweenukr@gmail.com>
2020-03-25 09:01:11 -0400

    remove commented code

--- Assets/Mirror/Runtime/NetworkManager.cs
+++ Assets/Mirror/Runtime/NetworkManager.cs
@@ -1259,11 +1259,6 @@ namespace Mirror
         public virtual void OnStartServer() { }
 
         /// <summary>
-        /// This is invoked when the client is started.
-        /// </summary>
-        //public virtual void OnStartClient() { }
-
-        /// <summary>
         /// This is called when a server is stopped - including when a host is stopped.
         /// </summary>
         public virtual void OnStopServer() { }
