3b0d0e512b773ab7d0affe69521f224322640b9d
Paul Pacheco <paulpach@gmail.com>
2020-03-30 14:05:32 -0500

    Use PipedConnection instead of ULocalConnections

--- Assets/Mirror/Runtime/NetworkServer.cs
+++ Assets/Mirror/Runtime/NetworkServer.cs
@@ -257,7 +257,6 @@ namespace Mirror
             NetworkConnectionToClient conn = new NetworkConnectionToClient(tconn);
             localConnection = conn;
             localClient = client;
-            AddConnection(conn);
 
             _ = ConnectionAcceptedAsync(conn);
 
