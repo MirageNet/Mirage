891225dab917dfa61d0b2533d688d585eeab295e
Paul Pacheco <paulpach@gmail.com>
2020-03-31 22:39:03 -0500

    feat: async multiplex transport

---
+++ Assets/Mirror/Runtime/Transport/AsyncMultiplexTransport.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Mirror
+{
+    public class AsyncMultiplexTransport : AsyncTransport
+    {
+
+        public AsyncTransport[] transports;
+
+        private Dictionary<Task<IConnection>, AsyncTransport> Accepters;
+
+        public override async Task<IConnection> AcceptAsync()
+        {
+            if (Accepters == null)
+            {
+                Accepters = new Dictionary<Task<IConnection>, AsyncTransport>();
+
+                foreach (AsyncTransport transport in transports)
+                {
+                    Task<IConnection> transportAccepter = transport.AcceptAsync();
+                    Accepters[transportAccepter] = transport;
+                }
+            }
+
+            // that's it nobody is left to accept
+            if (Accepters.Count == 0)
+                return null;
+
+            // wait for any one of them to accept
+            var task = await Task.WhenAny(Accepters.Keys);
+
+            AsyncTransport acceptedTransport = Accepters[task];
+            Accepters.Remove(task);
+
+            IConnection value = await task;
+
+            if (value == null)
+            {
+                // this transport closed. Get the next one
+                return await AcceptAsync();
+            }
+            else
+            {
+                // transport may accept more connections
+                task = acceptedTransport.AcceptAsync();
+                Accepters[task] = acceptedTransport;
+
+                return value;
+            }
+        }
+
+        public override async Task<IConnection> ConnectAsync(Uri uri)
+        {
+            foreach (AsyncTransport transport in transports)
+            {
+                try
+                {
+                    return await transport.ConnectAsync(uri);
+                }
+                catch (ArgumentException)
+                {
+                    // try the next transport
+                }
+            }
+            throw new ArgumentException($"No transport was able to connect to {uri}");
+        }
+
+        public override void Disconnect()
+        {
+            foreach (AsyncTransport transport in transports)
+                transport.Disconnect();
+        }
+
+        public override Task ListenAsync()
+        {
+            IEnumerable<Task> tasks = from t in transports select t.ListenAsync();
+            return Task.WhenAll(tasks);
+        }
+
+        public override Uri ServerUri()
+        {
+            return transports[0].ServerUri();
+        }
+    }
+}
