e671f1fcff26e626646d7981e1ad820e733a39e8
Paul Pacheco <paulpach@gmail.com>
2020-04-13 20:02:48 -0500

    feat: Button to register all prefabs in NetworkClient

---
+++ Assets/Mirror/Editor/NetworkClientInspector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace Mirror
+{
+    [CustomEditor(typeof(NetworkClient), true)]
+    [CanEditMultipleObjects]
+    public class NetworkClientInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Register All Prefabs"))
+            {
+                RegisterPrefabs((NetworkClient)target);
+                Undo.RecordObject(target, "Changed Area Of Effect");
+            }
+        }
+
+        public void RegisterPrefabs(NetworkClient gameObject)
+        {
+            ISet<GameObject> prefabs = LoadPrefabsContaining<NetworkIdentity>(".");
+
+            foreach (var existing in gameObject.spawnPrefabs)
+            {
+                prefabs.Add(existing);
+            }
+            gameObject.spawnPrefabs.Clear();
+            gameObject.spawnPrefabs.AddRange(prefabs);
+        }
+
+        public static ISet<GameObject> LoadPrefabsContaining<T>(string path) where T : UnityEngine.Component
+        {
+            var result = new HashSet<GameObject>();
+
+            var guids = AssetDatabase.FindAssets("t:Object", new[] { "Assets" });
+
+            foreach (var guid in guids)
+            {
+                var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+
+                NetworkIdentity obj = AssetDatabase.LoadAssetAtPath<NetworkIdentity>(assetPath);
+
+                if (obj != null)
+                    result.Add(obj.gameObject);
+            }
+            return result;
+        }
+    }
+}
---
+++ Assets/Mirror/Tests/Editor/NetworkClientInspectorTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Mirror.Tests
+{
+    [TestFixture(Category = "NetworkClient")]
+    public class NetworkClientInspectorTest
+    {
+
+        [Test]
+        public void RegisterPrefabs()
+        {
+            var gameObject = new GameObject("NetworkClient", typeof(NetworkClient));
+
+            NetworkClient client = gameObject.GetComponent<NetworkClient>();
+
+            NetworkClientInspector inspector = ScriptableObject.CreateInstance<NetworkClientInspector>();
+
+            inspector.RegisterPrefabs(client);
+
+            Assert.That(client.spawnPrefabs, Has.Count.GreaterThan(13));
+
+            foreach (var prefab in client.spawnPrefabs)
+            {
+                Assert.That(prefab.GetComponent<NetworkIdentity>(), Is.Not.Null);
+            }
+        }        
+    }
+}
