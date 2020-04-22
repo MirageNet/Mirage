c67719e0708c900479529d0baf9090b482ae1daa
uwee <uweenukr@gmail.com>
2020-04-22 00:05:21 -0400

    add new example that uses NetMan.ServerChangeScene

---
+++ Assets/Mirror/Examples/ChangeScene/Scripts/SceneSwticherHUD.cs
@@ -0,0 +1,24 @@
+using Mirror;
+using UnityEngine;
+
+public class SceneSwticherHUD : MonoBehaviour
+{
+    public NetworkManager networkManager;
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 400, 215, 9999));
+
+        if (GUILayout.Button("Switch to Room1"))
+        {
+            networkManager.ServerChangeScene("Room1");
+        }
+
+        if (GUILayout.Button("Switch to Room2"))
+        {
+            networkManager.ServerChangeScene("Room2");
+        }
+
+        GUILayout.EndArea();
+    }
+}
