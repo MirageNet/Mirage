848d4fc16334840aa769cfaa01c95d0b93afa5e2
uwee <uweenukr@gmail.com>
2020-06-26 13:50:47 -0400

    removing comment as pausing no longer happens in NG

--- Assets/Mirror/Runtime/NetworkClient.cs
+++ Assets/Mirror/Runtime/NetworkClient.cs
@@ -434,10 +434,6 @@ namespace Mirror
 
             if (logger.LogEnabled()) logger.Log("ClientChangeScene newSceneName:" + newSceneName + " networkSceneName:" + networkSceneName);
 
-            // vis2k: pause message handling while loading scene. otherwise we will process messages and then lose all
-            // the state as soon as the load is finishing, causing all kinds of bugs because of missing state.
-            // (client may be null after StopClient etc.)
-            logger.Log("ClientChangeScene: pausing handlers while scene is loading to avoid data loss after scene was loaded.");
             // Let client prepare for scene change
             OnClientChangeScene(newSceneName, sceneOperation, customHandling);
 
