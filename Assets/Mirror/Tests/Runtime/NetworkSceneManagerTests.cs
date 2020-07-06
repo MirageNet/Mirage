903dc640e2c0f8befb9bf5ae32f78649a0341453
uwee <uweenukr@gmail.com>
2020-07-05 22:08:34 -0400

    add NetworkSceneManagerTest

---
+++ Assets/Mirror/Tests/Runtime/NetworkSceneManagerTests.cs
@@ -0,0 +1,63 @@
+using System;
+using NSubstitute.Core;
+using NUnit.Framework;
+using UnityEngine.SceneManagement;
+
+namespace Mirror.Tests
+{
+    public class NetworkSceneManagerTests : HostSetup<MockComponent>
+    {
+        [Test]
+        public void ChangeServerSceneExceptionTest()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                sceneManager.ChangeServerScene(string.Empty);
+            });
+        }
+
+        int onAuthInvokeCounter;
+        void TestOnAuthenticatedInvoke(INetworkConnection conn)
+        {
+            onAuthInvokeCounter++;
+        }
+
+        int onOnServerSceneChangedCounter;
+        void TestOnServerSceneChangedInvoke(string scene)
+        {
+            onOnServerSceneChangedCounter++;
+        }
+
+        int onOnClientSceneChangedCounter;
+        void TestOnClientSceneChangedInvoke(INetworkConnection conn)
+        {
+            onOnClientSceneChangedCounter++;
+        }
+
+        [Test]
+        public void FinishLoadSceneHostTest()
+        {
+            client.Authenticated.AddListener(TestOnAuthenticatedInvoke);
+            sceneManager.ServerSceneChanged.AddListener(TestOnServerSceneChangedInvoke);
+            sceneManager.ClientSceneChanged.AddListener(TestOnClientSceneChangedInvoke);
+
+            sceneManager.FinishLoadScene();
+
+            Assert.That(onAuthInvokeCounter, Is.EqualTo(1));
+            Assert.That(onOnServerSceneChangedCounter, Is.EqualTo(1));
+            Assert.That(onOnClientSceneChangedCounter, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FinishLoadServerOnlyTest()
+        {
+            client.Disconnect();
+
+            sceneManager.ServerSceneChanged.AddListener(TestOnServerSceneChangedInvoke);
+
+            sceneManager.FinishLoadScene();
+
+            Assert.That(onOnServerSceneChangedCounter, Is.EqualTo(1));
+        }
+    }
+}
