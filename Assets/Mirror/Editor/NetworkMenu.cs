3edab94d814e4bb66ff6464a80153ee34d640a46
Paul Pacheco <paul.pacheco@aa.com>
2020-07-11 08:53:35 -0500

    Add Network menu after UI

--- Assets/Mirror/Editor/NetworkMenu.cs
+++ Assets/Mirror/Editor/NetworkMenu.cs
@@ -8,7 +8,7 @@ namespace Mirror
     public static class NetworkMenu
     {
         // Start is called before the first frame update
-        [MenuItem("GameObject/Network/NetworkManager", priority = 6)]
+        [MenuItem("GameObject/Network/NetworkManager", priority = 7)]
         public static GameObject CreateNetworkManager()
         {
             var go = new GameObject("NetworkManager", typeof(AsyncTcpTransport), typeof(NetworkSceneManager), typeof(NetworkClient), typeof(NetworkServer), typeof(NetworkManager), typeof(PlayerSpawner), typeof(NetworkManagerHUD));
