202b837a96686608df4043e68869cc6f6b46ce18
Paul Pacheco <paul.pacheco@aa.com>
2020-07-13 18:14:43 -0500

    Fix code smell

--- Assets/Mirror/Runtime/CustomAttributes.cs
+++ Assets/Mirror/Runtime/CustomAttributes.cs
@@ -93,6 +93,12 @@ namespace Mirror
         public MethodInvocationException(string message) : base(message)
         {
         }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client.
+        protected MethodInvocationException(SerializationInfo info,StreamingContext context) : base(info, context)
+        {
+        }
     }
 
     /// <summary>
