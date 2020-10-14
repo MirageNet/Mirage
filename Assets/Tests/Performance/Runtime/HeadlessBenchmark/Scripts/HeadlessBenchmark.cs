610e0faa46f8229bd271a9df0cd6093920b7711e
uwee <uweenukr@gmail.com>
2020-10-14 16:44:05 -0400

    break up code

--- Assets/Tests/Performance/Runtime/HeadlessBenchmark/Scripts/HeadlessBenchmark.cs
+++ Assets/Tests/Performance/Runtime/HeadlessBenchmark/Scripts/HeadlessBenchmark.cs
@@ -14,6 +14,7 @@ namespace Mirror.HeadlessBenchmark
         public string editorArgs;
 
         string[] cachedArgs;
+        string port;
 
         void Start()
         {
@@ -23,93 +24,24 @@ namespace Mirror.HeadlessBenchmark
             cachedArgs = editorArgs.Split(' ');
 #endif
 
-            StartCoroutine(HeadlessStart());
+            HeadlessStart();
         }
 
-        IEnumerator HeadlessStart()
+        void HeadlessStart()
         {
             //Try to find port
-            string port = GetArgValue("-port");
+            port = GetArgValue("-port");
 
             //Try to find Transport
-            string transport = GetArgValue("-transport");
-            if (!string.IsNullOrEmpty(transport))
-            {
-                if (transport.Equals("tcp"))
-                {
-                    TcpTransport newTransport = networkManager.gameObject.AddComponent<TcpTransport>();
-
-                    //Try to apply port if exists and needed by transport.
-                    if (!string.IsNullOrEmpty(port))
-                    {
-                        newTransport.Port = int.Parse(port);
-                    }
-
-                    networkManager.server.transport = newTransport;
-                    networkManager.client.Transport = newTransport;
-                }
-                if (transport.Equals("kcp"))
-                {
-                    KcpTransport newTransport = networkManager.gameObject.AddComponent<KcpTransport>();
-
-                    //Try to apply port if exists and needed by transport.
-                if (!string.IsNullOrEmpty(port))
-                    {
-                        newTransport.Port = ushort.Parse(port);
-                    }
-                    networkManager.server.transport = newTransport;
-                    networkManager.client.Transport = newTransport;
-                }
-            }
+            ParseForTransport();
 
             //Server mode?
-            if (!string.IsNullOrEmpty(GetArg("-server")))
-            {
-                networkManager.server.Started.AddListener(OnServerStarted);
-                networkManager.server.Authenticated.AddListener(conn => networkManager.server.SetClientReady(conn));
-                _ = networkManager.server.ListenAsync();
-                Debug.Log("Starting Server Only Mode");
-            }
+            ParseForServerMode();
 
             //Or client mode?
-            string client = GetArg("-client");
-            if (!string.IsNullOrEmpty(client))
-            {
-                //string address;
-                //network address provided?
-                string address = GetArgValue("-address");
-                if (string.IsNullOrEmpty(address))
-                {
-                    address = "localhost";
-                }
+            StartCoroutine(ParseForClientMode());
 
-                //nested clients
-                int clonesCount = 1;
-                string clonesString = GetArgValue("-client");
-                if (!string.IsNullOrEmpty(clonesString))
-                {
-                    clonesCount = int.Parse(clonesString);
-                }
-
-                Debug.Log("Starting " + clonesCount + " Clients");
-
-                // connect from a bunch of clients
-                for (int i = 0; i < clonesCount; i++)
-                    yield return StartClient(i, networkManager.client.Transport, address);
-            }
-
-            if (!string.IsNullOrEmpty(GetArg("-help")))
-            {
-                Debug.Log("--==MirrorNG HeadlessClients Benchmark==--");
-                Debug.Log("Please start your standalone application with the -nographics and -batchmode options");
-                Debug.Log("Also provide these arguments to control the autostart process:");
-                Debug.Log("-server (will run in server only mode)");
-                Debug.Log("-client 1234 (will run the specified number of clients)");
-                Debug.Log("-transport tcp (transport to be used in test. add more by editing HeadlessBenchmark.cs)");
-                Debug.Log("-address example.com (will run the specified number of clients)");
-                Debug.Log("-port 1234 (port used by transport)");
-                Debug.Log("-monster 100 (number of monsters to spawn on the server)");
-            }
+            ParseForHelp();
         }
 
         void OnServerStarted()
@@ -142,6 +74,94 @@ namespace Mirror.HeadlessBenchmark
                 yield return null;
 
             client.Send(new AddPlayerMessage());
+        }
+
+        void ParseForServerMode()
+        {
+            if (!string.IsNullOrEmpty(GetArg("-server")))
+            {
+                networkManager.server.Started.AddListener(OnServerStarted);
+                networkManager.server.Authenticated.AddListener(conn => networkManager.server.SetClientReady(conn));
+                _ = networkManager.server.ListenAsync();
+                Debug.Log("Starting Server Only Mode");
+            }
+        }
+
+        IEnumerator ParseForClientMode()
+        {
+            string client = GetArg("-client");
+            if (!string.IsNullOrEmpty(client))
+            {
+                //network address provided?
+                string address = GetArgValue("-address");
+                if (string.IsNullOrEmpty(address))
+                {
+                    address = "localhost";
+                }
+
+                //nested clients
+                int clonesCount = 1;
+                string clonesString = GetArgValue("-client");
+                if (!string.IsNullOrEmpty(clonesString))
+                {
+                    clonesCount = int.Parse(clonesString);
+                }
+
+                Debug.Log("Starting " + clonesCount + " Clients");
+
+                // connect from a bunch of clients
+                for (int i = 0; i < clonesCount; i++)
+                    yield return StartClient(i, networkManager.client.Transport, address);
+            }
+        }
+
+        void ParseForHelp()
+        {
+            if (!string.IsNullOrEmpty(GetArg("-help")))
+            {
+                Debug.Log("--==MirrorNG HeadlessClients Benchmark==--");
+                Debug.Log("Please start your standalone application with the -nographics and -batchmode options");
+                Debug.Log("Also provide these arguments to control the autostart process:");
+                Debug.Log("-server (will run in server only mode)");
+                Debug.Log("-client 1234 (will run the specified number of clients)");
+                Debug.Log("-transport tcp (transport to be used in test. add more by editing HeadlessBenchmark.cs)");
+                Debug.Log("-address example.com (will run the specified number of clients)");
+                Debug.Log("-port 1234 (port used by transport)");
+                Debug.Log("-monster 100 (number of monsters to spawn on the server)");
+            }
+        }
+
+        void ParseForTransport()
+        {
+            string transport = GetArgValue("-transport");
+            if (!string.IsNullOrEmpty(transport))
+            {
+                if (transport.Equals("tcp"))
+                {
+                    TcpTransport newTransport = networkManager.gameObject.AddComponent<TcpTransport>();
+
+                    //Try to apply port if exists and needed by transport.
+                    if (!string.IsNullOrEmpty(port))
+                    {
+                        newTransport.Port = int.Parse(port);
+                    }
+
+                    networkManager.server.transport = newTransport;
+                    networkManager.client.Transport = newTransport;
+                }
+                if (transport.Equals("kcp"))
+                {
+                    KcpTransport newTransport = networkManager.gameObject.AddComponent<KcpTransport>();
+
+                    //Try to apply port if exists and needed by transport.
+                    if (!string.IsNullOrEmpty(port))
+                    {
+                        newTransport.Port = ushort.Parse(port);
+                    }
+                    networkManager.server.transport = newTransport;
+                    networkManager.client.Transport = newTransport;
+                }
+            }
         }
 
         string GetArgValue(string name)
