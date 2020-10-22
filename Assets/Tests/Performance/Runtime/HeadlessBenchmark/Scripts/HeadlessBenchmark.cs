fa6f966807a4a24d30717a2f79bafa348cebaa4a
Paul Pacheco <paul.pacheco@aa.com>
2020-10-22 13:51:59 -0500

    Use kcp:// url

--- Assets/Tests/Performance/Runtime/HeadlessBenchmark/Scripts/HeadlessBenchmark.cs
+++ Assets/Tests/Performance/Runtime/HeadlessBenchmark/Scripts/HeadlessBenchmark.cs
@@ -163,7 +163,7 @@ namespace Mirror.HeadlessBenchmark
                 Console.WriteLine("Also provide these arguments to control the autostart process:");
                 Console.WriteLine("-server (will run in server only mode)");
                 Console.WriteLine("-client 1234 (will run the specified number of clients)");
-                Console.WriteLine("-transport tcp (transport to be used in test. add more by editing HeadlessBenchmark.cs)");
+                Console.WriteLine("-transport kcp (transport to be used in test. add more by editing HeadlessBenchmark.cs)");
                 Console.WriteLine("-address example.com (will run the specified number of clients)");
                 Console.WriteLine("-port 1234 (port used by transport)");
                 Console.WriteLine("-monster 100 (number of monsters to spawn on the server)");
