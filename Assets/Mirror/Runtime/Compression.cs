25264a3cdea584385a030f77dad20fa047638206
Paul Pacheco <paul.pacheco@aa.com>
2020-11-28 11:19:54 -0600

    fix smells

--- Assets/Mirror/Runtime/Compression.cs
+++ Assets/Mirror/Runtime/Compression.cs
@@ -1,17 +1,25 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mirror
 {
     public static class Compression
     {
-        internal static uint Compress(Quaternion expected)
+        /// <summary>
+        /// Lossy compression of normalized quaternion into 29 bits
+        /// </summary>
+        /// <param name="quaternion">The quaternion to compress</param>
+        /// <returns>29 bits of compressed quaternion</returns>
+        internal static uint Compress(Quaternion quaternion)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Decompress quaternions
+        /// </summary>
+        /// <param name="compressed">29 bits contained a compressed quaternion</param>
+        /// <returns>The normalized quaternion</returns>
         internal static Quaternion Decompress(uint compressed)
         {
             throw new NotImplementedException();
