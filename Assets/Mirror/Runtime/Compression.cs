3234cea578ab40d1a7c3e4fa3cc90712731c2e23
Paul Pacheco <paul.pacheco@aa.com>
2020-11-28 10:28:40 -0600

    feat: Quaternion compression as described by
    https://gafferongames.com/post/snapshot_compression/

---
+++ Assets/Mirror/Runtime/Compression.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mirror
+{
+    public static class Compression
+    {
+        internal static uint Compress(Quaternion expected)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Quaternion Decompress(uint compressed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
