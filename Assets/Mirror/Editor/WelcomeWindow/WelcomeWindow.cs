4fac9d6be80ab31c5dc3c4fcd6a414a104925a5f
Paul Pacheco <paul.pacheco@aa.com>
2020-12-26 12:41:57 -0600

    Move presentation content to xml

--- Assets/Mirror/Editor/WelcomeWindow/WelcomeWindow.cs
+++ Assets/Mirror/Editor/WelcomeWindow/WelcomeWindow.cs
@@ -21,43 +21,6 @@ namespace Mirror
     {
         #region Setup
 
-        #region Page variables
-
-        //type of page
-        private enum EScreens { welcome, changelog, quickstart, bestpractices, faq, sponsor, discord }
-
-        //the current page
-        private EScreens currentScreen = EScreens.welcome;
-
-        //data type that we want to retrieve when we are using this enum
-        private enum EPageDataType { header, description, redirectButtonTitle, redirectButtonUrl }
-
-        //headers of the different pages
-        private const string welcomePageHeader = "Welcome";
-        private const string changelogHeader = "Change Log";
-        private const string quickStartHeader = "Quick Start Guide";
-        private const string bestPracticesHeader = "Best Practices";
-        private const string faqHeader = "FAQ";
-        private const string sponsorHeader = "Sponsor Us";
-
-        //descriptions of the different pages
-        private const string welcomePageDescription = "Hello! Thank you for installing MirrorNG. Please visit all the pages on this window. Clicking the button at the bottom of the pages will redirect you to a webpage. Additionally, there are example projects in the Mirror folder that you can look at. \n\nHave fun using Mirror!";
-        private const string changelogDescription = "The Change Log is a list of changes made to MirrorNG. Sometimes these changes can cause your game to break.";
-        private const string quickStartDescription = "The Quick Start Guide is meant for people who just started using MirrorNG. The Quick Start Guide will help new users learn how to accomplish important tasks. It is highly recommended that you complete the guide.";
-        private const string bestPracticesDescription = "This page describes the best practices that you should use during development. Currently a work in progress.";
-        private const string faqDescription = "The FAQ page holds commonly asked questions. Currently, the FAQ page contains answers to: \n\n   1. Syncing custom data types \n   2. How to connect \n   3. Host migration \n   4. Server lists and matchmaking";
-        private const string sponsorDescription = "Sponsoring will give you access to Mirror PRO which gives you special access to tools and priority support.";
-
-        //titles of the redirect buttons
-        private const string welcomePageButtonTitle = "Visit API Reference";
-        private const string changelogPageButtonTitle = "Visit Change Log";
-        private const string quickStartPageButtonTitle = "Visit Quick Start Guide";
-        private const string bestPracticesPageButtonTitle = "Visit Best Practices Page";
-        private const string faqPageButtonTitle = "Visit FAQ";
-        private const string sponsorPageButtonTitle = "Sponsor Us";
-
-        #endregion
-
         #region Urls
 
         private const string welcomePageUrl = "https://mirrorng.github.io/MirrorNG/";
@@ -174,7 +137,7 @@ namespace Mirror
             //Load the UI
             //Each editor window contains a root VisualElement object
             VisualElement root = rootVisualElement;
-            var uxml = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(MirrorPath + "/Editor/WelcomeWindow/WelcomeWindow.uxml");
+            VisualTreeAsset uxml = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(MirrorPath + "/Editor/WelcomeWindow/WelcomeWindow.uxml");
             var uss = AssetDatabase.LoadAssetAtPath<StyleSheet>(MirrorPath + "/Editor/WelcomeWindow/WelcomeWindow.uss");
 
             //Load the descriptions
@@ -182,115 +145,47 @@ namespace Mirror
             uxml.CloneTree(root);
             root.styleSheets.Add(uss);
 
-            //set the icon image
-            var icon = root.Q<Image>("Icon");
-            icon.image = MirrorIcon;
-
             //set the version text
             var versionText = root.Q<Label>("VersionText");
             versionText.text = "v" + GetVersion().Substring(6);
 
-            //get the header
-            var header = root.Q<Label>("Header");
-            //get the description
-            var description = root.Q<Label>("Description");
-            //get the redirect button
-            var redirectButton = root.Q<Button>("Redirect");
-
-            //init the content in the right column
-            header.text = GetPageData(EPageDataType.header).ToString();
-            description.text = GetPageData(EPageDataType.description).ToString();
-            redirectButton.text = GetPageData(EPageDataType.redirectButtonTitle).ToString();
-            redirectButton.clicked += () => { new ButtonClicked(GetPageData(EPageDataType.redirectButtonUrl).ToString()); };
-
             #region Page buttons
 
             var button0 = root.Q<Button>("WelcomeButton");
-            button0.clicked += () => PageButtonClicked(header, description, redirectButton, 0);
+            button0.clicked += () => ShowTab("Welcome");
 
             var button1 = root.Q<Button>("ChangeLogButton");
-            button1.clicked += () => PageButtonClicked(header, description, redirectButton, 1);
+            button1.clicked += () => ShowTab("ChangeLog");
 
             var button2 = root.Q<Button>("QuickStartButton");
-            button2.clicked += () => PageButtonClicked(header, description, redirectButton, 2);
+            button2.clicked += () => ShowTab("QuickStart");
 
             var button3 = root.Q<Button>("BestPracticesButton");
-            button3.clicked += () => PageButtonClicked(header, description, redirectButton, 3);
+            button3.clicked += () => ShowTab("BestPractices");
 
             var button4 = root.Q<Button>("FaqButton");
-            button4.clicked += () => PageButtonClicked(header, description, redirectButton, 4);
+            button4.clicked += () => ShowTab("Faq");
 
             var button5 = root.Q<Button>("SponsorButton");
-            button5.clicked += () => PageButtonClicked(header, description, redirectButton, 5);
+            button5.clicked += () => ShowTab("Sponsor");
 
             var button6 = root.Q<Button>("DiscordButton");
-            button6.clicked += () => PageButtonClicked(header, description, redirectButton, 6);
+            button6.clicked += () => Application.OpenURL(discordInviteUrl);
 
+            ShowTab("Welcome");
             #endregion
-
         }
 
-        private void PageButtonClicked(Label header, Label description, Button redirectButton, int newScreen)
+        private void ShowTab(string screen)
         {
-            EScreens screen = (EScreens)newScreen;
-
-            if (screen != EScreens.discord)
+            VisualElement rightColumn = rootVisualElement.Q<VisualElement>("RightColumnBox");
+            foreach (VisualElement tab in rightColumn.Children())
             {
-                currentScreen = screen;
-                header.text = GetPageData(EPageDataType.header).ToString();
-                description.text = GetPageData(EPageDataType.description).ToString();
-                redirectButton.text = GetPageData(EPageDataType.redirectButtonTitle).ToString();
-                redirectButton.clicked += () => { new ButtonClicked(GetPageData(EPageDataType.redirectButtonUrl).ToString()); };
+                tab.style.display = tab.name == screen ? DisplayStyle.Flex : DisplayStyle.None;
             }
-            else
-            {
-                Application.OpenURL(discordInviteUrl);
-            }
-        }
-
-        //get the page data based on the page and the type needed
-        private object GetPageData(EPageDataType type)
-        {
-            string[] returnTypes = new string[8];
-
-            //check the data type, set return types based on data type
-            if (type == EPageDataType.header)
-            {
-                returnTypes = new[] { welcomePageHeader, quickStartHeader, bestPracticesHeader, faqHeader, sponsorHeader, changelogHeader };
-            }
-            else if (type == EPageDataType.description)
-            {
-                returnTypes = new[] { welcomePageDescription, quickStartDescription, bestPracticesDescription, faqDescription, sponsorDescription, changelogDescription };
-            }
-            else if (type == EPageDataType.redirectButtonTitle)
-            {
-                returnTypes = new[] { welcomePageButtonTitle, quickStartPageButtonTitle, bestPracticesPageButtonTitle, faqPageButtonTitle, sponsorPageButtonTitle, changelogPageButtonTitle };
-            }
-            else if (type == EPageDataType.redirectButtonUrl)
-            {
-                returnTypes = new[] { welcomePageUrl, quickStartUrl, bestPracticesUrl, faqUrl, sponsorUrl, changelogUrl };
-            }
-
-            //return results based on the current page
-            if (currentScreen == EScreens.welcome) { return returnTypes[0]; }
-            else if (currentScreen == EScreens.quickstart) { return returnTypes[1]; }
-            else if (currentScreen == EScreens.bestpractices) { return returnTypes[2]; }
-            else if (currentScreen == EScreens.faq) { return returnTypes[3]; }
-            else if (currentScreen == EScreens.sponsor) { return returnTypes[4]; }
-            else if (currentScreen == EScreens.changelog) { return returnTypes[5]; }
-
-            return "You forgot to update GetPageData()";
         }
 
         #endregion
     }
 
-    //used to prevent mulitple tabs from opening. only opens one
-    public class ButtonClicked
-    {
-        public ButtonClicked(string url)
-        {
-            Application.OpenURL(url);
-        }
-    }
 }
