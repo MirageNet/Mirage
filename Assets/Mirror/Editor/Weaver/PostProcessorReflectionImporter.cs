12f3b500c8255f1772a520fe0fa0713c3501a9cc
Paul Pacheco <paul.pacheco@aa.com>
2021-01-08 19:47:13 -0600

    fix sq warning

--- Assets/Mirror/Editor/Weaver/PostProcessorReflectionImporter.cs
+++ Assets/Mirror/Editor/Weaver/PostProcessorReflectionImporter.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Reflection;
 using Mono.Cecil;
-using UnityEngine;
 
 namespace Mirror.Weaver
 {
@@ -18,9 +15,9 @@ namespace Mirror.Weaver
             _correctCorlib = module.AssemblyReferences.FirstOrDefault(a => a.Name == "mscorlib" || a.Name == "netstandard" || a.Name == SystemPrivateCoreLib);
         }
 
-        public override AssemblyNameReference ImportReference(AssemblyName reference)
+        public override AssemblyNameReference ImportReference(AssemblyName name)
         {
-            return _correctCorlib != null && reference.Name == SystemPrivateCoreLib ? _correctCorlib : base.ImportReference(reference);
+            return _correctCorlib != null && name.Name == SystemPrivateCoreLib ? _correctCorlib : base.ImportReference(name);
         }
     }
 }
