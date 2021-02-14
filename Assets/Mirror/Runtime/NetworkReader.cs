8b66a9a341e2e8ae1f804c777c7075decb236de8
Paul Pacheco <paul.pacheco@aa.com>
2021-02-14 16:15:58 -0600

    Document IObjectLocator

--- Assets/Mirror/Runtime/NetworkReader.cs
+++ Assets/Mirror/Runtime/NetworkReader.cs
@@ -26,8 +26,17 @@ namespace Mirror
         public static Func<NetworkReader, T> Read { internal get; set; }
     }
 
+    /// <summary>
+    /// An object that implements this interface can find objects by their net id
+    /// This is used by readers when trying to deserialize gameobjects
+    /// </summary>
     public interface IObjectLocator
     {
+        /// <summary>
+        /// Finds a network identity by id
+        /// </summary>
+        /// <param name="netId">the id of the object to find</param>
+        /// <returns>The NetworkIdentity matching the netid or null if none is found</returns>
         NetworkIdentity this[uint netId] { get; }
     }
 
