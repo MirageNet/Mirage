b37d154e3a20782de37435a62eb530c707d50485
Paul Pacheco <paul.pacheco@aa.com>
2021-02-17 15:08:15 -0600

    refactor: load and stores should be field references
    We load and store field references,  so use that.

---
+++ Assets/Mirage/Weaver/Processors/FieldReferenceComparator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Mono.Cecil;
+
+namespace Mirage.Weaver
+{
+    internal class FieldReferenceComparator : IEqualityComparer<FieldReference>
+    {
+        public bool Equals(FieldReference x, FieldReference y)
+        {
+            return x.FullName == y.FullName;
+        }
+
+        public int GetHashCode(FieldReference obj) => obj.FullName.GetHashCode();
+    }
+}
