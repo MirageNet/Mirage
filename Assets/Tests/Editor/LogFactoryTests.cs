8efe7cea0eb8536335f46bf412a8ba71be74ae96
James Frowen <jamesfrowen5@gmail.com>
2021-02-18 15:53:53 +0000

    fix: stopping `: ` being at the start of all log messages (#606)
    * fix: stopping `: ` being at the start of all log messages
    
    * tests for extension methods

--- Assets/Tests/Editor/LogFactoryTests.cs
+++ Assets/Tests/Editor/LogFactoryTests.cs
@@ -1,4 +1,4 @@
-﻿using NSubstitute;
+using NSubstitute;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -43,8 +43,59 @@ namespace Mirage
 
             ILogHandler mockHandler = Substitute.For<ILogHandler>();
             logger.logHandler = mockHandler;
-            logger.Log("This message be logged");
-            mockHandler.Received().LogFormat(LogType.Log, null, "{0}", "This message be logged");
+            const string msg = "This message be logged";
+            logger.Log(msg);
+            mockHandler.Received().LogFormat(LogType.Log, null, "{0}", msg);
+        }
+
+        [Test]
+        public void LogWarningIgnore()
+        {
+            ILogger logger = LogFactory.GetLogger<LogFactoryTests>();
+            logger.filterLogType = LogType.Error;
+
+            ILogHandler mockHandler = Substitute.For<ILogHandler>();
+            logger.logHandler = mockHandler;
+            logger.LogWarning("This message should not be logged");
+            mockHandler.DidNotReceiveWithAnyArgs().LogFormat(default, null, null);
+        }
+
+        [Test]
+        public void LogWarningFull()
+        {
+            ILogger logger = LogFactory.GetLogger<LogFactoryTests>();
+            logger.filterLogType = LogType.Warning;
+
+            ILogHandler mockHandler = Substitute.For<ILogHandler>();
+            logger.logHandler = mockHandler;
+            const string msg = "This message be logged";
+            logger.LogWarning(msg);
+            mockHandler.Received().LogFormat(LogType.Warning, null, "{0}", msg);
+        }
+
+        [Test]
+        public void LogErrorIgnore()
+        {
+            ILogger logger = LogFactory.GetLogger<LogFactoryTests>();
+            logger.filterLogType = LogType.Exception;
+
+            ILogHandler mockHandler = Substitute.For<ILogHandler>();
+            logger.logHandler = mockHandler;
+            logger.LogError("This message should not be logged");
+            mockHandler.DidNotReceiveWithAnyArgs().LogFormat(default, null, null);
+        }
+
+        [Test]
+        public void LogErrorFull()
+        {
+            ILogger logger = LogFactory.GetLogger<LogFactoryTests>();
+            logger.filterLogType = LogType.Error;
+
+            ILogHandler mockHandler = Substitute.For<ILogHandler>();
+            logger.logHandler = mockHandler;
+            const string msg = "This message be logged";
+            logger.LogError(msg);
+            mockHandler.Received().LogFormat(LogType.Error, null, "{0}", msg);
         }
     }
 }
