631adceee9ea3d9a4f1ff0f885bfcec4b8e21502
James Frowen <jamesfrowendev@gmail.com>
2021-03-03 18:00:10 +0000

    feat: adding class that will invoke late handlers
    Event that can only be invoked once. Any Handlers added after invoke will be invoked immediately

---
+++ Assets/Mirage/Runtime/RunOnceEvent.cs
@@ -0,0 +1,148 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Mirage
+{
+    public abstract class RunOnceEventBase
+    {
+        protected abstract UnityEventBase baseEvent { get; }
+        protected bool hasInvoked { get; private set; }
+
+        protected void MarkInvoked()
+        {
+            if (hasInvoked) throw new InvalidOperationException("Event can only be invoked once Invoke");
+
+            hasInvoked = true;
+        }
+
+        /// <summary>
+        /// Resets event, removing all listens and allowing it to be invoked again
+        /// </summary>
+        public void Reset()
+        {
+            hasInvoked = false;
+            // todo this will remove inspector events, do we want to do that?
+            baseEvent.RemoveAllListeners();
+        }
+    }
+
+    /// <summary>
+    /// Event that can only run once, adding handler late will invoke right away
+    /// </summary>
+    /// <remarks>
+    /// Interface only contains AddHandler method because Invoke should only be called from the owner of the event
+    /// </remarks>
+    public interface IRunOnceEvent
+    {
+        void AddHandler(UnityAction handler);
+    }
+
+    [Serializable]
+    public sealed class RunOnceEvent : RunOnceEventBase, IRunOnceEvent
+    {
+        [SerializeField] UnityEvent innerEvent;
+
+        protected override UnityEventBase baseEvent => innerEvent;
+
+        public void AddHandler(UnityAction handler)
+        {
+            if (hasInvoked)
+            {
+                handler.Invoke();
+            }
+            else
+            {
+                innerEvent.AddListener(handler);
+            }
+        }
+
+        public void Invoke()
+        {
+            MarkInvoked();
+
+            innerEvent.Invoke();
+        }
+    }
+
+    /// <summary>
+    /// Event that can only run once, adding handler late will invoke right away
+    /// <para>
+    /// See <see cref="IRunOnceEvent"/> for more details
+    /// </para>
+    /// </summary>
+    public interface IRunOnceEvent<T0>
+    {
+        void AddHandler(UnityAction<T0> handler);
+    }
+
+    [Serializable]
+    public abstract class RunOnceEvent<T0> : RunOnceEventBase, IRunOnceEvent<T0>
+    {
+        [SerializeField] UnityEvent<T0> innerEvent;
+        protected override UnityEventBase baseEvent => innerEvent;
+
+        T0 arg0;
+
+        public void AddHandler(UnityAction<T0> handler)
+        {
+            if (hasInvoked)
+            {
+                handler.Invoke(arg0);
+            }
+            else
+            {
+                innerEvent.AddListener(handler);
+            }
+        }
+
+        public void Invoke(T0 arg0)
+        {
+            MarkInvoked();
+
+            this.arg0 = arg0;
+            innerEvent.Invoke(arg0);
+        }
+    }
+
+    /// <summary>
+    /// Event that can only run once, adding handler late will invoke right away
+    /// <para>
+    /// See <see cref="IRunOnceEvent"/> for more details
+    /// </para>
+    /// </summary>
+    public interface IRunOnceEvent<T0, T1>
+    {
+        void AddHandler(UnityAction<T0, T1> handler);
+    }
+    [Serializable]
+    public abstract class RunOnceEvent<T0, T1> : RunOnceEventBase, IRunOnceEvent<T0, T1>
+    {
+        [SerializeField] UnityEvent<T0, T1> innerEvent;
+        protected override UnityEventBase baseEvent => innerEvent;
+
+        T0 arg0;
+        T1 arg1;
+
+        public void AddHandler(UnityAction<T0, T1> handler)
+        {
+            if (hasInvoked)
+            {
+                handler.Invoke(arg0, arg1);
+            }
+            else
+            {
+                innerEvent.AddListener(handler);
+            }
+        }
+
+        public void Invoke(T0 arg0, T1 arg1)
+        {
+            MarkInvoked();
+
+            this.arg0 = arg0;
+            this.arg1 = arg1;
+            innerEvent.Invoke(arg0, arg1);
+        }
+    }
+}
