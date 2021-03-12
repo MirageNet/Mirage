0a2f6bb790a77d8ca9b462a4343a9d0167d1e1c8
Paul Pacheco <paul.pacheco@aa.com>
2021-03-12 16:30:30 -0600

    test: check quaternion compression for corner cases

--- Assets/Tests/Editor/Compression.cs
+++ Assets/Tests/Editor/Compression.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using UnityEngine;
+using RangeAttribute = NUnit.Framework.RangeAttribute;
 
 namespace Mirage
 {
@@ -16,7 +17,7 @@ namespace Mirage
 
             Quaternion decompressed = Compression.Decompress(compressed);
 
-            // decompressed should be almost the same
+            // decompressed should be almost the same,  dot product of 2 quaternion is 1 if they are the same
             Assert.That(Mathf.Abs(Quaternion.Dot(expected, decompressed)), Is.GreaterThan(1 - 0.001));
         }
 
@@ -29,6 +30,24 @@ namespace Mirage
 
             Vector3 euler = decompressed.eulerAngles;
             Assert.That(euler.y, Is.EqualTo(90).Within(0.1));
+        }
+
+        [Test]
+        public void CompressCornerCases(
+            [Range(0, 360, 45)] int x,
+            [Range(0, 360, 45)] int y,
+            [Range(0, 360, 45)] int z
+            )
+        {
+            var expected = Quaternion.Euler(x, y, z);
+
+            uint compressed = Compression.Compress(Quaternion.Euler(x, y, z));
+
+            Quaternion decompressed = Compression.Decompress(compressed);
+
+            // decompressed should be almost the same,  dot product of 2 quaternion is 1 if they are the same
+            Assert.That(Mathf.Abs(Quaternion.Dot(expected, decompressed)), Is.GreaterThan(1 - 0.001));
+
         }
     }
 }
