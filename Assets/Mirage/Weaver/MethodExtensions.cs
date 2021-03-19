e97fd4e604b6d23d9159b17c5154a842c1074731
James Frowen <jamesfrowen5@gmail.com>
2021-03-19 00:42:36 +0000

    adding todos for documentation

--- Assets/Mirage/Weaver/MethodExtensions.cs
+++ Assets/Mirage/Weaver/MethodExtensions.cs
@@ -10,9 +10,11 @@ namespace Mirage.Weaver
     /// </summary>
     public static class MethodExtensions
     {
+        // todo add documentation 
         public static ParameterDefinition AddParam<T>(this MethodDefinition method, string name, ParameterAttributes attributes = ParameterAttributes.None)
             => AddParam(method, method.Module.ImportReference(typeof(T)), name, attributes);
 
+        // todo add documentation 
         public static ParameterDefinition AddParam(this MethodDefinition method, TypeReference typeRef, string name, ParameterAttributes attributes = ParameterAttributes.None)
         {
             var param = new ParameterDefinition(name, attributes, typeRef);
@@ -20,8 +22,10 @@ namespace Mirage.Weaver
             return param;
         }
 
+        // todo add documentation 
         public static VariableDefinition AddLocal<T>(this MethodDefinition method) => AddLocal(method, method.Module.ImportReference(typeof(T)));
 
+        // todo add documentation 
         public static VariableDefinition AddLocal(this MethodDefinition method, TypeReference type)
         {
             var local = new VariableDefinition(type);
@@ -29,18 +33,21 @@ namespace Mirage.Weaver
             return local;
         }
 
+        // todo add documentation 
         public static Instruction Create(this ILProcessor worker, OpCode code, Expression<Action> expression)
         {
             MethodReference typeref = worker.Body.Method.Module.ImportReference(expression);
             return worker.Create(code, typeref);
         }
 
+        // todo add documentation 
         public static Instruction Create<T>(this ILProcessor worker, OpCode code, Expression<Action<T>> expression)
         {
             MethodReference typeref = worker.Body.Method.Module.ImportReference(expression);
             return worker.Create(code, typeref);
         }
 
+        // todo add documentation 
         public static Instruction Create<T, TR>(this ILProcessor worker, OpCode code, Expression<Func<T, TR>> expression)
         {
             MethodReference typeref = worker.Body.Method.Module.ImportReference(expression);
