a7f4a0bd35ad890db1083c227a08d9823de9e674
Paul Pacheco <paulpach@gmail.com>
2021-03-19 11:45:11 -0500

    fix: NetId are now ok to wrap
    A long-running server may end up wrapping the net id.

--- Assets/Mirage/Runtime/ServerObjectManager.cs
+++ Assets/Mirage/Runtime/ServerObjectManager.cs
@@ -45,7 +45,12 @@ namespace Mirage
         public SpawnEvent UnSpawned => _unSpawned;
 
         uint nextNetworkId = 1;
-        uint GetNextNetworkId() => nextNetworkId++;
+        uint GetNextNetworkId() 
+        {
+            while (SpawnedObjects.HasKey(nextNetworkId))
+                nextNetworkId++;
+            return nextNetworkId++;
+        }
 
         public readonly Dictionary<uint, NetworkIdentity> SpawnedObjects = new Dictionary<uint, NetworkIdentity>();
 
