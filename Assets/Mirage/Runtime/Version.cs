cdde46ace02664af3a0828d77fed0f0ad61754e9
James Frowen <jamesfrowendev@gmail.com>
2021-03-23 15:08:31 +0000

    moving version to its own file

---
+++ Assets/Mirage/Runtime/Version.cs
@@ -0,0 +1,8 @@
+﻿namespace Mirage
+{
+    public static class Version
+    {
+        public static readonly string Current = typeof(NetworkIdentity).Assembly.GetName().Version.ToString();
+    }
+
+}
