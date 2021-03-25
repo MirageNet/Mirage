0f4cdc50910e3ec63a3b93c12633b026039cd696
Paul Pacheco <paulpach@gmail.com>
2021-03-25 14:14:36 -0500

    feat: sample for interest management (#727)
    Simple example for interest management.
    
    2 types of objects, ones that don't move (red ones), and ones that move (blue ones),  a client should only see objects near it's tank
    
    <img width="930" alt="Screen Shot 2021-03-25 at 1 12 18 PM" src="https://user-images.githubusercontent.com/466007/112522662-e650bc80-8d6b-11eb-8001-f917e30c111b.png">
    
    Performance of rebuild observers:
    <img width="884" alt="Screen Shot 2021-03-25 at 1 13 05 PM" src="https://user-images.githubusercontent.com/466007/112522871-20ba5980-8d6c-11eb-8f96-8a2771d95ffa.png">

--- Assets/Mirage/Components/NetworkProximityChecker.cs
+++ Assets/Mirage/Components/NetworkProximityChecker.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Mirage.Logging;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 namespace Mirage
 {
---
+++ Assets/Mirage/Samples~/InterestManagement/Scripts/Wander.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Mirage.Examples.InterestManagement
+{
+    public class Wander : MonoBehaviour
+    {
+        public NavMeshAgent agent;
+
+        public Bounds bounds;
+
+        // Start is called before the first frame update
+        public void StartMoving()
+        {
+            StartCoroutine(Move());
+        }
+
+        public IEnumerator Move()
+        {
+            while (true)
+            {
+
+                var position = new Vector3(
+                    (Random.value - 0.5f) * bounds.size.x + bounds.center.x,
+                    (Random.value - 0.5f) * bounds.size.y + bounds.center.y,
+                    (Random.value - 0.5f) * bounds.size.z + bounds.center.z
+                );
+
+                agent.destination = position;
+
+                yield return new WaitForSeconds(Random.Range(1.0f, 5.0f));
+            }
+        }
+    }
+}
