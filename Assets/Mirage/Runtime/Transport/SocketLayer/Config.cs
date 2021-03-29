c6b9b99c4dd73b3824b24cedf94c29a7cb9ddbba
James Frowen <jamesfrowendev@gmail.com>
2021-03-29 22:56:51 +0100

    adding comments to config

--- Assets/Mirage/Runtime/Transport/SocketLayer/Config.cs
+++ Assets/Mirage/Runtime/Transport/SocketLayer/Config.cs
@@ -3,16 +3,40 @@ namespace Mirage.SocketLayer
     public struct Config
     {
         // server
-
+        /// <summary>
+        /// Max concurrent connections server will accept
+        /// </summary>
         public int MaxConnections;
 
 
-        // connections
-
+        // client
+        /// <summary>
+        /// How often connect attempt message will be re-sent if server does not reply
+        /// </summary>
         public float ConnectAttemptInterval;
+
+        /// <summary>
+        /// How many times attempt to connect before giving up
+        /// </summary>
         public int MaxConnectAttempts;
 
+        /// <summary>
+        /// how long after previous send before sending keep alive message
+        /// <para>Keep alive is to stop connection from timing out</para>
+        /// <para>keep alive is sent over unreliable so this interval should be low enough so that <see cref="TimeoutDuration"/> does not timeout if some unreliable packets are missed </para>
+        /// </summary>
         public float KeepAliveInterval;
-        public float DisconnectTimeout;
+
+        /// <summary>
+        /// how long without a message before disconnecting connection
+        /// </summary>
+        public float TimeoutDuration;
+
+
+        // shared
+        /// <summary>
+        /// How long after disconnect before connection is fully removed from Peer
+        /// </summary>
+        public float DisconnectDuration;
     }
 }
