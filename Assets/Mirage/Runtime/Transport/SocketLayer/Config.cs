c7b3657c6eaa2b4c504d75be86c2ed50ff84d8bb
James Frowen <jamesfrowendev@gmail.com>
2021-03-29 23:04:35 +0100

    making config a class and setting default value

--- Assets/Mirage/Runtime/Transport/SocketLayer/Config.cs
+++ Assets/Mirage/Runtime/Transport/SocketLayer/Config.cs
@@ -1,42 +1,44 @@
 namespace Mirage.SocketLayer
 {
-    public struct Config
+    public class Config
     {
-        // server
+        #region server 
         /// <summary>
         /// Max concurrent connections server will accept
         /// </summary>
-        public int MaxConnections;
+        public int MaxConnections = 100;
+        #endregion
 
-
-        // client
+        #region client
         /// <summary>
         /// How often connect attempt message will be re-sent if server does not reply
         /// </summary>
-        public float ConnectAttemptInterval;
+        public float ConnectAttemptInterval = 0.25f;
 
         /// <summary>
         /// How many times attempt to connect before giving up
         /// </summary>
-        public int MaxConnectAttempts;
+        public int MaxConnectAttempts = 10;
 
         /// <summary>
         /// how long after previous send before sending keep alive message
         /// <para>Keep alive is to stop connection from timing out</para>
         /// <para>keep alive is sent over unreliable so this interval should be low enough so that <see cref="TimeoutDuration"/> does not timeout if some unreliable packets are missed </para>
         /// </summary>
-        public float KeepAliveInterval;
+        public float KeepAliveInterval = 1;
 
         /// <summary>
         /// how long without a message before disconnecting connection
         /// </summary>
-        public float TimeoutDuration;
+        public float TimeoutDuration = 5;
+        #endregion
 
 
-        // shared
+        #region shared
         /// <summary>
         /// How long after disconnect before connection is fully removed from Peer
         /// </summary>
-        public float DisconnectDuration;
+        public float DisconnectDuration = 2;
+        #endregion
     }
 }
