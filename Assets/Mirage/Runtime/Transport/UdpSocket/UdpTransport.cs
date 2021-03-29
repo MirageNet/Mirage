32a5c23b487d6612cdc490df21670a9d6fdf294f
James Frowen <jamesfrowendev@gmail.com>
2021-03-29 22:14:22 +0100

    adding udp socket implementation

---
+++ Assets/Mirage/Runtime/Transport/UdpSocket/UdpTransport.cs
@@ -0,0 +1,85 @@
+using System.Net;
+using System.Net.Sockets;
+using Mirage.SocketLayer;
+using UnityEngine;
+
+namespace Mirage
+{
+    public sealed class UdpTransport : TransportV2
+    {
+        [SerializeField] int port;
+
+        public override ISocket CreateClientSocket()
+        {
+            return new UDPSocket();
+        }
+
+        public override ISocket CreateServerSocket()
+        {
+            return new UDPSocket();
+        }
+
+        public override EndPoint GetBindEndPoint()
+        {
+            return new IPEndPoint(IPAddress.Any, port);
+        }
+        public override EndPoint GetConnectEndPoint(string address)
+        {
+            return new IPEndPoint(IPAddress.Parse(address), port);
+        }
+
+        public override bool ClientSupported => platformNotWebgl;
+        public override bool ServerSupported => platformNotWebgl;
+        private static bool platformNotWebgl => Application.platform != RuntimePlatform.WebGLPlayer;
+    }
+
+    public class UDPSocket : ISocket
+    {
+        readonly Socket socket;
+
+        public UDPSocket()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        }
+
+        public void Bind(EndPoint endPoint)
+        {
+            socket.Bind(endPoint);
+        }
+
+        public void Connect(EndPoint endPoint)
+        {
+            socket.Connect(endPoint);
+        }
+
+        public void Close()
+        {
+            socket.Close();
+            socket.Dispose();
+        }
+
+        /// <summary>
+        /// Is message avaliable
+        /// </summary>
+        /// <returns>true if data to read</returns>
+        public bool Poll()
+        {
+            return socket.Poll(0, SelectMode.SelectRead);
+        }
+
+        public void Recieve(byte[] buffer, ref EndPoint endPoint, out int bytesReceived)
+        {
+            // todo do we need to set if null
+            endPoint = endPoint ?? new IPEndPoint(IPAddress.Any, 0);
+            bytesReceived = socket.ReceiveFrom(buffer, ref endPoint);
+        }
+
+        public void Send(EndPoint endPoint, byte[] data, int? length = null)
+        {
+            int size = length ?? data.Length;
+            // todo check disconnected
+            // todo what SocketFlags??
+            socket.SendTo(data, size, SocketFlags.None, (IPEndPoint)endPoint);
+        }
+    }
+}
