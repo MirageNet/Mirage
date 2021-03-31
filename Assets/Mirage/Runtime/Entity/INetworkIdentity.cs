2f3fe99570f73c80f880d4922b4abc01ffb8d24b
dragonslaya <steve.s@bigredplanetgames.com>
2021-03-30 22:49:24 -0400

    start design of breaking free from unity usage for our class driven. This will allow us to make component specific stuff for different engines.
    Signed-off-by: dragonslaya <steve.s@bigredplanetgames.com>

---
+++ Assets/Mirage/Runtime/Entity/INetworkIdentity.cs
@@ -0,0 +1,10 @@
+namespace Mirage.Entity
+{
+    public interface INetworkIdentity
+    {
+        /// <summary>
+        ///     Network identity id.
+        /// </summary>
+        public uint NetId { get; }
+    }
+}
---
+++ Assets/Mirage/Runtime/Entity/NetworkBehaviourComponent.cs
@@ -0,0 +1,9 @@
+using Mirage;
+using Mirage.Components;
+
+namespace Assets.Mirage.Runtime.Entity
+{
+    public class NetworkBehaviourComponent : MirageBaseComponent<NetworkBehaviour>
+    {
+    }
+}
