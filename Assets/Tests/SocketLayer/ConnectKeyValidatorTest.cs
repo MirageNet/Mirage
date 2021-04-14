88762390dbb207d24e736f6fc751272329bc382e
James Frowen <jamesfrowendev@gmail.com>
2021-04-14 17:32:14 +0100

    planning tests

---
+++ Assets/Tests/SocketLayer/ConnectKeyValidatorTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Mirage.SocketLayer.Tests
+{
+    [Category("SocketLayer")]
+    public class ConnectKeyValidatorTest
+    {
+        [Test] public void KeyIsSameEachCall() { Assert.Ignore("not implemented"); }
+        [Test] public void ValidateReturnsTrueIfKeyIsCorrect() { Assert.Ignore("not implemented"); }
+        [Test] public void ValidateReturnsFalseIfKeyIsCorrect() { Assert.Ignore("not implemented"); }
+        [Test] public void CopySetsKeyIn2ndElementOfBuffer() { Assert.Ignore("not implemented"); }
+    }
+}
