55ecbb6e0cc73e0e30875f3f7a3344f403a68013
James Frowen <jamesfrowendev@gmail.com>
2021-04-20 16:33:40 +0100

    moving Channel to its own file

--- Assets/Mirage/Runtime/Transport/IConnection.cs
+++ Assets/Mirage/Runtime/Transport/IConnection.cs
@@ -6,15 +6,6 @@ using Cysharp.Threading.Tasks;
 namespace Mirage
 {
 
-    public static class Channel
-    {
-        // 2 well known channels
-        // transports can implement other channels
-        // to expose their features
-        public const int Reliable = 0;
-        public const int Unreliable = 1;
-    }
-
     public interface IConnection
     {
         void Send(ArraySegment<byte> data, int channel = Channel.Reliable);
