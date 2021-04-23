8177f387225ea7841cd9ab4daac38bcd25d78900
uwee <uweenukr@gmail.com>
2021-04-23 08:45:01 -0400

    refactor: remove internal method as replace is no longer virtual (#785)

--- Assets/Tests/Runtime/ClientServer/ServerObjectManagerTests.cs
+++ Assets/Tests/Runtime/ClientServer/ServerObjectManagerTests.cs
@@ -257,11 +257,11 @@ namespace Mirage.Tests.Runtime.ClientServer
         }
 
         [Test]
-        public void InternalReplacePlayerNoIdentityExceptionTest()
+        public void ReplacePlayerNoIdentityExceptionTest()
         {
             Assert.Throws<ArgumentException>(() =>
             {
-                serverObjectManager.InternalReplacePlayerForConnection(connectionToClient, new GameObject(), true);
+                serverObjectManager.ReplaceCharacter(connectionToClient, new GameObject(), true);
             });
         }
 
