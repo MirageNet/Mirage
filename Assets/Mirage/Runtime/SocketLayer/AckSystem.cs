4c6eaec2b3e1576b5706bffe02407c10d02b68ef
James Frowen <jamesfrowendev@gmail.com>
2021-05-23 17:39:17 +0100

    fixing metrics not being set on ack sysystem

--- Assets/Mirage/Runtime/SocketLayer/AckSystem.cs
+++ Assets/Mirage/Runtime/SocketLayer/AckSystem.cs
@@ -60,13 +60,14 @@ namespace Mirage.SocketLayer
         /// <param name="connection"></param>
         /// <param name="ackTimeout">how long after last send before sending empty ack</param>
         /// <param name="time"></param>
-        public AckSystem(IRawConnection connection, Config config, ITime time, BufferPool bufferPool, Metrics metrics = null)
+        public AckSystem(IRawConnection connection, Config config, ITime time, BufferPool bufferPool, Metrics metrics)
         {
             if (config == null) throw new ArgumentNullException(nameof(config));
 
             this.connection = connection;
             this.time = time;
             this.bufferPool = bufferPool;
+            this.metrics = metrics;
 
             ackTimeout = config.TimeBeforeEmptyAck;
             emptyAckLimit = config.EmptyAckLimit;
