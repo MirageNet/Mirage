8939f3a8334dbf1a1458d39126a00fe8ed7163ae
James Frowen <jamesfrowendev@gmail.com>
2021-05-23 17:06:28 +0100

    cleanign up FoldoutEventDrawer

--- Assets/Mirage/Editor/FoldoutEventDrawer.cs
+++ Assets/Mirage/Editor/FoldoutEventDrawer.cs
@@ -1,4 +1,3 @@
-using Mirage.Logging;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
@@ -8,31 +7,34 @@ namespace Mirage
     [CustomPropertyDrawer(typeof(FoldoutEventAttribute))]
     public class FoldoutEventDrawer : PropertyDrawer
     {
-        static readonly ILogger logger = LogFactory.GetLogger(typeof(FoldoutEventDrawer));
-
         UnityEventDrawer _unityEventDrawer;
-        UnityEventDrawer UnityEventDrawer
-        {
-            get
-            {
-                if (_unityEventDrawer == null)
-                {
-                    _unityEventDrawer = new UnityEventDrawer();
-                }
-                return _unityEventDrawer;
-            }
-        }
+        UnityEventDrawer UnityEventDrawer => _unityEventDrawer ?? (_unityEventDrawer = new UnityEventDrawer());
+
+        static GUIStyle Style => EditorStyles.label;
+        static float Margin => Style.margin.vertical;
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            return EditorStyles.label.CalcSize(label).y +
-                (property.isExpanded
-                    ? EditorStyles.label.margin.vertical * 2 + UnityEventDrawer.GetPropertyHeight(property, label)
-                    : EditorStyles.label.margin.vertical);
+            float labelHeight = GetLabelHeight(label);
+            if (property.isExpanded)
+            {
+                return labelHeight + Margin + UnityEventDrawer.GetPropertyHeight(property, label);
+            }
+            else
+            {
+                return labelHeight;
+            }
         }
+
+        private static float GetLabelHeight(GUIContent label)
+        {
+            // label + margin
+            return Style.CalcSize(label).y + Margin;
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            float labelHeight = EditorStyles.label.CalcSize(label).y + EditorStyles.label.margin.vertical * 2;
+            float labelHeight = GetLabelHeight(label) + Margin;
             var labelRec = new Rect(position)
             {
                 height = labelHeight
@@ -49,19 +51,6 @@ namespace Mirage
             {
                 UnityEventDrawer.OnGUI(eventRec, property, label);
             }
-        }
-
-        protected SceneAsset GetBuildSettingsSceneObject(string sceneName)
-        {
-            foreach (EditorBuildSettingsScene buildScene in EditorBuildSettings.scenes)
-            {
-                SceneAsset sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(buildScene.path);
-                if (sceneAsset.name == sceneName)
-                {
-                    return sceneAsset;
-                }
-            }
-            return null;
         }
     }
 }
