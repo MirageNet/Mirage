a4e8689cc15157ff83b045944e1f516937365134
James Frowen <jamesfrowen5@gmail.com>
2021-05-24 04:01:43 +0100

    fix: fixing udp socket exception on linux (#809)
    wrapping IOControl in try/catch, only setting it on windows

--- Assets/Mirage/Runtime/Sockets/Udp/UdpSocketFactory.cs
+++ Assets/Mirage/Runtime/Sockets/Udp/UdpSocketFactory.cs
@@ -72,10 +72,6 @@ namespace Mirage.Sockets.Udp
         Socket socket;
         IPEndPoint AnyEndpoint;
 
-        public UdpSocket()
-        {
-        }
-
         public void Bind(EndPoint endPoint)
         {
             AnyEndpoint = endPoint as IPEndPoint;
@@ -94,20 +90,40 @@ namespace Mirage.Sockets.Udp
             };
 
             socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-
-            // stops "SocketException: Connection reset by peer"
-            // this error seems to be caused by a failed send, resulting in the next polling being true, even those endpoint is closed
-            // see https://stackoverflow.com/a/15232187/8479976
-
-            // this IOControl sets the reporting of "unrealable" to false, stoping SocketException after a connection closes without sending disconnect message
-            const uint IOC_IN = 0x80000000;
-            const uint IOC_VENDOR = 0x18000000;
-            const uint SIO_UDP_CONNRESET = IOC_IN | IOC_VENDOR | 12;
-            byte[] _false = new byte[] { 0, 0, 0, 0 };
-
-            socket.IOControl(unchecked((int)SIO_UDP_CONNRESET), _false, null);
+            TrySetIOControl(socket);
 
             return socket;
+        }
+
+        private static void TrySetIOControl(Socket socket)
+        {
+            try
+            {
+                if (Application.platform != RuntimePlatform.WindowsPlayer && Application.platform != RuntimePlatform.WindowsEditor)
+                {
+                    // IOControl only seems to work on windows
+                    // gives "SocketException: The descriptor is not a socket" when running on github action on Linux
+                    // see https://github.com/mono/mono/blob/f74eed4b09790a0929889ad7fc2cf96c9b6e3757/mcs/class/System/System.Net.Sockets/Socket.cs#L2763-L2765
+                    return;
+                }
+
+                // stops "SocketException: Connection reset by peer"
+                // this error seems to be caused by a failed send, resulting in the next polling being true, even those endpoint is closed
+                // see https://stackoverflow.com/a/15232187/8479976
+
+                // this IOControl sets the reporting of "unrealable" to false, stoping SocketException after a connection closes without sending disconnect message
+                const uint IOC_IN = 0x80000000;
+                const uint IOC_VENDOR = 0x18000000;
+                const uint SIO_UDP_CONNRESET = IOC_IN | IOC_VENDOR | 12;
+                byte[] _false = new byte[] { 0, 0, 0, 0 };
+
+                socket.IOControl(unchecked((int)SIO_UDP_CONNRESET), _false, null);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Exception setting IOControl");
+                Debug.LogException(e);
+            }
         }
 
         public void Connect(EndPoint endPoint)
