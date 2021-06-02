51f95f3ca5e5fa75eb97b7240450fa578cc0df54
James Frowen <jamesfrowendev@gmail.com>
2021-06-02 02:43:28 +0100

    test: moving test class to its own file

---
+++ Assets/Tests/Runtime/Host/GameobjectExtensionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Mirage.Tests.Runtime.Host
+{
+    public class GameobjectExtensionTests : HostSetup<MockComponent>
+    {
+        [Test]
+        public void GetNetworkIdentity()
+        {
+            Assert.That(playerGO.GetNetworkIdentity(), Is.EqualTo(identity));
+        }
+
+        [Test]
+        public void GetNoNetworkIdentity()
+        {
+            // create a GameObject without NetworkIdentity
+            var goWithout = new GameObject();
+
+            // GetNetworkIdentity for GO without identity
+            // (error log is expected)
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                _ = goWithout.GetNetworkIdentity();
+            });
+
+            // clean up
+            Object.Destroy(goWithout);
+        }
+
+    }
+}
--- Assets/Tests/Runtime/Host/ServerObjectManagerTest.cs
+++ Assets/Tests/Runtime/Host/ServerObjectManagerTest.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using Cysharp.Threading.Tasks;
 using NSubstitute;
@@ -10,33 +9,6 @@ using Object = UnityEngine.Object;
 
 namespace Mirage.Tests.Runtime.Host
 {
-
-    public class GameobjectExtensionTests : HostSetup<MockComponent>
-    {
-        [Test]
-        public void GetNetworkIdentity()
-        {
-            Assert.That(playerGO.GetNetworkIdentity(), Is.EqualTo(identity));
-        }
-
-        [Test]
-        public void GetNoNetworkIdentity()
-        {
-            // create a GameObject without NetworkIdentity
-            var goWithout = new GameObject();
-
-            // GetNetworkIdentity for GO without identity
-            // (error log is expected)
-            Assert.Throws<InvalidOperationException>(() =>
-            {
-                _ = goWithout.GetNetworkIdentity();
-            });
-
-            // clean up
-            Object.Destroy(goWithout);
-        }
-
-    }
 
     [TestFixture]
     public class ServerObjectManagerHostTest : HostSetup<MockComponent>
