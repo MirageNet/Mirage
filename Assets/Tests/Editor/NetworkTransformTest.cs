7828e660657e33e01cdc898c95525ef400b848cc
James Frowen <jamesfrowendev@gmail.com>
2021-06-13 16:41:34 +0100

    fixing writer init

--- Assets/Tests/Editor/NetworkTransformTest.cs
+++ Assets/Tests/Editor/NetworkTransformTest.cs
@@ -7,16 +7,32 @@ namespace Mirage
     [TestFixture]
     public class NetworkTransformTest
     {
+        NetworkWriter writer = new NetworkWriter(1300);
+        NetworkReader reader = new NetworkReader();
+
+        [SetUp]
+        public void Setup()
+        {
+            writer.Reset();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            reader.Dispose();
+        }
+
         [Test]
         public void SerializeIntoWriterTest()
         {
-            var writer = new NetworkWriter();
             var position = new Vector3(1, 2, 3);
             var rotation = new Quaternion(0.1f, 0.2f, 0.3f, 0.4f);
             var scale = new Vector3(0.5f, 0.6f, 0.7f);
 
             NetworkTransformBase.SerializeIntoWriter(writer, position, rotation, scale);
-            var reader = new NetworkReader(writer.ToArray());
+
+            reader.Reset(writer.ToArraySegment());
+
             Assert.That(reader.ReadVector3(), Is.EqualTo(position));
             Assert.That(reader.ReadQuaternion(), Is.EqualTo(rotation));
             Assert.That(reader.ReadVector3(), Is.EqualTo(scale));
