5e2b81121ed55e7b87cb472e62c8932bf199f1d9
James Frowen <jamesfrowendev@gmail.com>
2021-06-13 19:11:20 +0100

    changing names of extension methods to help stop stackoverflow
     having different names makes it easier to spot when a function is calling itself

--- Assets/Mirage/Runtime/Serialization/SystemTypesExtensions.cs
+++ Assets/Mirage/Runtime/Serialization/SystemTypesExtensions.cs
@@ -48,21 +48,20 @@ namespace Mirage.Serialization
             public decimal decimalValue;
         }
 
-        public static void WriteByte(this NetworkWriter writer, byte value) => writer.WriteByte(value);
+        public static void WriteByteExtension(this NetworkWriter writer, byte value) => writer.WriteByte(value);
 
-        public static void WriteSByte(this NetworkWriter writer, sbyte value) => writer.WriteByte((byte)value);
+        public static void WriteSByteExtension(this NetworkWriter writer, sbyte value) => writer.WriteSByte(value);
 
         public static void WriteChar(this NetworkWriter writer, char value) => writer.WriteUInt16(value);
 
-        public static void WriteBoolean(this NetworkWriter writer, bool value) => writer.WriteByte((byte)(value ? 1 : 0));
+        public static void WriteBooleanExtension(this NetworkWriter writer, bool value) => writer.WriteBoolean(value);
 
-        public static void WriteUInt16(this NetworkWriter writer, ushort value)
+        public static void WriteUInt16Extension(this NetworkWriter writer, ushort value)
         {
-            writer.WriteByte((byte)value);
-            writer.WriteByte((byte)(value >> 8));
+            writer.WriteUInt16(value);
         }
 
-        public static void WriteInt16(this NetworkWriter writer, short value) => writer.WriteUInt16((ushort)value);
+        public static void WriteInt16Extension(this NetworkWriter writer, short value) => writer.WriteInt16(value);
 
         public static void WriteSingleConverter(this NetworkWriter writer, float value)
         {
@@ -80,7 +79,6 @@ namespace Mirage.Serialization
             };
             writer.WriteUInt64(converter.longValue);
         }
-
         public static void WriteDecimalConverter(this NetworkWriter writer, decimal value)
         {
             // the only way to read it without allocations is to both read and
@@ -113,18 +111,12 @@ namespace Mirage.Serialization
 
 
 
-        public static byte ReadByte(this NetworkReader reader) => reader.ReadByte();
-        public static sbyte ReadSByte(this NetworkReader reader) => (sbyte)reader.ReadByte();
+        public static byte ReadByteExtension(this NetworkReader reader) => reader.ReadByte();
+        public static sbyte ReadSByteExtension(this NetworkReader reader) => reader.ReadSByte();
         public static char ReadChar(this NetworkReader reader) => (char)reader.ReadUInt16();
-        public static bool ReadBoolean(this NetworkReader reader) => reader.ReadByte() != 0;
-        public static short ReadInt16(this NetworkReader reader) => (short)reader.ReadUInt16();
-        public static ushort ReadUInt16(this NetworkReader reader)
-        {
-            ushort value = 0;
-            value |= reader.ReadByte();
-            value |= (ushort)(reader.ReadByte() << 8);
-            return value;
-        }
+        public static bool ReadBooleanExtension(this NetworkReader reader) => reader.ReadBoolean();
+        public static short ReadInt16Extension(this NetworkReader reader) => reader.ReadInt16();
+        public static ushort ReadUInt16Extension(this NetworkReader reader) => reader.ReadUInt16();
         public static float ReadSingleConverter(this NetworkReader reader)
         {
             var converter = new UIntFloat
