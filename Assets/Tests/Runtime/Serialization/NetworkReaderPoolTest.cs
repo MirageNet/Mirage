55ca9bc9f051684be230e10782d89d9324f2f09a
James Frowen <jamesfrowendev@gmail.com>
2021-06-13 16:45:19 +0100

    removing old pool tests

--- Assets/Tests/Runtime/Serialization/NetworkReaderPoolTest.cs
+++ Assets/Tests/Runtime/Serialization/NetworkReaderPoolTest.cs
@@ -1,129 +1,125 @@
-using System;
-using System.Linq;
-using Mirage.Serialization;
-using NUnit.Framework;
-
 namespace Mirage.Tests.Runtime.Serialization
 {
-    public class NetworkReaderPoolTest
-    {
-        int defaultCapacity;
+    // todo add tests for Pool
+    //public class NetworkReaderPoolTest
+    //{
+    //    int defaultCapacity;
 
-        [SetUp]
-        public void SetUp()
-        {
-            defaultCapacity = NetworkReaderPool.Capacity;
-        }
+    //    [SetUp]
+    //    public void SetUp()
+    //    {
+    //        defaultCapacity = NetworkReaderPool.Capacity;
+    //    }
 
-        [TearDown]
-        public void TearDown()
-        {
-            NetworkReaderPool.Capacity = defaultCapacity;
-        }
+    //    [TearDown]
+    //    public void TearDown()
+    //    {
+    //        NetworkReaderPool.Capacity = defaultCapacity;
+    //    }
 
-        [Test]
-        public void TestPoolRecycling()
-        {
-            object firstReader;
+    //    [Test]
+    //    public void TestPoolRecycling()
+    //    {
+    //        object firstReader;
 
-            using (PooledNetworkReader Reader = NetworkReaderPool.GetReader(default(ArraySegment<byte>)))
-            {
-                firstReader = Reader;
-            }
+    //        using (PooledNetworkReader Reader = NetworkReaderPool.GetReader(default(ArraySegment<byte>)))
+    //        {
+    //            firstReader = Reader;
+    //        }
 
-            using (PooledNetworkReader Reader = NetworkReaderPool.GetReader(default(ArraySegment<byte>)))
-            {
-                Assert.That(Reader, Is.SameAs(firstReader), "Pool should reuse the Reader");
-            }
-        }
+    //        using (PooledNetworkReader Reader = NetworkReaderPool.GetReader(default(ArraySegment<byte>)))
+    //        {
+    //            Assert.That(Reader, Is.SameAs(firstReader), "Pool should reuse the Reader");
+    //        }
+    //    }
 
-        [Test]
-        public void PoolCanGetMoreReadersThanPoolSize()
-        {
-            NetworkReaderPool.Capacity = 5;
+    //    [Test]
+    //    public void PoolCanGetMoreReadersThanPoolSize()
+    //    {
+    //        NetworkReaderPool.Capacity = 5;
 
-            const int testReaderCount = 10;
-            var Readers = new PooledNetworkReader[testReaderCount];
+    //        const int testReaderCount = 10;
+    //        var Readers = new PooledNetworkReader[testReaderCount];
 
-            for (int i = 0; i < testReaderCount; i++)
-            {
-                Readers[i] = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
-            }
+    //        for (int i = 0; i < testReaderCount; i++)
+    //        {
+    //            Readers[i] = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        }
 
-            // Make sure all Readers are different
-            Assert.That(Readers.Distinct().Count(), Is.EqualTo(testReaderCount));
-        }
+    //        // Make sure all Readers are different
+    //        Assert.That(Readers.Distinct().Count(), Is.EqualTo(testReaderCount));
+    //    }
 
-        [Test]
-        public void PoolReUsesReadersUpToSizeLimit()
-        {
-            NetworkReaderPool.Capacity = 1;
+    //    [Test]
+    //    public void PoolReUsesReadersUpToSizeLimit()
+    //    {
+    //        NetworkReaderPool.Capacity = 1;
 
-            // get 2 Readers
-            PooledNetworkReader a = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
-            PooledNetworkReader b = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        // get 2 Readers
+    //        PooledNetworkReader a = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        PooledNetworkReader b = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
 
-            // recycle all
-            NetworkReaderPool.Recycle(a);
-            NetworkReaderPool.Recycle(b);
+    //        // recycle all
+    //        NetworkReaderPool.Recycle(a);
+    //        NetworkReaderPool.Recycle(b);
 
-            // get 2 new ones
-            PooledNetworkReader c = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
-            PooledNetworkReader d = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        // get 2 new ones
+    //        PooledNetworkReader c = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        PooledNetworkReader d = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
 
-            // exactly one should be reused, one should be new
-            bool cReused = c == a || c == b;
-            bool dReused = d == a || d == b;
-            Assert.That((cReused && !dReused) ||
-                        (!cReused && dReused));
-        }
+    //        // exactly one should be reused, one should be new
+    //        bool cReused = c == a || c == b;
+    //        bool dReused = d == a || d == b;
+    //        Assert.That((cReused && !dReused) ||
+    //                    (!cReused && dReused));
+    //    }
 
-        // if we shrink the capacity, the internal 'next' needs to be adjusted
-        // to the new capacity so we don't get a IndexOutOfRangeException
-        [Test]
-        public void ShrinkCapacity()
-        {
-            NetworkReaderPool.Capacity = 2;
+    //    // if we shrink the capacity, the internal 'next' needs to be adjusted
+    //    // to the new capacity so we don't get a IndexOutOfRangeException
+    //    [Test]
+    //    public void ShrinkCapacity()
+    //    {
+    //        NetworkReaderPool.Capacity = 2;
 
-            // get Reader and recycle so we have 2 in there, hence 'next' is at limit
-            PooledNetworkReader a = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
-            PooledNetworkReader b = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
-            NetworkReaderPool.Recycle(a);
-            NetworkReaderPool.Recycle(b);
+    //        // get Reader and recycle so we have 2 in there, hence 'next' is at limit
+    //        PooledNetworkReader a = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        PooledNetworkReader b = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        NetworkReaderPool.Recycle(a);
+    //        NetworkReaderPool.Recycle(b);
 
-            // shrink
-            NetworkReaderPool.Capacity = 1;
+    //        // shrink
+    //        NetworkReaderPool.Capacity = 1;
 
-            // get one. should return the only one which is still in there.
-            PooledNetworkReader c = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
-            Assert.That(c, !Is.Null);
-            Assert.That(c == a || c == b);
-        }
+    //        // get one. should return the only one which is still in there.
+    //        PooledNetworkReader c = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        Assert.That(c, !Is.Null);
+    //        Assert.That(c == a || c == b);
+    //    }
 
-        // if we grow the capacity, things should still work fine
-        [Test]
-        public void GrowCapacity()
-        {
-            NetworkReaderPool.Capacity = 1;
+    //    // if we grow the capacity, things should still work fine
+    //    [Test]
+    //    public void GrowCapacity()
+    //    {
+    //        NetworkReaderPool.Capacity = 1;
 
-            // create and recycle one
-            PooledNetworkReader a = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
-            NetworkReaderPool.Recycle(a);
+    //        // create and recycle one
+    //        PooledNetworkReader a = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        NetworkReaderPool.Recycle(a);
 
-            // grow capacity
-            NetworkReaderPool.Capacity = 2;
+    //        // grow capacity
+    //        NetworkReaderPool.Capacity = 2;
 
-            // get two
-            PooledNetworkReader b = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
-            PooledNetworkReader c = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
-            Assert.That(b, !Is.Null);
-            Assert.That(c, !Is.Null);
+    //        // get two
+    //        PooledNetworkReader b = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        PooledNetworkReader c = NetworkReaderPool.GetReader(default(ArraySegment<byte>));
+    //        Assert.That(b, !Is.Null);
+    //        Assert.That(c, !Is.Null);
 
-            // exactly one should be reused, one should be new
-            bool bReused = b == a;
-            bool cReused = c == a;
-            Assert.That((bReused && !cReused) ||
-                        (!bReused && cReused));
-        }
-    }
+    //        // exactly one should be reused, one should be new
+    //        bool bReused = b == a;
+    //        bool cReused = c == a;
+    //        Assert.That((bReused && !cReused) ||
+    //                    (!bReused && cReused));
+    //    }
+    //}
 }
--- Assets/Tests/Runtime/Serialization/NetworkWriterPoolTest.cs
+++ Assets/Tests/Runtime/Serialization/NetworkWriterPoolTest.cs
@@ -1,128 +1,125 @@
-using System.Linq;
-using Mirage.Serialization;
-using NUnit.Framework;
-
 namespace Mirage.Tests.Runtime.Serialization
 {
-    public class NetworkWriterPoolTest
-    {
-        int defaultCapacity;
+    // todo add tests for Pool
+    //public class NetworkWriterPoolTest
+    //{
+    //    int defaultCapacity;
 
-        [SetUp]
-        public void SetUp()
-        {
-            defaultCapacity = NetworkWriterPool.Capacity;
-        }
+    //    [SetUp]
+    //    public void SetUp()
+    //    {
+    //        defaultCapacity = NetworkWriterPool.Capacity;
+    //    }
 
-        [TearDown]
-        public void TearDown()
-        {
-            NetworkWriterPool.Capacity = defaultCapacity;
-        }
+    //    [TearDown]
+    //    public void TearDown()
+    //    {
+    //        NetworkWriterPool.Capacity = defaultCapacity;
+    //    }
 
-        [Test]
-        public void TestPoolRecycling()
-        {
-            object firstWriter;
+    //    [Test]
+    //    public void TestPoolRecycling()
+    //    {
+    //        object firstWriter;
 
-            using (PooledNetworkWriter writer = NetworkWriterPool.GetWriter())
-            {
-                firstWriter = writer;
-            }
+    //        using (PooledNetworkWriter writer = NetworkWriterPool.GetWriter())
+    //        {
+    //            firstWriter = writer;
+    //        }
 
-            using (PooledNetworkWriter writer = NetworkWriterPool.GetWriter())
-            {
-                Assert.That(writer, Is.SameAs(firstWriter), "Pool should reuse the writer");
-            }
-        }
+    //        using (PooledNetworkWriter writer = NetworkWriterPool.GetWriter())
+    //        {
+    //            Assert.That(writer, Is.SameAs(firstWriter), "Pool should reuse the writer");
+    //        }
+    //    }
 
-        [Test]
-        public void PoolCanGetMoreWritersThanPoolSize()
-        {
-            NetworkWriterPool.Capacity = 5;
+    //    [Test]
+    //    public void PoolCanGetMoreWritersThanPoolSize()
+    //    {
+    //        NetworkWriterPool.Capacity = 5;
 
-            const int testWriterCount = 10;
-            var writers = new PooledNetworkWriter[testWriterCount];
+    //        const int testWriterCount = 10;
+    //        var writers = new PooledNetworkWriter[testWriterCount];
 
-            for (int i = 0; i < testWriterCount; i++)
-            {
-                writers[i] = NetworkWriterPool.GetWriter();
-            }
+    //        for (int i = 0; i < testWriterCount; i++)
+    //        {
+    //            writers[i] = NetworkWriterPool.GetWriter();
+    //        }
 
-            // Make sure all writers are different
-            Assert.That(writers.Distinct().Count(), Is.EqualTo(testWriterCount));
-        }
+    //        // Make sure all writers are different
+    //        Assert.That(writers.Distinct().Count(), Is.EqualTo(testWriterCount));
+    //    }
 
-        [Test]
-        public void PoolReUsesWritersUpToSizeLimit()
-        {
-            NetworkWriterPool.Capacity = 1;
+    //    [Test]
+    //    public void PoolReUsesWritersUpToSizeLimit()
+    //    {
+    //        NetworkWriterPool.Capacity = 1;
 
-            // get 2 writers
-            PooledNetworkWriter a = NetworkWriterPool.GetWriter();
-            PooledNetworkWriter b = NetworkWriterPool.GetWriter();
+    //        // get 2 writers
+    //        PooledNetworkWriter a = NetworkWriterPool.GetWriter();
+    //        PooledNetworkWriter b = NetworkWriterPool.GetWriter();
 
-            // recycle all
-            NetworkWriterPool.Recycle(a);
-            NetworkWriterPool.Recycle(b);
+    //        // recycle all
+    //        NetworkWriterPool.Recycle(a);
+    //        NetworkWriterPool.Recycle(b);
 
-            // get 2 new ones
-            PooledNetworkWriter c = NetworkWriterPool.GetWriter();
-            PooledNetworkWriter d = NetworkWriterPool.GetWriter();
+    //        // get 2 new ones
+    //        PooledNetworkWriter c = NetworkWriterPool.GetWriter();
+    //        PooledNetworkWriter d = NetworkWriterPool.GetWriter();
 
-            // exactly one should be reused, one should be new
-            bool cReused = c == a || c == b;
-            bool dReused = d == a || d == b;
-            Assert.That((cReused && !dReused) ||
-                        (!cReused && dReused));
-        }
+    //        // exactly one should be reused, one should be new
+    //        bool cReused = c == a || c == b;
+    //        bool dReused = d == a || d == b;
+    //        Assert.That((cReused && !dReused) ||
+    //                    (!cReused && dReused));
+    //    }
 
-        // if we shrink the capacity, the internal 'next' needs to be adjusted
-        // to the new capacity so we don't get a IndexOutOfRangeException
-        [Test]
-        public void ShrinkCapacity()
-        {
-            NetworkWriterPool.Capacity = 2;
+    //    // if we shrink the capacity, the internal 'next' needs to be adjusted
+    //    // to the new capacity so we don't get a IndexOutOfRangeException
+    //    [Test]
+    //    public void ShrinkCapacity()
+    //    {
+    //        NetworkWriterPool.Capacity = 2;
 
-            // get writer and recycle so we have 2 in there, hence 'next' is at limit
-            PooledNetworkWriter a = NetworkWriterPool.GetWriter();
-            PooledNetworkWriter b = NetworkWriterPool.GetWriter();
-            NetworkWriterPool.Recycle(a);
-            NetworkWriterPool.Recycle(b);
+    //        // get writer and recycle so we have 2 in there, hence 'next' is at limit
+    //        PooledNetworkWriter a = NetworkWriterPool.GetWriter();
+    //        PooledNetworkWriter b = NetworkWriterPool.GetWriter();
+    //        NetworkWriterPool.Recycle(a);
+    //        NetworkWriterPool.Recycle(b);
 
-            // shrink
-            NetworkWriterPool.Capacity = 1;
+    //        // shrink
+    //        NetworkWriterPool.Capacity = 1;
 
-            // get one. should return the only one which is still in there.
-            PooledNetworkWriter c = NetworkWriterPool.GetWriter();
-            Assert.That(c, !Is.Null);
-            Assert.That(c == a || c == b);
-        }
+    //        // get one. should return the only one which is still in there.
+    //        PooledNetworkWriter c = NetworkWriterPool.GetWriter();
+    //        Assert.That(c, !Is.Null);
+    //        Assert.That(c == a || c == b);
+    //    }
 
-        // if we grow the capacity, things should still work fine
-        [Test]
-        public void GrowCapacity()
-        {
-            NetworkWriterPool.Capacity = 1;
+    //    // if we grow the capacity, things should still work fine
+    //    [Test]
+    //    public void GrowCapacity()
+    //    {
+    //        NetworkWriterPool.Capacity = 1;
 
-            // create and recycle one
-            PooledNetworkWriter a = NetworkWriterPool.GetWriter();
-            NetworkWriterPool.Recycle(a);
+    //        // create and recycle one
+    //        PooledNetworkWriter a = NetworkWriterPool.GetWriter();
+    //        NetworkWriterPool.Recycle(a);
 
-            // grow capacity
-            NetworkWriterPool.Capacity = 2;
+    //        // grow capacity
+    //        NetworkWriterPool.Capacity = 2;
 
-            // get two
-            PooledNetworkWriter b = NetworkWriterPool.GetWriter();
-            PooledNetworkWriter c = NetworkWriterPool.GetWriter();
-            Assert.That(b, !Is.Null);
-            Assert.That(c, !Is.Null);
+    //        // get two
+    //        PooledNetworkWriter b = NetworkWriterPool.GetWriter();
+    //        PooledNetworkWriter c = NetworkWriterPool.GetWriter();
+    //        Assert.That(b, !Is.Null);
+    //        Assert.That(c, !Is.Null);
 
-            // exactly one should be reused, one should be new
-            bool bReused = b == a;
-            bool cReused = c == a;
-            Assert.That((bReused && !cReused) ||
-                        (!bReused && cReused));
-        }
-    }
+    //        // exactly one should be reused, one should be new
+    //        bool bReused = b == a;
+    //        bool cReused = c == a;
+    //        Assert.That((bReused && !cReused) ||
+    //                    (!bReused && cReused));
+    //    }
+    //}
 }
