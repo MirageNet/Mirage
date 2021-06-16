876b5ca7a913b16953ddb7518ffa9ed2c4844bec
James Frowen <jamesfrowendev@gmail.com>
2021-06-16 15:24:53 +0100

    tests

---
+++ Assets/Tests/Runtime/Serialization/WeaverIgnoreTest.cs
@@ -0,0 +1,53 @@
+using System;
+using Mirage.Serialization;
+using NUnit.Framework;
+
+namespace Mirage.Tests.Runtime.Serialization
+{
+    public class WeaverIgnoreTest
+    {
+        [Test]
+        public void DoesNotUseCustomWriter()
+        {
+            // check method names
+            Assert.That(Writer<MyCustomType>.Write.Method.Name, Is.Not.EqualTo(new Action<NetworkWriter, MyCustomType>(MyCustomTypeExtension.WriteOnlyPartOfCustom).Method.Name));
+            Assert.That(Reader<MyCustomType>.Read.Method.Name, Is.Not.EqualTo(new Func<NetworkReader, MyCustomType>(MyCustomTypeExtension.ReadOnlyPartOfCustom).Method.Name));
+
+            // check writing and reading
+            var data = new MyCustomType
+            {
+                first = 10,
+                second = 20,
+            };
+            var writer = new NetworkWriter();
+            writer.Write(data);
+            var reader = new NetworkReader(writer.ToArraySegment());
+            MyCustomType copy = reader.Read<MyCustomType>();
+
+            // should have copied both fields,
+            // if it uses custom extension methods it will only write first
+            Assert.That(copy.first, Is.EqualTo(data.first));
+            Assert.That(copy.second, Is.EqualTo(data.second));
+        }
+
+    }
+    public struct MyCustomType
+    {
+        public int first;
+        public int second;
+    }
+    public static class MyCustomTypeExtension
+    {
+        [WeaverIgnore]
+        public static void WriteOnlyPartOfCustom(this NetworkWriter writer, MyCustomType value)
+        {
+            writer.WriteInt32(value.first);
+        }
+        [WeaverIgnore]
+
+        public static MyCustomType ReadOnlyPartOfCustom(this NetworkReader reader)
+        {
+            return new MyCustomType { first = reader.ReadInt32() };
+        }
+    }
+}
