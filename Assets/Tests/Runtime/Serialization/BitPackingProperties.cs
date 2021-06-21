77e5c1203ee8b58ab450b7f5540f1a7af9d3c2f3
James Frowen <jamesfrowendev@gmail.com>
2021-06-21 15:21:00 +0100

    tests for properties

---
+++ Assets/Tests/Runtime/Serialization/BitPackingProperties.cs
@@ -0,0 +1,110 @@
+﻿using Mirage.Serialization;
+using NUnit.Framework;
+
+namespace Mirage.Tests.Runtime.Serialization
+{
+    public class BitPackingProperties
+    {
+        private readonly NetworkWriter writer = new NetworkWriter(1300);
+        private readonly NetworkReader reader = new NetworkReader();
+
+        readonly byte[] sampleData = new byte[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        const int BITS_PER_BYTE = 8;
+
+        [TearDown]
+        public void TearDown()
+        {
+            writer.Reset();
+            reader.Dispose();
+        }
+
+
+        [Test]
+        public void WriterBitPositionStartsAtZero()
+        {
+            Assert.That(writer.BitPosition, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void WriterByteLengthStartsAtZero()
+        {
+            Assert.That(writer.ByteLength, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ReaderBitPositionStartsStartsAtZero()
+        {
+            reader.Reset(sampleData);
+            Assert.That(reader.BitPosition, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ReaderBytePositionStartsStartsAtZero()
+        {
+            reader.Reset(sampleData);
+            Assert.That(reader.BytePosition, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ReaderBitLengthStartsStartsAtArrayLength()
+        {
+            reader.Reset(sampleData);
+            Assert.That(reader.BitLength, Is.EqualTo(sampleData.Length * BITS_PER_BYTE));
+        }
+
+
+
+        [Test]
+        public void WriterBitPositionIncreasesAfterWriting()
+        {
+            writer.Write(0, 15);
+            Assert.That(writer.BitPosition, Is.EqualTo(15));
+
+            writer.Write(0, 50);
+            Assert.That(writer.BitPosition, Is.EqualTo(65));
+        }
+
+        [Test]
+        public void WriterByteLengthIncreasesAfterWriting_ShouldRoundUp()
+        {
+            writer.Write(0, 15);
+            Assert.That(writer.ByteLength, Is.EqualTo(2));
+
+            writer.Write(0, 50);
+            Assert.That(writer.ByteLength, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void ReaderBitPositionIncreasesAfterReading()
+        {
+            reader.Reset(sampleData);
+            _ = reader.Read(15);
+            Assert.That(reader.BitPosition, Is.EqualTo(15));
+
+            _ = reader.Read(50);
+            Assert.That(reader.BitPosition, Is.EqualTo(65));
+        }
+
+        [Test]
+        public void ReaderBytePositionIncreasesAfterReading_ShouldRoundUp()
+        {
+            reader.Reset(sampleData);
+            _ = reader.Read(15);
+            Assert.That(reader.BytePosition, Is.EqualTo(2));
+
+            _ = reader.Read(50);
+            Assert.That(reader.BytePosition, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void ReaderBitLengthDoesnotIncreasesAfterReading()
+        {
+            reader.Reset(sampleData);
+            _ = reader.Read(15);
+            Assert.That(reader.BitLength, Is.EqualTo(sampleData.Length * BITS_PER_BYTE));
+
+            _ = reader.Read(50);
+            Assert.That(reader.BitLength, Is.EqualTo(sampleData.Length * BITS_PER_BYTE));
+        }
+    }
+}
---
+++ Assets/Tests/Runtime/Serialization/BitPackingReusingWriterTests.cs
@@ -0,0 +1,93 @@
+﻿using Mirage.Serialization;
+using NUnit.Framework;
+
+namespace Mirage.Tests.Runtime.Serialization
+{
+    public class BitPackingReusingWriterTests
+    {
+        private NetworkWriter writer;
+        private NetworkReader reader;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            writer = new NetworkWriter(1300);
+            reader = new NetworkReader();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            writer.Reset();
+            reader.Dispose();
+        }
+
+
+        [Test]
+        public void WriteUShortAfterReset()
+        {
+            ushort value1 = 0b0101;
+            ushort value2 = 0x1000;
+
+            // write first value
+            writer.WriteUInt16(value1);
+
+            reader.Reset(writer.ToArray());
+            ushort out1 = reader.ReadUInt16();
+            Assert.That(out1, Is.EqualTo(value1));
+
+            // reset and write 2nd value
+            writer.Reset();
+
+            writer.WriteUInt16(value2);
+
+            reader.Reset(writer.ToArray());
+            ushort out2 = reader.ReadUInt16();
+            Assert.That(out2, Is.EqualTo(value2), "Value 2 was incorrect");
+        }
+
+        [Test]
+        [TestCase(0b0101ul, 0x1000ul)]
+        [TestCase(0xffff_0000_ffff_fffful, 0x0000_ffff_1111_0000ul)]
+        public void WriteULongAfterReset(ulong value1, ulong value2)
+        {
+            // write first value
+            writer.WriteUInt64(value1);
+
+            reader.Reset(writer.ToArray());
+            ulong out1 = reader.ReadUInt64();
+            Assert.That(out1, Is.EqualTo(value1));
+
+            // reset and write 2nd value
+            writer.Reset();
+
+            writer.WriteUInt64(value2);
+
+            reader.Reset(writer.ToArray());
+            ulong out2 = reader.ReadUInt64();
+            Assert.That(out2, Is.EqualTo(value2), "Value 2 was incorrect");
+        }
+
+        [Test]
+        [TestCase(0b0101ul, 0x1000ul)]
+        [TestCase(0xffff_0000_ffff_fffful, 0x0000_ffff_1111_0000ul)]
+        public void WriteULongWriteBitsAfterReset(ulong value1, ulong value2)
+        {
+            // write first value
+            writer.Write(value1, 64);
+
+            reader.Reset(writer.ToArray());
+            ulong out1 = reader.Read(64);
+            Assert.That(out1, Is.EqualTo(value1));
+
+            // reset and write 2nd value
+            writer.Reset();
+
+            writer.Write(value2, 64);
+
+            reader.Reset(writer.ToArray());
+            ulong out2 = reader.Read(64);
+            Assert.That(out2, Is.EqualTo(value2), "Value 2 was incorrect");
+        }
+    }
+}
