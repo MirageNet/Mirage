0df5c7f909babed76f01aee7626991fdb7f26be7
James Frowen <jamesfrowendev@gmail.com>
2021-08-16 14:05:16 +0100

    feat: adding helper classes for packing float, vector and quaternion (#847)
    * feat: quaternion packer
    
    * new packers
    
    * updating extensions
    
    * deleting old compresion class
    
    * adding vec2 packer
    
    * bit helper and tests
    
    * float packer constructor tests
    
    * float packer tests
    
    * fix: casting values to ulong so they arn't cut off if bit count is large
    
    * quaternion packing tests
    
    * reducing repeat count to 1000
    
    * allowing extra precision
    
    * tests for vector packers
    
    * fixing code smell
    
    * tests for pack extensions
    
    * moving var int to its own file
    
    * removing new function
    
    * test for unpack
    
    * fixing unpack test
    
    * really fixing test
    
    * removing un-used function

---
+++ Assets/Tests/Runtime/Serialization/Packers/BitHelperTest.cs
@@ -0,0 +1,21 @@
+﻿using Mirage.Serialization;
+using NUnit.Framework;
+
+namespace Mirage.Tests.Runtime.Serialization.Packers
+{
+    public class BitHelperTest
+    {
+        [Test]
+        [TestCase(2, 0.05f, ExpectedResult = 7)]
+        [TestCase(100, 0.1f, ExpectedResult = 11)]
+        [TestCase(.707f, 0.002f, ExpectedResult = 10)]
+        [TestCase(1000, 0.1f, ExpectedResult = 15)]
+        [TestCase(2000, 0.01f, ExpectedResult = 19)]
+        // 1023 is 10 bits, but max is -+ so 11 bits
+        [TestCase(1023, 1, ExpectedResult = 11)]
+        public int ReturnCorrectBitCount(float max, float precision)
+        {
+            return BitHelper.BitCount(max, precision);
+        }
+    }
+}
