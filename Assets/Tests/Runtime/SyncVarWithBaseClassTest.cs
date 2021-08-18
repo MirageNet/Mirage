4cfc0c3e55e6142d4d7595faa9caefe24cb658f8
James Frowen <jamesfrowendev@gmail.com>
2021-08-18 18:28:14 +0100

    test: test to make sure sync var count is carried over to child behaviours

---
+++ Assets/Tests/Runtime/SyncVarWithBaseClassTest.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Mirage.Tests.Runtime.SyncVarWithBaseClass
+{
+    public class A : NetworkBehaviour
+    {
+        [SyncVar] public int i0;
+        [SyncVar] public int i1;
+        [SyncVar] public int i2;
+        [SyncVar] public int i3;
+    }
+    public class B : A
+    {
+        [SyncVar] public int i4;
+        [SyncVar] public int i5;
+        [SyncVar] public int i6;
+    }
+    public class C : B
+    {
+        [SyncVar] public int i7;
+        [SyncVar] public int i8;
+        [SyncVar] public int i9;
+    }
+
+    public class SyncVarWithBaseClassTest
+    {
+        [Test]
+        public void SetsCorrectDirtyBit()
+        {
+            var go = new GameObject("SetsCorrectDirtyBit", typeof(NetworkIdentity), typeof(C));
+            C behaviour = go.GetComponent<C>();
+            Assert.That(behaviour.SyncVarDirtyBits, Is.Zero, "Should start at zero");
+
+            ulong expected = 0ul;
+
+            behaviour.i0 = 1;
+            SetDirtyBit(ref expected, 0);
+            Assert.That(behaviour.SyncVarDirtyBits, Is.EqualTo(expected));
+
+
+            behaviour.i3 = 1;
+            SetDirtyBit(ref expected, 3);
+            Assert.That(behaviour.SyncVarDirtyBits, Is.EqualTo(expected));
+
+            behaviour.i6 = 1;
+            SetDirtyBit(ref expected, 6);
+            Assert.That(behaviour.SyncVarDirtyBits, Is.EqualTo(expected));
+
+            behaviour.i8 = 1;
+            SetDirtyBit(ref expected, 8);
+            Assert.That(behaviour.SyncVarDirtyBits, Is.EqualTo(expected));
+        }
+        void SetDirtyBit(ref ulong bits, int index)
+        {
+            bits |= 1ul << index;
+        }
+    }
+}
