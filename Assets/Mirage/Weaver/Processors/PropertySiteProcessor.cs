bbe401be81e984cedf1237c2142b76c1438efc38
James Frowen <jamesfrowendev@gmail.com>
2021-09-15 03:57:31 +0100

    removing resolve from PropertySiteProcessor

--- Assets/Mirage/Weaver/Processors/PropertySiteProcessor.cs
+++ Assets/Mirage/Weaver/Processors/PropertySiteProcessor.cs
@@ -33,8 +33,7 @@ namespace Mirage.Weaver
             {
                 if (opField.DeclaringType.IsGenericInstance || opField.DeclaringType.HasGenericParameters) // We're calling to a generic class
                 {
-                    var newField = i.Operand as FieldReference;
-                    var genericType = (GenericInstanceType)newField.DeclaringType;
+                    var genericType = (GenericInstanceType)opField.DeclaringType;
                     i.OpCode = OpCodes.Callvirt;
                     i.Operand = replacement.MakeHostInstanceGeneric(genericType);
                 }
@@ -55,8 +54,7 @@ namespace Mirage.Weaver
             {
                 if (opField.DeclaringType.IsGenericInstance || opField.DeclaringType.HasGenericParameters) // We're calling to a generic class
                 {
-                    var newField = i.Operand as FieldReference;
-                    var genericType = (GenericInstanceType)newField.DeclaringType;
+                    var genericType = (GenericInstanceType)opField.DeclaringType;
                     i.OpCode = OpCodes.Callvirt;
                     i.Operand = replacement.MakeHostInstanceGeneric(genericType);
                 }
@@ -73,39 +71,21 @@ namespace Mirage.Weaver
         {
             if (instr.OpCode == OpCodes.Stfld && instr.Operand is FieldReference opFieldst)
             {
-                FieldReference resolved = opFieldst.Resolve();
-                if (resolved == null)
-                {
-                    resolved = opFieldst.DeclaringType.Resolve().GetField(opFieldst.Name);
-                }
-
                 // this instruction sets the value of a field. cache the field reference.
-                ProcessInstructionSetterField(instr, resolved);
+                ProcessInstructionSetterField(instr, opFieldst);
             }
 
             if (instr.OpCode == OpCodes.Ldfld && instr.Operand is FieldReference opFieldld)
             {
-                FieldReference resolved = opFieldld.Resolve();
-                if (resolved == null)
-                {
-                    resolved = opFieldld.DeclaringType.Resolve().GetField(opFieldld.Name);
-                }
-
                 // this instruction gets the value of a field. cache the field reference.
-                ProcessInstructionGetterField(instr, resolved);
+                ProcessInstructionGetterField(instr, opFieldld);
             }
 
             if (instr.OpCode == OpCodes.Ldflda && instr.Operand is FieldReference opFieldlda)
             {
-                FieldReference resolved = opFieldlda.Resolve();
-                if (resolved == null)
-                {
-                    resolved = opFieldlda.DeclaringType.Resolve().GetField(opFieldlda.Name);
-                }
-
                 // loading a field by reference,  watch out for initobj instruction
                 // see https://github.com/vis2k/Mirror/issues/696
-                ProcessInstructionLoadAddress(md, ref instr, resolved);
+                ProcessInstructionLoadAddress(md, ref instr, opFieldlda);
             }
         }
 
