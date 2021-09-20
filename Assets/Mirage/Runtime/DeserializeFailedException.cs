1848e65a4178241dd97310f17f427c871400c082
James Frowen <jamesfrowendev@gmail.com>
2021-09-20 16:41:29 +0100

    refactor: Removing un-needed constructors

--- Assets/Mirage/Runtime/DeserializeFailedException.cs
+++ Assets/Mirage/Runtime/DeserializeFailedException.cs
@@ -3,21 +3,13 @@ using System.Runtime.Serialization;
 
 namespace Mirage
 {
-
     [Serializable]
     public class DeserializeFailedException : Exception
     {
-        public DeserializeFailedException()
-        {
-        }
-
         public DeserializeFailedException(string message) : base(message)
         {
         }
 
-        public DeserializeFailedException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
         protected DeserializeFailedException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
--- Assets/Tests/Editor/DeserializeFailedExceptionTest.cs
+++ Assets/Tests/Editor/DeserializeFailedExceptionTest.cs
@@ -6,15 +6,6 @@ namespace Mirage.Tests
     public class DeserializeFailedExceptionTest
     {
         [Test]
-        public void InvalidMessageTest()
-        {
-            Assert.Throws<DeserializeFailedException>(() =>
-            {
-                throw new DeserializeFailedException();
-            });
-        }
-
-        [Test]
         public void InvalidMessageWithTextTest()
         {
             DeserializeFailedException ex = Assert.Throws<DeserializeFailedException>(() =>
@@ -23,18 +14,6 @@ namespace Mirage.Tests
             });
 
             Assert.That(ex.Message, Is.EqualTo("Test Message"));
-        }
-
-        [Test]
-        public void InvalidMessageWithTextAndInnerTest()
-        {
-            DeserializeFailedException ex = Assert.Throws<DeserializeFailedException>(() =>
-            {
-                throw new DeserializeFailedException("Test Message Too", new System.Exception());
-            });
-
-            Assert.That(ex.Message, Is.EqualTo("Test Message Too"));
-            Assert.That(ex.InnerException, Is.TypeOf(typeof(System.Exception)));
         }
     }
 }
