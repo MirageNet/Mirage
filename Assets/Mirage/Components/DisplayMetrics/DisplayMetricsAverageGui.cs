454a78df5f0f26353ec7abcb93cc4f29b4ec8b20
James Frowen <jamesfrowendev@gmail.com>
2021-09-23 15:38:41 +0100

    only showing received/sent in display metrics

--- Assets/Mirage/Components/DisplayMetrics/DisplayMetricsAverageGui.cs
+++ Assets/Mirage/Components/DisplayMetrics/DisplayMetricsAverageGui.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 namespace Mirage.DisplayMetrics
 {
+    /// <summary>
+    /// This is an example of how to show metrics, It only shows some of the values inside <see cref="Mirage.SocketLayer.Metrics"/>
+    /// <para>If you want to show more of the values then create a copy of this class and add values to DrawAverage</para>
+    /// </summary>
     public class DisplayMetricsAverageGui : MonoBehaviour
     {
         public Metrics Metrics { get; set; }
@@ -38,17 +42,8 @@ namespace Mirage.DisplayMetrics
             double sendCount = 0;
             double sendBytes = 0;
 
-            double sendUnconnectedCount = 0;
-            double sendUnconnectedBytes = 0;
-
-            double resendCount = 0;
-            double resendBytes = 0;
-
             double receiveCount = 0;
             double receiveBytes = 0;
-
-            double receiveUnconnectedBytes = 0;
-            double receiveUnconnectedCount = 0;
 
             Metrics.Frame[] array = Metrics.buffer;
             int count = 0;
@@ -65,17 +60,8 @@ namespace Mirage.DisplayMetrics
                 sendCount += array[i].sendCount;
                 sendBytes += array[i].sendBytes;
 
-                sendUnconnectedCount += array[i].sendUnconnectedCount;
-                sendUnconnectedBytes += array[i].sendUnconnectedBytes;
-
-                resendCount += array[i].resendCount;
-                resendBytes += array[i].resendBytes;
-
                 receiveCount += array[i].receiveCount;
                 receiveBytes += array[i].receiveBytes;
-
-                receiveUnconnectedBytes += array[i].receiveUnconnectedBytes;
-                receiveUnconnectedCount += array[i].receiveUnconnectedCount;
             }
 
             GUILayout.Label($"connectionCount: {connectionCount / count:0.0}");
@@ -83,18 +69,8 @@ namespace Mirage.DisplayMetrics
             GUILayout.Label($"sendCount: {sendCount / count:0.0}");
             GUILayout.Label($"sendBytes: {sendBytes / count:0.00}");
             GUILayout.Space(8);
-            GUILayout.Label($"sendUnconnectedCount: {sendUnconnectedCount / count:0.0}");
-            GUILayout.Label($"sendUnconnectedBytes: {sendUnconnectedBytes / count:0.00}");
-            GUILayout.Space(8);
-            GUILayout.Label($"resendCount: {resendCount / count:0.0}");
-            GUILayout.Label($"resendBytes: {resendBytes / count:0.00}");
-            GUILayout.Space(8);
             GUILayout.Label($"receiveCount: {receiveCount / count:0.0}");
             GUILayout.Label($"receiveBytes: {receiveBytes / count:0.00}");
-            GUILayout.Space(8);
-            GUILayout.Label($"receiveUnconnectedCount: {receiveUnconnectedCount / count:0.0}");
-            GUILayout.Label($"receiveUnconnectedBytes: {receiveUnconnectedBytes / count:0.00}");
-
         }
     }
 }
