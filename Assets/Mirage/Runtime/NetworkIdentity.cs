8adf83db2cba4c12c75ca39ea1e5d0059cc71bff
James Frowen <jamesfrowendev@gmail.com>
2021-11-01 16:13:09 +0000

    refactor(NetworkIdentity): removing clientAuthorityCallback (#971)
    Leftover static event from Mirror. It is unlikely anyone uses this event. If they do we can always add it back in a more appropriate place
    
    BREAKING CHANGE: NetworkIdentity.clientAuthorityCallback removed

--- Assets/Mirage/Runtime/NetworkIdentity.cs
+++ Assets/Mirage/Runtime/NetworkIdentity.cs
@@ -385,21 +385,6 @@ namespace Mirage
         }
 
         /// <summary>
-        /// The delegate type for the clientAuthorityCallback.
-        /// </summary>
-        /// <param name="player">The network connection that is gaining or losing authority.</param>
-        /// <param name="identity">The object whose client authority status is being changed.</param>
-        /// <param name="authorityState">The new state of client authority of the object for the connection.</param>
-        public delegate void ClientAuthorityCallback(INetworkPlayer player, NetworkIdentity identity, bool authorityState);
-
-        /// <summary>
-        /// A callback that can be populated to be notified when the client-authority state of objects changes.
-        /// <para>Whenever an object is spawned with client authority, or the client authority status of an object is changed with AssignClientAuthority or RemoveClientAuthority, then this callback will be invoked.</para>
-        /// <para>This callback is only invoked on the server.</para>
-        /// </summary>
-        public static event ClientAuthorityCallback clientAuthorityCallback;
-
-        /// <summary>
         /// this is used when a connection is destroyed, since the "observers" property is read-only
         /// </summary>
         /// <param name="player"></param>
@@ -1139,8 +1124,6 @@ namespace Mirage
             // The client will match to the existing object
             // update all variables and assign authority
             ServerObjectManager.SendSpawnMessage(this, player);
-
-            clientAuthorityCallback?.Invoke(player, this, true);
         }
 
         /// <summary>
@@ -1162,8 +1145,6 @@ namespace Mirage
 
             if (Owner != null)
             {
-                clientAuthorityCallback?.Invoke(Owner, this, false);
-
                 INetworkPlayer previousOwner = Owner;
 
                 Owner = null;
