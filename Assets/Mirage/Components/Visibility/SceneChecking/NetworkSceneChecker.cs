2c228a757ed576731807911eccc65159b9155bf5
James Frowen <jamesfrowendev@gmail.com>
2021-11-02 00:20:52 +0000

    Update NetworkSceneChecker.cs

--- Assets/Mirage/Components/Visibility/SceneChecking/NetworkSceneChecker.cs
+++ Assets/Mirage/Components/Visibility/SceneChecking/NetworkSceneChecker.cs
@@ -14,7 +14,7 @@ namespace Mirage
     [AddComponentMenu("Network/NetworkSceneChecker")]
     [RequireComponent(typeof(NetworkIdentity))]
     [HelpURL("https://miragenet.github.io/Mirage/Articles/Components/NetworkSceneChecker.html")]
-    [System.Obsolete("This checker is inefficient, Use SimpleSceneChecker Instead")]
+    [System.Obsolete("This checker is inefficient, use SimpleSceneChecker instead")]
     public class NetworkSceneChecker : NetworkVisibility
     {
         static readonly ILogger logger = LogFactory.GetLogger(typeof(NetworkSceneChecker));
