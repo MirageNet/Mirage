1dafacff27b5595860d54d244c1801727f752f35
James Frowen <jamesfrowendev@gmail.com>
2021-11-16 02:29:58 +0000

    test: fixing and improving PlayerSceneReadyHostTests
    - using Substitute instead of PipedConnections

--- Assets/Tests/Runtime/Host/PlayerSceneReadyHostTests.cs
+++ Assets/Tests/Runtime/Host/PlayerSceneReadyHostTests.cs
@@ -1,30 +1,64 @@
+using System.Collections.Generic;
+using NSubstitute;
 using NUnit.Framework;
-using static Mirage.Tests.LocalConnections;
 
 namespace Mirage.Tests.Runtime.Host
 {
     public class PlayerSceneReadyHostTests : HostSetup<MockComponent>
     {
         [Test]
-        public void SetClientReadyAndNotReadyTest()
+        public void NetworkPlayerStartsReady()
         {
-            (_, NetworkPlayer connection) = PipedConnections(ClientMessageHandler, ServerMessageHandler);
-            Assert.That(connection.SceneIsReady, Is.True, "Starts in a ready scene");
-
-            sceneManager.SetClientNotReady(connection);
-            Assert.That(connection.SceneIsReady, Is.False);
+            var player = new NetworkPlayer(Substitute.For<SocketLayer.IConnection>());
+            Assert.That(player.SceneIsReady, Is.True);
         }
 
         [Test]
-        public void SetAllClientsNotReadyTest()
+        public void SetClientReadyAndNotReadyTest()
+        {
+            INetworkPlayer player = Substitute.For<INetworkPlayer>();
+            // set ready to true, then check if methods sets it back to falase
+            player.SceneIsReady = true;
+
+            sceneManager.SetClientNotReady(player);
+            Assert.That(player.SceneIsReady, Is.False);
+        }
+
+        [Test]
+        public void SetAllClientsNotReady_SetsAllPlayersInGivenListToNotReady()
+        {
+            var players = new List<INetworkPlayer>();
+            // add first ready client
+            INetworkPlayer first = Substitute.For<INetworkPlayer>();
+            first.SceneIsReady = true;
+            players.Add(first);
+
+            // add second ready client
+            INetworkPlayer second = Substitute.For<INetworkPlayer>();
+            second.SceneIsReady = true;
+            players.Add(second);
+
+            // add second ready client
+            INetworkPlayer thrid = Substitute.For<INetworkPlayer>();
+            thrid.SceneIsReady = true;
+
+            // set all not ready
+            sceneManager.SetAllClientsNotReady(players);
+            Assert.That(first.SceneIsReady, Is.False);
+            Assert.That(second.SceneIsReady, Is.False);
+            Assert.That(thrid.SceneIsReady, Is.True, "should not be changed because it isn't in given list");
+        }
+
+        [Test]
+        public void SetAllClientsNotReady_UsesServerPlayersByDefault()
         {
             // add first ready client
-            (_, NetworkPlayer first) = PipedConnections(ClientMessageHandler, ServerMessageHandler);
+            INetworkPlayer first = Substitute.For<INetworkPlayer>();
             first.SceneIsReady = true;
             server.AddTestPlayer(first);
 
             // add second ready client
-            (_, NetworkPlayer second) = PipedConnections(ClientMessageHandler, ServerMessageHandler);
+            INetworkPlayer second = Substitute.For<INetworkPlayer>();
             second.SceneIsReady = true;
             server.AddTestPlayer(second);
 
