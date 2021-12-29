ead317f7ea35c3faaa538326145ed56bd0a3f438
James Frowen <jamesfrowendev@gmail.com>
2021-12-29 15:28:46 +0000

    fix: fixing log settings that have no namespace (#1014)
    - removing duplicates
    - updating settings from factory
    - making sure classes with no namespace save/load correctly

---
+++ Assets/Mirage/Editor/Logging/LogSettingChecker.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using Mirage.Logging;
+using UnityEngine;
+
+namespace Mirage.EditorScripts.Logging
+{
+    /// <summary>
+    /// Removes duplicates and updates log settings from LogFactory
+    /// </summary>
+    public class LogSettingChecker
+    {
+        readonly LogSettingsSO settings;
+        readonly HashSet<string> duplicateChecker = new HashSet<string>();
+
+        public LogSettingChecker(LogSettingsSO settings)
+        {
+            this.settings = settings;
+        }
+
+        public void Refresh()
+        {
+            duplicateChecker.Clear();
+            RemoveDuplicates();
+            AddNewFromFactory();
+        }
+
+        void RemoveDuplicates()
+        {
+            for (int i = 0; i < settings.LogLevels.Count; i++)
+            {
+                bool added = duplicateChecker.Add(settings.LogLevels[i].FullName);
+                // is duplicate, remove it
+                if (!added)
+                {
+                    settings.LogLevels.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        void AddNewFromFactory()
+        {
+            // try add new types
+            foreach (KeyValuePair<string, ILogger> logger in LogFactory.Loggers)
+            {
+                bool added = duplicateChecker.Add(logger.Key);
+                // is new, add it
+                if (added)
+                {
+                    settings.LogLevels.Add(new LogSettingsSO.LoggerSettings(logger.Key, logger.Value.filterLogType));
+                }
+            }
+        }
+    }
+}
