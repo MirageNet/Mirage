a8b0af6a6fdeb0baa0f5fcdbd6202fff97b19a18
Tufan Meriç Uyguner <56236951+TufanMeric@users.noreply.github.com>
2022-01-18 21:38:08 +0300

    Update INetworkSceneManager.cs
    Updated event comments

--- Assets/Mirage/Runtime/INetworkSceneManager.cs
+++ Assets/Mirage/Runtime/INetworkSceneManager.cs
@@ -13,11 +13,17 @@ namespace Mirage
     }
 
     /// <summary>
-    /// Event fires from <see cref="INetworkSceneManager">INetworkSceneManager</see> when a scene change happens on either Server or Client.
+    /// Event fires from <see cref="INetworkSceneManager">INetworkSceneManager</see> when a scene change finishes on either Server or Client.
     /// <para>Scene - Loaded scene</para>
     /// <para>SceneOperation - Scene change type (Normal, Additive Load, Additive Unload).</para>
     /// </summary>
     [Serializable] public class SceneChangeFinishedEvent : UnityEvent<Scene, SceneOperation> { }
+
+    /// <summary>
+    /// Event fires from <see cref="INetworkSceneManager">INetworkSceneManager</see> when a scene change begins on either Server or Client.
+    /// <para>Scene - Name or path of the scene that's about to be loaded</para>
+    /// <para>SceneOperation - Scene change type (Normal, Additive Load, Additive Unload).</para>
+    /// </summary>
     [Serializable] public class SceneChangeStartedEvent : UnityEvent<string, SceneOperation> { }
 
     [Serializable] public class PlayerSceneChangeEvent : UnityEvent<INetworkPlayer> { }
