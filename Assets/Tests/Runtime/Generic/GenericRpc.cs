2239f0ec8392e6bb49aeda8458dc2c663cbbef16
James Frowen <jamesfrowendev@gmail.com>
2022-02-03 19:24:55 +0000

    more tests

--- Assets/Tests/Runtime/Generic/GenericRpc.cs
+++ Assets/Tests/Runtime/Generic/GenericRpc.cs
@@ -6,108 +6,34 @@ using UnityEngine.TestTools;
 
 namespace Mirage.Tests.Runtime.ClientServer.RpcTests
 {
-    //// note, this can't be a behaviour by itself, it must have a child class in order to be used
-    //public class GenericWithRpc_behaviour<T> : NetworkBehaviour
-    //{
-    //    public event Action<T> clientCalled;
-    //    public event Action<T> serverCalled;
-
-    //    [ClientRpc]
-    //    public void MyRpc(T value)
-    //    {
-    //        clientCalled?.Invoke(value);
-    //    }
-
-
-    //    [ServerRpc(requireAuthority = false)]
-    //    public void MyRpc(T value, INetworkPlayer sender)
-    //    {
-    //        serverCalled?.Invoke(value);
-    //    }
-
-    //    public void MyRpc_Weaver(T value)
-    //    {
-    //        if (base.IsClient)
-    //        {
-    //            MyRpc(default);
-    //        }
-    //        PooledNetworkWriter writer = NetworkWriterPool.GetWriter();
-    //        writer.Write(value);
-    //        ClientRpcSender.Send(this, -1903946991, writer, 0, false);
-    //        writer.Release();
-    //    }
-    //}
-    //public class GenericWithRpc_behaviourInt : GenericWithRpc_behaviour<int> { }
-
-    //public class GenericWithRpc : ClientServerSetup<GenericWithRpc_behaviourInt>
-    //{
-    //    [UnityTest]
-    //    public IEnumerator CanCallServerRpc()
-    //    {
-    //        const int num = 32;
-    //        Action<int> sub = Substitute.For<Action<int>>();
-    //        serverComponent.serverCalled += sub;
-    //        clientComponent.MyRpc(num, default);
-
-    //        yield return null;
-    //        yield return null;
-
-    //        sub.Received(1).Invoke(num);
-    //    }
-
-    //    [UnityTest]
-    //    public IEnumerator CanCallClientRpc()
-    //    {
-    //        const int num = 32;
-    //        Action<int> sub = Substitute.For<Action<int>>();
-    //        clientComponent.clientCalled += sub;
-    //        serverComponent.MyRpc(num);
-
-    //        yield return null;
-    //        yield return null;
-
-    //        sub.Received(1).Invoke(num);
-    //    }
-    //}
-
-
-    // note, this can't be a behaviour by itself, it must have a child class in order to be used
-    public class NotGenericWithRpcBase_behaviour<T> : NetworkBehaviour
-    {
-        [SyncVar] int baseValue;
-
-    }
-    public class NotGenericWithRpc_behaviour<T> : NotGenericWithRpcBase_behaviour<T>
+    public class GenericWithRpc_behaviour<T> : NetworkBehaviour
     {
         public event Action<int> clientCalled;
         public event Action<int> serverCalled;
 
-        [SyncVar] int value;
 
-        //[ServerRpc(requireAuthority = false)]
-        //public void MyRpc2(int value, INetworkPlayer sender)
-        //{
-        //    serverCalled?.Invoke(value);
-        //}
-        //[ClientRpc]
-        //public void MyRpc(int value)
-        //{
-        //    clientCalled?.Invoke(value);
-        //}
+        [ServerRpc(requireAuthority = false)]
+        public void MyRpc2(int value, INetworkPlayer sender)
+        {
+            serverCalled?.Invoke(value);
+        }
+        [ClientRpc]
+        public void MyRpc(int value)
+        {
+            clientCalled?.Invoke(value);
+        }
 
         public int GetInt() => 0;
     }
 
-    public class NotGenericWithRpc_behaviourInt : NotGenericWithRpc_behaviour<int>
+    public class GenericWithRpc_behaviourInt : GenericWithRpc_behaviour<int>
     {
-        [SyncVar] int moreValue;
     }
-    public class NotGenericWithRpc_behaviourObject : NotGenericWithRpc_behaviour<object>
+    public class GenericWithRpc_behaviourObject : GenericWithRpc_behaviour<object>
     {
-        [SyncVar] int moreValue;
     }
 
-    public class NotGenericWithRpcInt : ClientServerSetup<NotGenericWithRpc_behaviourInt>
+    public class GenericWithRpcInt : ClientServerSetup<GenericWithRpc_behaviourInt>
     {
         [Test]
         public void DoesNotError()
@@ -121,7 +47,7 @@ namespace Mirage.Tests.Runtime.ClientServer.RpcTests
             const int num = 32;
             Action<int> sub = Substitute.For<Action<int>>();
             serverComponent.serverCalled += sub;
-            //clientComponent.MyRpc2(num, default);
+            clientComponent.MyRpc2(num, default);
 
             yield return null;
             yield return null;
@@ -135,7 +61,7 @@ namespace Mirage.Tests.Runtime.ClientServer.RpcTests
             const int num = 32;
             Action<int> sub = Substitute.For<Action<int>>();
             clientComponent.clientCalled += sub;
-            //serverComponent.MyRpc(num);
+            serverComponent.MyRpc(num);
 
             yield return null;
             yield return null;
@@ -143,7 +69,7 @@ namespace Mirage.Tests.Runtime.ClientServer.RpcTests
             sub.Received(1).Invoke(num);
         }
     }
-    public class NotGenericWithRpcObject : ClientServerSetup<NotGenericWithRpc_behaviourObject>
+    public class GenericWithRpcObject : ClientServerSetup<GenericWithRpc_behaviourObject>
     {
         [Test]
         public void DoesNotError()
@@ -157,7 +83,7 @@ namespace Mirage.Tests.Runtime.ClientServer.RpcTests
             const int num = 32;
             Action<int> sub = Substitute.For<Action<int>>();
             serverComponent.serverCalled += sub;
-            //clientComponent.MyRpc2(num, default);
+            clientComponent.MyRpc2(num, default);
 
             yield return null;
             yield return null;
@@ -171,12 +97,92 @@ namespace Mirage.Tests.Runtime.ClientServer.RpcTests
             const int num = 32;
             Action<int> sub = Substitute.For<Action<int>>();
             clientComponent.clientCalled += sub;
-            //serverComponent.MyRpc(num);
+            serverComponent.MyRpc(num);
 
             yield return null;
             yield return null;
 
             sub.Received(1).Invoke(num);
+        }
+    }
+}
+
+namespace Mirage.Tests.Runtime.ClientServer.SyncVars
+{
+    // note, this can't be a behaviour by itself, it must have a child class in order to be used
+    public class GenericWithSyncVarBase_behaviour<T> : NetworkBehaviour
+    {
+        [SyncVar] public int baseValue;
+
+    }
+    public class GenericWithSyncVar_behaviour<T> : GenericWithSyncVarBase_behaviour<T>
+    {
+        [SyncVar] public int value;
+
+        public int GetInt() => 0;
+    }
+
+    public class GenericWithSyncVar_behaviourInt : GenericWithSyncVar_behaviour<int>
+    {
+        [SyncVar] public int moreValue;
+    }
+    public class GenericWithSyncVar_behaviourObject : GenericWithSyncVar_behaviour<object>
+    {
+        [SyncVar] public int moreValue;
+    }
+
+    public class GenericWithSyncVarInt : ClientServerSetup<GenericWithSyncVar_behaviourInt>
+    {
+        [Test]
+        public void DoesNotError()
+        {
+            // nothing
+        }
+
+        [UnityTest]
+        public IEnumerator SyncToClient()
+        {
+            const int num1 = 11;
+            const int num2 = 12;
+            const int num3 = 13;
+            Action<int> sub = Substitute.For<Action<int>>();
+            serverComponent.baseValue = num1;
+            serverComponent.value = num2;
+            serverComponent.moreValue = num3;
+
+            yield return null;
+            yield return null;
+
+            Assert.That(clientComponent.baseValue, Is.EqualTo(num1));
+            Assert.That(clientComponent.value, Is.EqualTo(num2));
+            Assert.That(clientComponent.moreValue, Is.EqualTo(num3));
+        }
+    }
+    public class GenericWithSyncVarObject : ClientServerSetup<GenericWithSyncVar_behaviourObject>
+    {
+        [Test]
+        public void DoesNotError()
+        {
+            // nothing
+        }
+
+        [UnityTest]
+        public IEnumerator SyncToClient()
+        {
+            const int num1 = 11;
+            const int num2 = 12;
+            const int num3 = 13;
+            Action<int> sub = Substitute.For<Action<int>>();
+            serverComponent.baseValue = num1;
+            serverComponent.value = num2;
+            serverComponent.moreValue = num3;
+
+            yield return null;
+            yield return null;
+
+            Assert.That(clientComponent.baseValue, Is.EqualTo(num1));
+            Assert.That(clientComponent.value, Is.EqualTo(num2));
+            Assert.That(clientComponent.moreValue, Is.EqualTo(num3));
         }
     }
 }
