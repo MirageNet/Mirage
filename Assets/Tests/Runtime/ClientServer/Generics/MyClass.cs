e6eb3661b27412d00963051b8672d19ab9c40499
James Frowen <jamesfrowendev@gmail.com>
2022-02-07 18:10:40 +0000

    test: fixing test classes being excluded

---
+++ Assets/Tests/Runtime/ClientServer/Generics/MyClass.cs
@@ -0,0 +1,9 @@
+namespace Mirage.Tests.Runtime.ClientServer.Generics
+{
+    // NetworkMessage to tell weaver to make writer
+    [NetworkMessage]
+    public class MyClass
+    {
+        public int Value;
+    }
+}
