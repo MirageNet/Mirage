aaf6daa8d871c452c030a47ddbef76a8e88cb798
James Frowen <jamesfrowendev@gmail.com>
2022-02-16 19:40:35 +0000

    extra tests

--- Assets/Tests/Runtime/ClientServer/Generics/GenericMessage.cs
+++ Assets/Tests/Runtime/ClientServer/Generics/GenericMessage.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Collections;
+using Mirage.Collections;
 using Mirage.Serialization;
+using NSubstitute;
 using NUnit.Framework;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace Mirage.Tests.Runtime.ClientServer.Generics
 {
     public struct MyMessage<T>
     {
-        public int Value;
+        public T Value;
     }
 
     public class GenericMessages : ClientServerSetup<MockComponent>
@@ -46,6 +50,102 @@ namespace Mirage.Tests.Runtime.ClientServer.Generics
             yield return null;
 
             Assert.That(called, Is.EqualTo(1));
+        }
+    }
+    public class GenericSyncList_Behaviour : NetworkBehaviour
+    {
+        public readonly SyncList<MyStruct<int>> myList = new SyncList<MyStruct<int>>();
+
+        public struct MyStruct<T>
+        {
+            public T value;
+        }
+    }
+    public class GenericSyncList : ClientServerSetup<GenericSyncList_Behaviour>
+    {
+        [UnityTest]
+        public IEnumerator SyncsValues()
+        {
+            const int num1 = 32;
+            const int num2 = 48;
+            serverComponent.myList.Add(new GenericSyncList_Behaviour.MyStruct<int> { value = num1 });
+            serverComponent.myList.Add(new GenericSyncList_Behaviour.MyStruct<int> { value = num2 });
+
+            yield return new WaitForSeconds(0.4f);
+
+            Assert.That(clientComponent.myList.Count, Is.EqualTo(2));
+            Assert.That(clientComponent.myList[0], Is.EqualTo(num1));
+            Assert.That(clientComponent.myList[1], Is.EqualTo(num2));
+
+            serverComponent.myList.Remove(new GenericSyncList_Behaviour.MyStruct<int> { value = num1 });
+
+            yield return new WaitForSeconds(0.4f);
+
+            Assert.That(clientComponent.myList.Count, Is.EqualTo(1));
+            Assert.That(clientComponent.myList[1], Is.EqualTo(num2));
+        }
+    }
+
+    public class GenericStructAsArg_Behaviour : NetworkBehaviour
+    {
+        public struct MyStruct<T>
+        {
+            public T value;
+        }
+        public struct MyHolder
+        {
+            public MyStruct<int> inner;
+        }
+
+        public event Action<int> structParam;
+        public event Action<int> holderParam;
+
+        [ClientRpc]
+        public void MyRpc(MyStruct<int> value)
+        {
+            structParam.Invoke(value.value);
+        }
+
+        [ClientRpc]
+        public void MyRpcHolder(MyHolder value)
+        {
+            holderParam.Invoke(value.inner.value);
+        }
+    }
+    public class GenericStructAsArg : ClientServerSetup<GenericStructAsArg_Behaviour>
+    {
+        [UnityTest]
+        public IEnumerator CanCallServerRpc()
+        {
+            const int num = 32;
+            Action<int> sub = Substitute.For<Action<int>>();
+            clientComponent.structParam += sub;
+            clientComponent.MyRpc(new GenericStructAsArg_Behaviour.MyStruct<int> { value = num });
+
+            yield return null;
+            yield return null;
+
+            sub.Received(1).Invoke(num);
+        }
+
+        [UnityTest]
+        public IEnumerator CanCallServerRpcWithHolder()
+        {
+            const int num = 32;
+            Action<int> sub = Substitute.For<Action<int>>();
+            clientComponent.holderParam += sub;
+            clientComponent.MyRpcHolder(new GenericStructAsArg_Behaviour.MyHolder
+            {
+                inner = new GenericStructAsArg_Behaviour.MyStruct<int>
+                {
+                    value = num
+                }
+            });
+
+            yield return null;
+            yield return null;
+
+            sub.Received(1).Invoke(num);
         }
     }
 }
