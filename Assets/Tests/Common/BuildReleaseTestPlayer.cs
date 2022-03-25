b22ba8759efc045310c3c3b191bc1610bfcced8a
James Frowen <jamesfrowendev@gmail.com>
2022-03-25 15:02:18 +0000

    test: adding script to create release build for performance test

---
+++ Assets/Tests/Common/BuildReleaseTestPlayer.cs
@@ -0,0 +1,120 @@
+#if UNITY_EDITOR
+using System;
+using System.IO;
+using System.Reflection;
+using Mirage.Tests.PlayerTests.BuildScripts;
+using UnityEditor;
+using UnityEditor.TestTools;
+using UnityEditor.TestTools.TestRunner.Api;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+[assembly: TestPlayerBuildModifier(typeof(MakeRestsReleaseMode))]
+[assembly: PostBuildCleanup(typeof(MakeRestsReleaseMode))]
+
+namespace Mirage.Tests.PlayerTests.BuildScripts
+{
+    public static class BuildReleaseTestPlayer
+    {
+        // [MenuItem("Build/Build performance tests", priority = 1005)]
+        public static void BuildPerformanceTestsUsingRunner()
+        {
+            BuildTarget target = EditorUserBuildSettings.activeBuildTarget;
+            // use Temp folder so unity cleans it up on close
+            string targetFolder = "./Temp/TestPlayerBuild";
+            string exePath = $"{targetFolder}/mirage.exe";
+
+            // deleta any old build
+            CleanUpBuildFolder(targetFolder);
+
+            TestRunnerApi runner = ScriptableObject.CreateInstance<TestRunnerApi>();
+            MakeRestsReleaseMode.ApplyReleaseMode = true;
+            // Create ExecutionSettings is the same as the settings created by the testrunner gui builds
+
+            var filter = new Filter
+            {
+                testMode = TestMode.PlayMode,
+                testNames = new string[] {
+                    // IMPORTANT add tests here before running
+                    // todo create window so this doesn't require editing code
+                    "Mirage.Tests.Performance.NetworkIdentitySpawningPerformance.SpawnManyObjects"
+                }
+            };
+            var testSettings = new ExecutionSettings
+            {
+                // DONT USE runSynchronously IT BREAKS THE TEST RUNNER
+                overloadTestRunSettings = GetRunSettings(exePath),
+                filters = new Filter[] { filter },
+            };
+            // targetPlatform is internal but is used by TestRunnerApi, so we need to use reflection to set value
+            typeof(ExecutionSettings).GetField("targetPlatform", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(testSettings, target);
+
+            runner.Execute(testSettings);
+        }
+
+        private static void CleanUpBuildFolder(string targetFolder)
+        {
+            // try catch, because it is ok if this fails, but we also want to log the failure
+            try
+            {
+                if (Directory.Exists(targetFolder))
+                {
+                    Directory.Delete(targetFolder, true);
+                }
+                Debug.Log($"Deleted Temp folder {targetFolder}");
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogException(e);
+            }
+        }
+
+        static ITestRunSettings GetRunSettings(string targetPath)
+        {
+            // reflection because that is the only way to create buildOnly only with TestRunnerApi
+            var assembly = Assembly.Load("UnityEditor.TestRunner");
+            string typeName = "UnityEditor.TestTools.TestRunner.PlayerLauncherTestRunSettings";
+            object runSettings = assembly.CreateInstance(typeName);
+            runSettings.GetType().GetProperty("buildOnly").SetValue(runSettings, true);
+            runSettings.GetType().GetProperty("buildOnlyLocationPath").SetValue(runSettings, targetPath);
+
+            return (ITestRunSettings)runSettings;
+        }
+    }
+
+    /// <summary>
+    /// Converts PlayerWithRest to use ReleaseMode instead of debug mode
+    /// <para>use <see cref="MakeRestsReleaseMode.ApplyReleaseMode"/> before build to enable</para>
+    /// </summary>
+    public class MakeRestsReleaseMode : ITestPlayerBuildModifier, IPostBuildCleanup
+    {
+        public static bool ApplyReleaseMode = true;
+
+        public BuildPlayerOptions ModifyOptions(BuildPlayerOptions playerOptions)
+        {
+            if (ApplyReleaseMode)
+            {
+                Debug.Log("Appling MakeRestsReleaseMode");
+
+                BuildOptions options = playerOptions.options;
+                // Do not launch the player after the build completes.
+                options &= ~BuildOptions.AutoRunPlayer;
+                options |= BuildOptions.ShowBuiltPlayer;
+                // Use release mode and dont connect to editor
+                options &= ~BuildOptions.Development;
+                options &= ~BuildOptions.ConnectToHost;
+                options &= ~BuildOptions.WaitForPlayerConnection;
+
+                playerOptions.options = options;
+            }
+            return playerOptions;
+        }
+
+        public void Cleanup()
+        {
+            ApplyReleaseMode = false;
+        }
+    }
+}
+#endif
+
---
+++ Assets/Tests/Common/PlayerWithTestsCallbacks.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using Mirage.Tests.PlayerTests;
+using NUnit.Framework.Interfaces;
+using UnityEngine;
+using UnityEngine.TestRunner;
+
+[assembly: TestRunCallback(typeof(PlayerWithTestsCallbacks))]
+
+namespace Mirage.Tests.PlayerTests
+{
+    /// <summary>
+    /// Saves test results when running outside of editor
+    /// </summary>
+    public class PlayerWithTestsCallbacks : ITestRunCallback
+    {
+        public void RunStarted(ITest testsToRun)
+        {
+            // we only run these callbacks if we are in player
+            if (Application.isEditor) return;
+
+            // set framerate so 60 so player doesn't use all cpu
+            Application.targetFrameRate = 60;
+        }
+
+        public void RunFinished(ITestResult testResults)
+        {
+            if (Application.isEditor) return;
+
+            TryWriteToFile(testResults);
+
+            // quit after tests are finished
+            Debug.Log($"Quiting after test run finished");
+            Application.Quit();
+        }
+
+        private static void TryWriteToFile(ITestResult testResults)
+        {
+            try
+            {
+                string xml = testResults.ToXml(true).OuterXml;
+                string savePath = Path.Combine(Application.persistentDataPath, "PlayerWithTests-results.xml");
+                Debug.Log($"Saving results to {savePath}");
+                File.WriteAllText(savePath, xml);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+
+        public void TestStarted(ITest test) { }
+
+        public void TestFinished(ITestResult result) { }
+    }
+}
