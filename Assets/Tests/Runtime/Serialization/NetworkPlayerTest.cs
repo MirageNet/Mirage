4a47f9f22855e3a478f4dc5034de3bf983bb1394
James Frowen <jamesfrowendev@gmail.com>
2022-03-31 17:29:49 +0100

    test: adding tests for networkplayer event

--- Assets/Tests/Runtime/Serialization/NetworkPlayerTest.cs
+++ Assets/Tests/Runtime/Serialization/NetworkPlayerTest.cs
@@ -6,18 +6,75 @@ using UnityEngine;
 
 namespace Mirage.Tests.Runtime
 {
-    public class NetworkPlayerMessageSendingTest
+    public class NetworkPlayerTestBase
     {
-        private NetworkPlayer player;
-        private SocketLayer.IConnection connection;
+        protected NetworkPlayer player;
+        protected SocketLayer.IConnection connection;
 
         [SetUp]
-        public void SetUp()
+        public virtual void SetUp()
         {
             connection = Substitute.For<SocketLayer.IConnection>();
             player = new NetworkPlayer(connection);
         }
+    }
 
+    public class NetworkPlayerCharactorTest : NetworkPlayerTestBase
+    {
+        [Test]
+        public void EventCalledWhenIdentityChanged()
+        {
+            NetworkIdentity character = new GameObject("EventCalledWhenIdentityChanged").AddComponent<NetworkIdentity>();
+
+            Action<NetworkIdentity> action = Substitute.For<Action<NetworkIdentity>>();
+            player.OnIdentityChanged += action;
+            player.Identity = character;
+
+            action.Received(1).Invoke(character);
+            action.ClearReceivedCalls();
+
+            player.Identity = null;
+            action.Received(1).Invoke(null);
+        }
+
+        [Test]
+        public void EventNotCalledWhenIdentityIsSame()
+        {
+            NetworkIdentity character = new GameObject("EventNotCalledWhenIdentityIsSame").AddComponent<NetworkIdentity>();
+
+            Action<NetworkIdentity> action = Substitute.For<Action<NetworkIdentity>>();
+            player.OnIdentityChanged += action;
+            player.Identity = character;
+            action.ClearReceivedCalls();
+
+            // set to same value
+            player.Identity = character;
+            action.DidNotReceive().Invoke(Arg.Any<NetworkIdentity>());
+        }
+
+        [Test]
+        public void HasCharacterReturnsFalseIfIdentityIsSet()
+        {
+            Debug.Assert(player.Identity == null, "player had an identity, this test is invalid");
+            Assert.That(player.HasCharacter, Is.False);
+        }
+
+        [Test]
+        public void HasCharacterReturnsTrueIfIdentityIsSet()
+        {
+            NetworkIdentity character = new GameObject("HasCharacterReturnsTrueIfIdentityIsSet").AddComponent<NetworkIdentity>();
+
+            player.Identity = character;
+
+            Debug.Assert(player.Identity != null, "player did not have identity, this test is invalid");
+            Assert.That(player.HasCharacter, Is.True);
+
+            GameObject.Destroy(character.gameObject);
+        }
+    }
+
+    public class NetworkPlayerMessageSendingTest : NetworkPlayerTestBase
+    {
         [Test]
         [TestCase(Channel.Reliable)]
         [TestCase(Channel.Unreliable)]
@@ -57,26 +114,6 @@ namespace Mirage.Tests.Runtime
             player.Send(new ArraySegment<byte>(new byte[] { 0, 1, 2 }));
             connection.DidNotReceive().SendReliable(Arg.Any<byte[]>());
             connection.DidNotReceive().SendUnreliable(Arg.Any<byte[]>());
-        }
-
-        [Test]
-        public void HasCharacterReturnsFalseIfIdentityIsSet()
-        {
-            Debug.Assert(player.Identity == null, "player had an identity, this test is invalid");
-            Assert.That(player.HasCharacter, Is.False);
-        }
-
-        [Test]
-        public void HasCharacterReturnsTrueIfIdentityIsSet()
-        {
-            NetworkIdentity character = new GameObject("HasCharacterReturnsTrueIfIdentityIsSet").AddComponent<NetworkIdentity>();
-
-            player.Identity = character;
-
-            Debug.Assert(player.Identity != null, "player did not have identity, this test is invalid");
-            Assert.That(player.HasCharacter, Is.True);
-
-            GameObject.Destroy(character.gameObject);
         }
     }
 }
