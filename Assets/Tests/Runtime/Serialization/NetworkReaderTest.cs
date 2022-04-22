8e9a298d415e8301ff969166b023a4a399214c2b
James Frowen <jamesfrowendev@gmail.com>
2022-04-22 19:04:27 +0100

    test: extra tests for new objectlocator stuff

--- Assets/Tests/Runtime/Serialization/NetworkReaderTest.cs
+++ Assets/Tests/Runtime/Serialization/NetworkReaderTest.cs
@@ -1,5 +1,7 @@
+using System;
 using System.IO;
 using Mirage.Serialization;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Mirage.Tests.Runtime.Serialization
@@ -23,6 +25,51 @@ namespace Mirage.Tests.Runtime.Serialization
                     reader.ReadBytes(bytes, 0, bytes.Length + 1);
                 });
             }
+        }
+
+        [Test]
+        public void GettingReaderSetObjectLocator_WithBytes()
+        {
+            IObjectLocator locator = Substitute.For<IObjectLocator>();
+            byte[] bytes = { 0x00, 0x01 };
+            using (PooledNetworkReader reader = NetworkReaderPool.GetReader(bytes, locator))
+            {
+                Assert.That(reader.ObjectLocator, Is.EqualTo(locator));
+            }
+        }
+
+        [Test]
+        public void GettingReaderSetObjectLocator_WithSegment()
+        {
+            IObjectLocator locator = Substitute.For<IObjectLocator>();
+            byte[] bytes = { 0x00, 0x01 };
+            var segment = new ArraySegment<byte>(bytes);
+            using (PooledNetworkReader reader = NetworkReaderPool.GetReader(segment, locator))
+            {
+                Assert.That(reader.ObjectLocator, Is.EqualTo(locator));
+            }
+        }
+
+        [Test]
+        public void GettingReaderSetObjectLocator_WithBytesAndLength()
+        {
+            IObjectLocator locator = Substitute.For<IObjectLocator>();
+            byte[] bytes = { 0x00, 0x01 };
+            using (PooledNetworkReader reader = NetworkReaderPool.GetReader(bytes, 0, 2, locator))
+            {
+                Assert.That(reader.ObjectLocator, Is.EqualTo(locator));
+            }
+        }
+
+        [Test]
+        public void ThrowsIfNotMirageReader()
+        {
+            var reader = new NetworkReader();
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() =>
+            {
+                _ = reader.ToMirageReader();
+            });
+            Assert.That(exception, Has.Message.EqualTo("Reader is not MirageNetworkReader"));
         }
     }
 }
