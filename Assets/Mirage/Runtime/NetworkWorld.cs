c7046234a5d19660898f527fbfa00e298c6bc8f8
James Frowen <jamesfrowendev@gmail.com>
2022-08-17 16:11:29 +0100

    refactor: adding logs to network world

--- Assets/Mirage/Runtime/NetworkWorld.cs
+++ Assets/Mirage/Runtime/NetworkWorld.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Mirage.Logging;
 using UnityEngine;
 
@@ -48,6 +49,8 @@ namespace Mirage
             if (netId != identity.NetId) throw new ArgumentException("NetworkIdentity did not have matching netId", nameof(identity));
             if (_spawnedObjects.TryGetValue(netId, out var existing) && existing != null) throw new ArgumentException("An Identity with same id already exists in network world", nameof(netId));
 
+            if (logger.LogEnabled()) logger.Log($"Adding [netId={netId}, name={identity.name}] to World");
+
             // dont use add, netId might already exist but have been destroyed
             // this can happen client side. we check for this case in TryGetValue above
             _spawnedObjects[netId] = identity;
@@ -57,21 +60,7 @@ namespace Mirage
         internal void RemoveIdentity(NetworkIdentity identity)
         {
             var netId = identity.NetId;
-            var removed = _spawnedObjects.Remove(netId);
-            // only invoke event if values was successfully removed
-            if (removed)
-                onUnspawn?.Invoke(identity);
-        }
-
-        internal void RemoveDestroyedObjects()
-        {
-            var removalCollection = new List<NetworkIdentity>(SpawnedIdentities);
-
-            foreach (var identity in removalCollection)
-            {
-                if (identity == null)
-                    _spawnedObjects.Remove(identity.NetId);
-            }
+            RemoveInternal(netId, identity);
         }
 
         internal void RemoveIdentity(uint netId)
@@ -79,10 +68,38 @@ namespace Mirage
             if (netId == 0) throw new ArgumentException("id can not be zero", nameof(netId));
 
             _spawnedObjects.TryGetValue(netId, out var identity);
+            RemoveInternal(netId, identity);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void RemoveInternal(uint netId, NetworkIdentity identity)
+        {
             var removed = _spawnedObjects.Remove(netId);
             // only invoke event if values was successfully removed
             if (removed)
+            {
+                if (logger.LogEnabled()) logger.Log($"Removing [netId={netId}, name={identity?.name}] from World");
                 onUnspawn?.Invoke(identity);
+            }
+            else
+            {
+                if (logger.LogEnabled()) logger.Log($"Did not remove [netId={netId}, name={identity?.name}] from World. Maybe it was previosuly removed?");
+            }
+        }
+
+        internal void RemoveDestroyedObjects()
+        {
+            if (logger.LogEnabled()) logger.Log($"Removing destroyed objects");
+            var removalCollection = new List<NetworkIdentity>(SpawnedIdentities);
+
+            foreach (var identity in removalCollection)
+            {
+                if (identity == null)
+                {
+                    if (logger.LogEnabled()) logger.Log($"Removing destroyed object:[netId={identity.NetId}]");
+                    _spawnedObjects.Remove(identity.NetId);
+                }
+            }
         }
 
         internal void ClearSpawnedObjects()
