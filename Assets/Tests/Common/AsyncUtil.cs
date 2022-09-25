8bb138f7bb27bee3c6269a3de2af1b16f4660b46
James Frowen <jamesfrowendev@gmail.com>
2022-09-25 16:57:11 +0100

    test: adding timeout to WaitUntilSpawn and cleaning up AsyncUtil

--- Assets/Tests/Common/AsyncUtil.cs
+++ Assets/Tests/Common/AsyncUtil.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Diagnostics;
-using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 
 namespace Mirage.Tests
@@ -10,10 +7,11 @@ namespace Mirage.Tests
 
     public static class AsyncUtil
     {
-        public static async UniTask<NetworkIdentity> WaitUntilSpawn(NetworkWorld world, uint netId)
+        [AssertionMethod]
+        public static async UniTask<NetworkIdentity> WaitUntilSpawn(NetworkWorld world, uint netId, double timeoutSeconds = 2)
         {
             NetworkIdentity identity = null;
-            await UniTask.WaitUntil(() => world.TryGetIdentity(netId, out identity));
+            await WaitUntilWithTimeout(() => world.TryGetIdentity(netId, out identity), timeoutSeconds);
 
             return identity;
         }
@@ -22,30 +20,6 @@ namespace Mirage.Tests
         public static UniTask WaitUntilWithTimeout(Func<bool> predicate, double seconds = 2)
         {
             return UniTask.WaitUntil(predicate).Timeout(TimeSpan.FromSeconds(seconds));
-        }
-
-        // Unity's nunit does not support async tests
-        // so we do this boilerplate to run our async methods
-        public static IEnumerator RunAsync(Func<Task> block)
-        {
-            var task = block();
-
-            while (!task.IsCompleted) { yield return null; }
-            if (task.IsFaulted) { throw task.Exception; }
-        }
-
-        public static async Task WaitFor(Func<bool> predicate, float timeout = 2f)
-        {
-            var stopWatch = new Stopwatch();
-            stopWatch.Start();
-
-            while (!predicate())
-            {
-                await Task.Delay(1);
-
-                if (stopWatch.ElapsedMilliseconds > timeout * 1000)
-                    throw new TimeoutException("Task did not complete in time");
-            }
         }
     }
 }
