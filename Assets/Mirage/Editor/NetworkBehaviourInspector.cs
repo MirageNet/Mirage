99e9519c59bc73d9f4bbdd02ae105178e09fb5c4
James Frowen <jamesfrowendev@gmail.com>
2022-12-05 14:32:36 +0000

    feat: adding option to exclude NetworkBehaviourInspector
    required so users can make custom inspectors with addons like Odin.
    
    Users would add EXCLUDE_NETWORK_BEHAVIOUR_INSPECTOR to their define list, and then unity should recompile mirage scripts and exclude the inspector

--- Assets/Mirage/Editor/NetworkBehaviourInspector.cs
+++ Assets/Mirage/Editor/NetworkBehaviourInspector.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
@@ -6,25 +5,78 @@ using Mirage.Collections;
 using Mirage.Logging;
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Mirage
 {
+#if !EXCLUDE_NETWORK_BEHAVIOUR_INSPECTOR
     [CustomEditor(typeof(NetworkBehaviour), true)]
     [CanEditMultipleObjects]
     public class NetworkBehaviourInspector : Editor
     {
         private static readonly ILogger logger = LogFactory.GetLogger(typeof(NetworkBehaviourInspector));
+        private NetworkBehaviourInspectorDrawer _drawer;
 
+        private void OnEnable()
+        {
+            if (target == null) { logger.LogWarning("NetworkBehaviourInspector had no target object"); return; }
+
+            // If target's base class is changed from NetworkBehaviour to MonoBehaviour
+            // then Unity temporarily keep using this Inspector causing things to break
+            if (!(target is NetworkBehaviour)) { return; }
+
+            _drawer = new NetworkBehaviourInspectorDrawer(target, serializedObject);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            _drawer.DrawDefaultSyncLists();
+            _drawer.DrawDefaultSyncSettings();
+        }
+    }
+#endif
+
+    /// <summary>
+    /// split from Editor class so that people can use this with custom inspectoer
+    /// </summary>
+    public class NetworkBehaviourInspectorDrawer
+    {
         /// <summary>
         /// List of all visible syncVars in target class
         /// </summary>
-        protected List<string> syncVarNames = new List<string>();
-        private bool syncsAnything;
-        private SyncListDrawer syncListDrawer;
+        private readonly List<string> _syncVarNames = new List<string>();
+        private readonly bool _syncsAnything;
+        private readonly SyncListDrawer _syncListDrawer;
+        private readonly SerializedObject _serializedObject;
 
-        // does this type sync anything? otherwise we don't need to show syncInterval
-        private bool SyncsAnything(Type scriptClass)
+        public NetworkBehaviourInspectorDrawer(Object target, SerializedObject serializedObject)
         {
+            _serializedObject = serializedObject;
+
+            _syncVarNames = new List<string>();
+            foreach (var field in InspectorHelper.GetAllFields(target.GetType(), typeof(NetworkBehaviour)))
+            {
+                if (field.IsSyncVar() && field.IsVisibleField())
+                {
+                    _syncVarNames.Add(field.Name);
+                }
+            }
+
+            _syncListDrawer = new SyncListDrawer(serializedObject.targetObject);
+
+            _syncsAnything = SyncsAnything(target);
+        }
+
+        /// <summary>
+        /// does this type sync anything? otherwise we don't need to show syncInterval
+        /// </summary>
+        /// <param name="scriptClass"></param>
+        /// <returns></returns>
+        public static bool SyncsAnything(Object target)
+        {
+            var scriptClass = target.GetType();
+
             // check for all SyncVar fields, they don't have to be visible
             foreach (var field in InspectorHelper.GetAllFields(scriptClass, typeof(NetworkBehaviour)))
             {
@@ -49,61 +101,27 @@ namespace Mirage
             // Any SyncObject should be added to syncObjects when unity creates an
             // object so we can cheeck length of list so see if sync objects exists
             var syncObjectsField = scriptClass.GetField("syncObjects", BindingFlags.NonPublic | BindingFlags.Instance);
-            var syncObjects = (List<ISyncObject>)syncObjectsField.GetValue(serializedObject.targetObject);
+            var syncObjects = (List<ISyncObject>)syncObjectsField.GetValue(target);
 
             return syncObjects.Count > 0;
-        }
-
-        private void OnEnable()
-        {
-            if (target == null) { logger.LogWarning("NetworkBehaviourInspector had no target object"); return; }
-
-            // If target's base class is changed from NetworkBehaviour to MonoBehaviour
-            // then Unity temporarily keep using this Inspector causing things to break
-            if (!(target is NetworkBehaviour)) { return; }
-
-            var scriptClass = target.GetType();
-
-            syncVarNames = new List<string>();
-            foreach (var field in InspectorHelper.GetAllFields(scriptClass, typeof(NetworkBehaviour)))
-            {
-                if (field.IsSyncVar() && field.IsVisibleField())
-                {
-                    syncVarNames.Add(field.Name);
-                }
-            }
-
-            syncListDrawer = new SyncListDrawer(serializedObject.targetObject);
-
-            syncsAnything = SyncsAnything(scriptClass);
-        }
-
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
-            DrawDefaultSyncLists();
-            DrawDefaultSyncSettings();
         }
 
         /// <summary>
         /// Draws Sync Objects that are IEnumerable
         /// </summary>
-        protected void DrawDefaultSyncLists()
+        public void DrawDefaultSyncLists()
         {
-            // Need this check incase OnEnable returns early
-            if (syncListDrawer == null) { return; }
-
-            syncListDrawer.Draw();
+            _syncListDrawer?.Draw();
         }
 
         /// <summary>
         /// Draws SyncSettings if the NetworkBehaviour has anything to sync
         /// </summary>
-        protected void DrawDefaultSyncSettings()
+        public void DrawDefaultSyncSettings()
         {
             // does it sync anything? then show extra properties
             // (no need to show it if the class only has Cmds/Rpcs and no sync)
-            if (!syncsAnything)
+            if (!_syncsAnything)
             {
                 return;
             }
@@ -111,41 +129,46 @@ namespace Mirage
             EditorGUILayout.Space();
             EditorGUILayout.LabelField("Sync Settings", EditorStyles.boldLabel);
 
-            EditorGUILayout.PropertyField(serializedObject.FindProperty("syncMode"));
-            EditorGUILayout.PropertyField(serializedObject.FindProperty("syncInterval"));
+            EditorGUILayout.PropertyField(_serializedObject.FindProperty("syncMode"));
+            EditorGUILayout.PropertyField(_serializedObject.FindProperty("syncInterval"));
 
             // apply
-            serializedObject.ApplyModifiedProperties();
+            _serializedObject.ApplyModifiedProperties();
         }
     }
+
+    /// <summary>
+    /// Draws sync lists in inspector
+    /// <para>because synclists are not serailzied by unity they will not show up in the default inspector, so we need a custom draw to draw them</para>
+    /// </summary>
     public class SyncListDrawer
     {
-        private readonly UnityEngine.Object targetObject;
-        private readonly List<SyncListField> syncListFields;
+        private readonly Object _targetObject;
+        private readonly List<SyncListField> _syncListFields;
 
-        public SyncListDrawer(UnityEngine.Object targetObject)
+        public SyncListDrawer(Object targetObject)
         {
-            this.targetObject = targetObject;
-            syncListFields = new List<SyncListField>();
+            _targetObject = targetObject;
+            _syncListFields = new List<SyncListField>();
             foreach (var field in InspectorHelper.GetAllFields(targetObject.GetType(), typeof(NetworkBehaviour)))
             {
                 if (field.IsSyncObject() && field.IsVisibleSyncObject())
                 {
-                    syncListFields.Add(new SyncListField(field));
+                    _syncListFields.Add(new SyncListField(field));
                 }
             }
         }
 
         public void Draw()
         {
-            if (syncListFields.Count == 0) { return; }
+            if (_syncListFields.Count == 0) { return; }
 
             EditorGUILayout.Space();
             EditorGUILayout.LabelField("Sync Lists", EditorStyles.boldLabel);
 
-            for (var i = 0; i < syncListFields.Count; i++)
+            for (var i = 0; i < _syncListFields.Count; i++)
             {
-                DrawSyncList(syncListFields[i]);
+                DrawSyncList(_syncListFields[i]);
             }
         }
 
@@ -156,7 +179,7 @@ namespace Mirage
             {
                 using (new EditorGUI.IndentLevelScope())
                 {
-                    var fieldValue = syncListField.field.GetValue(targetObject);
+                    var fieldValue = syncListField.field.GetValue(_targetObject);
                     if (fieldValue is IEnumerable synclist)
                     {
                         var index = 0;
