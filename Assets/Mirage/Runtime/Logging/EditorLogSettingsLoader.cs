25ef86d63c4121e3e338b37c54f085cac1126d05
Hertzole <hampusringstrom@gmail.com>
2022-12-22 16:09:04 +0100

    fix deprecation warning

--- Assets/Mirage/Runtime/Logging/EditorLogSettingsLoader.cs
+++ Assets/Mirage/Runtime/Logging/EditorLogSettingsLoader.cs
@@ -29,7 +29,11 @@ namespace Mirage.Logging
             if (cache != null)
                 return cache;
 
+#if UNITY_2021_1_OR_NEWER
+            cache = Object.FindFirstObjectByType<LogSettingsSO>();
+#else
             cache = Object.FindObjectOfType<LogSettingsSO>();
+#endif
             if (cache != null)
                 return cache;
 
