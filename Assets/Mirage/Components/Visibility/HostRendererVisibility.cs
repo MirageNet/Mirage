64c09bfb532afc29e9956839d7715fb9f3d8016c
James Frowen <jamesfrowendev@gmail.com>
2023-02-09 18:20:31 +0000

    feat: adding component that can be used to disable renderers on host player
    Disables renderers based on if the object is visible to the host player or not

---
+++ Assets/Mirage/Components/Visibility/HostRendererVisibility.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+namespace Mirage
+{
+    /// <summary>
+    /// Disables all Renders on GameObject when the NetworkIdentity is not visible too the host player because of a <see cref="NetworkVisibility"/>
+    /// </summary>
+    [RequireComponent(typeof(NetworkVisibility))]
+    public class HostRendererVisibility : NetworkBehaviour
+    {
+        private NetworkVisibility _visibility;
+        private Renderer[] _renderers;
+
+        private void Awake()
+        {
+            _visibility = GetComponent<NetworkVisibility>();
+            _visibility.OnVisibilityChanged += OnVisibilityChanged;
+            _renderers = GetComponentsInChildren<Renderer>();
+        }
+
+        private void OnDestroy()
+        {
+            if (_visibility != null)
+                _visibility.OnVisibilityChanged -= OnVisibilityChanged;
+        }
+
+        /// <summary>
+        /// Can be used to override default GetComponentsInChildren for renderers. Useful if setting up Renderer after Awake is called
+        /// </summary>
+        /// <param name="renderers"></param>
+        public void SetRenderers(Renderer[] renderers)
+        {
+            _renderers = renderers;
+        }
+
+        private void OnVisibilityChanged(INetworkPlayer player, bool visible)
+        {
+            if (!IsLocalClient)
+                return;
+
+            if (player == Server.LocalPlayer)
+                SetHostVisibility(visible);
+        }
+
+        private void SetHostVisibility(bool visible)
+        {
+            for (var i = 0; i < _renderers.Length; i++)
+                _renderers[i].enabled = visible;
+        }
+    }
+}
