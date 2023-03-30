cf8e6a17088b0b78d7a2693e3afd57bc5f64c084
James Frowen <jamesfrowendev@gmail.com>
2023-03-30 12:29:15 +0100

    test: tests for DeserializeMask

--- Assets/Tests/Runtime/Syncing/SyncVarWithBaseClassTest.cs
+++ Assets/Tests/Runtime/Syncing/SyncVarWithBaseClassTest.cs
@@ -1,3 +1,4 @@
+using Mirage.Serialization;
 using NUnit.Framework;
 
 namespace Mirage.Tests.Runtime.Syncing.SyncVarWithBaseClass
@@ -24,16 +25,26 @@ namespace Mirage.Tests.Runtime.Syncing.SyncVarWithBaseClass
 
     public class SyncVarWithBaseClassTest : TestBase
     {
+        protected readonly NetworkWriter _writer = new NetworkWriter(1300);
+        protected readonly MirageNetworkReader _reader = new MirageNetworkReader();
+        private C behaviour;
+
+        [SetUp]
+        public void Setup()
+        {
+            behaviour = CreateBehaviour<C>();
+        }
         [TearDown]
         public void TearDown()
         {
             TearDownTestObjects();
+            _writer.Reset();
+            _reader.Dispose();
         }
 
         [Test]
         public void SetsCorrectDirtyBit()
         {
-            C behaviour = CreateBehaviour<C>();
             Assert.That(behaviour.SyncVarDirtyBits, Is.Zero, "Should start at zero");
 
             var expected = 0ul;
@@ -58,6 +69,77 @@ namespace Mirage.Tests.Runtime.Syncing.SyncVarWithBaseClass
         private void SetDirtyBit(ref ulong bits, int index)
         {
             bits |= 1ul << index;
+        }
+
+        [Test]
+        public void DeserializeMaskIsSet()
+        {
+            var expected = 0ul;
+            // A
+            SetDirtyBit(ref expected, 0);
+            SetDirtyBit(ref expected, 2);
+            // B
+            SetDirtyBit(ref expected, 5);
+            SetDirtyBit(ref expected, 6);
+            // C
+            SetDirtyBit(ref expected, 7);
+            SetDirtyBit(ref expected, 9);
+
+            // should be fine to set mask rather than single bit
+            behaviour.SetDirtyBit(expected);
+            behaviour.OnSerialize(_writer, false);
+            behaviour.ClearDirtyBits();
+
+            // we expect 10 bit mask + 6 packed ints
+            var expectedLength = 10 + (8 * 6);
+            Assert.That(_writer.BitPosition, Is.EqualTo(expectedLength));
+
+            _reader.Reset(_writer.ToArraySegment());
+
+            Assert.That(behaviour._deserializeMask, Is.Zero);
+            behaviour.OnDeserialize(_reader, false);
+
+            Assert.That(behaviour._deserializeMask, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void DeserializeMaskIsClearedEachTime()
+        {
+            // set it once using first test
+            DeserializeMaskIsSet();
+            _writer.Reset();
+
+            // then set it again, but with different mask
+            var expected = 0ul;
+            // A
+            SetDirtyBit(ref expected, 1);
+            SetDirtyBit(ref expected, 8);
+
+            // should be fine to set mask rather than single bit
+            behaviour.SetDirtyBit(expected);
+            behaviour.OnSerialize(_writer, false);
+            behaviour.ClearDirtyBits();
+
+            _reader.Reset(_writer.ToArraySegment());
+
+            // wont be cleared until OnDeserialize is called
+            Assert.That(behaviour._deserializeMask, Is.Not.Zero.And.Not.EqualTo(expected));
+            behaviour.OnDeserialize(_reader, false);
+
+            Assert.That(behaviour._deserializeMask, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void DeserializeMaskIsntSetForInitial()
+        {
+            behaviour.OnSerialize(_writer, true);
+
+            _reader.Reset(_writer.ToArraySegment());
+
+            Assert.That(behaviour._deserializeMask, Is.Zero);
+            behaviour.OnDeserialize(_reader, true);
+
+            Assert.That(behaviour._deserializeMask, Is.Zero); // still zero
         }
     }
 }
