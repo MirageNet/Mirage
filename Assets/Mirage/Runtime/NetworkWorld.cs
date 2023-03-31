5ad1f837d22f0710574d0b98e3e1f403084f73f7
James Frowen <jamesfrowendev@gmail.com>
2023-03-31 16:03:36 +0100

    feat: adding helper methods for networkWorld

--- Assets/Mirage/Runtime/NetworkWorld.cs
+++ Assets/Mirage/Runtime/NetworkWorld.cs
@@ -132,4 +132,37 @@ namespace Mirage
 
         }
     }
+
+    public static class NetworkWorldExtensions
+    {
+        /// <summary>
+        /// adds an event handler, and invokes it on current objects in world
+        /// </summary>
+        /// <param name="action"></param>
+        public static void AddAndInvokeOnSpawn(this NetworkWorld world, Action<NetworkIdentity> action)
+        {
+            world.onSpawn += action;
+            foreach (var identity in world.SpawnedIdentities)
+            {
+                action.Invoke(identity);
+            }
+        }
+
+        /// <summary>
+        /// adds an event handler, and invokes it on current objects in world
+        /// </summary>
+        /// <param name="action"></param>
+        public static void AddAndInvokeOnAuthorityChanged(this NetworkWorld world, AuthorityChanged action)
+        {
+            world.OnAuthorityChanged += action;
+            foreach (var identity in world.SpawnedIdentities)
+            {
+                if (identity.HasAuthority)
+                {
+                    // owner might be null, but that is fine
+                    action.Invoke(identity, true, identity.Owner);
+                }
+            }
+        }
+    }
 }
