ac037f0f5cc3eee096c89a452bcdc70006ccdd75
James Frowen <jamesfrowendev@gmail.com>
2023-04-27 19:02:20 +0100

    style: moving NetworkWorldExtensions to its own file

---
+++ Assets/Mirage/Runtime/Extensions/NetworkWorldExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Mirage
+{
+    public static class NetworkWorldExtensions
+    {
+        /// <summary>
+        /// adds an event handler, and invokes it on current objects in world
+        /// </summary>
+        /// <param name="action"></param>
+        public static void AddAndInvokeOnSpawn(this NetworkWorld world, Action<NetworkIdentity> action)
+        {
+            world.onSpawn += action;
+            foreach (var identity in world.SpawnedIdentities)
+            {
+                action.Invoke(identity);
+            }
+        }
+
+        /// <summary>
+        /// adds an event handler, and invokes it on current objects in world
+        /// </summary>
+        /// <param name="action"></param>
+        public static void AddAndInvokeOnAuthorityChanged(this NetworkWorld world, AuthorityChanged action)
+        {
+            world.OnAuthorityChanged += action;
+            foreach (var identity in world.SpawnedIdentities)
+            {
+                if (identity.HasAuthority)
+                {
+                    // owner might be null, but that is fine
+                    action.Invoke(identity, true, identity.Owner);
+                }
+            }
+        }
+    }
+}
