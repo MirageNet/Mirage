19e0849f4d56279b7920600f4a945af0e17bfb8e
James Frowen <jamesfrowendev@gmail.com>
2023-05-17 18:13:53 +0100

    feat: adding extra function to use callback instead of async
    need callback to handle arraysegment<byte> because arrays are re-used

--- Assets/Mirage/Runtime/Utils/MessageWaiter.cs
+++ Assets/Mirage/Runtime/Utils/MessageWaiter.cs
@@ -4,7 +4,8 @@ using Cysharp.Threading.Tasks;
 namespace Mirage
 {
     /// <summary>
-    /// Waits for 1 message
+    /// Register handler just for 1 message
+    /// <para>Useful on client when you want too receive a single auth message</para>
     /// </summary>
     public class MessageWaiter<T>
     {
@@ -12,6 +13,7 @@ namespace Mirage
         private INetworkPlayer _sender;
         private T _message;
         private MessageHandler _messageHandler;
+        private MessageDelegateWithPlayer<T> callback;
 
         public MessageWaiter(MessageHandler messageHandler, bool allowUnauthenticated = false)
         {
@@ -26,12 +28,22 @@ namespace Mirage
             _received = true;
 
             _messageHandler.UnregisterHandler<T>();
+            callback?.Invoke(player, message);
         }
 
         public async UniTask<(INetworkPlayer sender, T message)> WaitAsync()
         {
             await UniTask.WaitUntil(() => _received);
             return (_sender, _message);
+        }
+
+        /// <summary>
+        /// Use callback instead of async for methods that uses ArraySegment, because internal buffer will be recylced and data will be load before Async completes
+        /// </summary>
+        /// <param name="callback"></param>
+        public void Callback(MessageDelegateWithPlayer<T> callback)
+        {
+            this.callback = callback;
         }
     }
 }
