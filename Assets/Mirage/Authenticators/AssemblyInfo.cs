b35560496a85974f3fd068d4c48b290082a4bc96
James Frowen <jamesfrowendev@gmail.com>
2023-05-24 18:23:46 +0100

    feat: updating Authenticators

--- Assets/Mirage/Authenticators/AssemblyInfo.cs
+++ Assets/Mirage/Authenticators/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("138.0.0")]
+
+[assembly: InternalsVisibleTo("Mirage.Tests.Runtime")]
--- Assets/Mirage/Authenticators/BasicAuthenticator.cs
+++ Assets/Mirage/Authenticators/BasicAuthenticator.cs
@@ -1,147 +1,40 @@
-using System.Collections;
-using Mirage.Logging;
-using UnityEngine;
+using Mirage.Authentication;
 
 namespace Mirage.Authenticators
 {
-    /// <summary>
-    /// Basic Authenticator that lets the server/host set a "passcode" in order to connect.
-    /// <para>
-    /// This code could be a short string that can be used to host a private game.
-    /// The host would set the code and then give it to their friends allowing them to join.
-    /// </para>
-    /// </summary>
-    [AddComponentMenu("Network/Authenticators/BasicAuthenticator")]
-    public class BasicAuthenticator : NetworkAuthenticator
+    public class BasicAuthenticator : NetworkAuthenticator<BasicAuthenticator.JoinMessage>
     {
-        private static readonly ILogger logger = LogFactory.GetLogger(typeof(BasicAuthenticator));
+        public string ServerCode;
 
-        /// <summary>
-        /// Code given to clients so that they can connect to the server/host
-        /// <para>
-        /// Set this in inspector or at runtime when the server/host starts
-        /// </para>
-        /// </summary>
-        [Header("Custom Properties")]
-        public string serverCode;
-
-
-        /// <summary>
-        /// Use whatever credentials make sense for your game.
-        /// <para>
-        ///     This example uses a code so that only players that know the code can join.
-        /// </para>
-        /// <para>
-        ///     You might want to use an accessToken or passwords. Be aware that the normal connection
-        ///     in Mirage is not encrypted so sending secure information directly is not adviced
-        /// </para>
-        /// </summary>
-
-        [NetworkMessage]
-        private struct AuthRequestMessage
+        // called on server to validate
+        protected override AuthenticationResult Authenticate(JoinMessage message)
         {
-            public string serverCode;
-        }
-
-        [NetworkMessage]
-        private struct AuthResponseMessage
-        {
-            public bool success;
-            public string message;
-        }
-
-
-        #region Server Authenticate
-
-        public override void ServerSetup(NetworkServer server)
-        {
-            // register messsage for Auth when server starts
-            // this will ensure the handlers are ready when client connects (even in host mode)
-
-            server.MessageHandler.RegisterHandler<AuthRequestMessage>(OnAuthRequestMessage);
-        }
-
-        public override void ServerAuthenticate(INetworkPlayer player)
-        {
-            // wait for AuthRequestMessage from client
-        }
-
-        private void OnAuthRequestMessage(INetworkPlayer player, AuthRequestMessage msg)
-        {
-            if (logger.LogEnabled()) logger.LogFormat(LogType.Log, "Authentication Request: {0} {1}", msg.serverCode);
-
-            // check if client send the same code as the one stored in the server
-            if (msg.serverCode == serverCode)
+            if (ServerCode == message.ServerCode)
             {
-                // create and send msg to client so it knows to proceed
-                player.Send(new AuthResponseMessage
-                {
-                    success = true,
-                    message = "Success"
-                });
-
-                ServerAccept(player);
+                return AuthenticationResult.CreateSuccess(this, null);
             }
             else
             {
-                // create and send msg to client so it knows to disconnect
-                var authResponseMessage = new AuthResponseMessage
-                {
-                    success = false,
-                    message = "Invalid code"
-                };
-
-                ServerReject(player);
-
-                player.Send(authResponseMessage);
-
-                // disconnect the client after 1 second so that response message gets delivered
-                StartCoroutine(DelayedDisconnect(player, 1));
+                return AuthenticationResult.CreateFail("Server code invalid", this);
             }
         }
 
-        private IEnumerator DelayedDisconnect(INetworkPlayer player, float waitTime)
+        // called on client to create message to send to server
+        public void SendCode(NetworkClient client, string serverCode = null)
         {
-            yield return new WaitForSeconds(waitTime);
-            player.Disconnect();
-        }
-
-        #endregion
-
-        #region Client Authenticate
-
-        public override void ClientSetup(NetworkClient client)
-        {
-            // register messsage for Auth when client starts
-            // this will ensure the handlers are ready when client connects (even in host mode)
-
-            client.MessageHandler.RegisterHandler<AuthResponseMessage>(OnAuthResponseMessage);
-        }
-
-        public override void ClientAuthenticate(INetworkPlayer player)
-        {
-            // The serverCode should be set on the client before connection to the server.
-            // When the client connects it sends the code and the server checks that it is correct
-            player.Send(new AuthRequestMessage
+            var message = new JoinMessage
             {
-                serverCode = serverCode,
-            });
+                // use the argument or field if null
+                ServerCode = serverCode ?? ServerCode
+            };
+
+            SendAuthentication(client, message);
         }
 
-        private void OnAuthResponseMessage(INetworkPlayer player, AuthResponseMessage msg)
+        [NetworkMessage]
+        public struct JoinMessage
         {
-            if (msg.success)
-            {
-                if (logger.LogEnabled()) logger.LogFormat(LogType.Log, "Authentication Success: {0}", msg.message);
-                ClientAccept(player);
-            }
-            else
-            {
-                logger.LogFormat(LogType.Error, "Authentication Fail: {0}", msg.message);
-                ClientReject(player);
-            }
+            public string ServerCode;
         }
-
-        #endregion
     }
 }
