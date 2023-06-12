29c0b925f58ea2c7de5691fa00c489d1b70663c6
James Frowen <jamesfrowendev@gmail.com>
2023-06-12 22:11:19 +0100

    test: adding rpc test for multiple components on same Identity

--- Assets/Tests/Runtime/ClientServer/ClientServerComponentTests.cs
+++ Assets/Tests/Runtime/ClientServer/ClientServerComponentTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using Cysharp.Threading.Tasks;
 using NSubstitute;
@@ -9,7 +8,7 @@ using Random = UnityEngine.Random;
 
 namespace Mirage.Tests.Runtime.ClientServer
 {
-    public class ClientServerComponentTests : ClientServerSetup<MockRpcComponent>
+    public class ClientServerComponentTests : ClientServerSetup
     {
         [Test]
         public void CheckNotHost()
@@ -18,95 +17,6 @@ namespace Mirage.Tests.Runtime.ClientServer
 
             Assert.That(serverPlayerGO, Is.Not.Null);
             Assert.That(clientPlayerGO, Is.Not.Null);
-        }
-
-        [UnityTest]
-        public IEnumerator ServerRpc()
-        {
-            clientComponent.Server2Args(1, "hello");
-
-            yield return null;
-            yield return null;
-
-            Assert.That(serverComponent.Server2ArgsCalls.Count, Is.EqualTo(1));
-            Assert.That(serverComponent.Server2ArgsCalls[0].arg1, Is.EqualTo(1));
-            Assert.That(serverComponent.Server2ArgsCalls[0].arg2, Is.EqualTo("hello"));
-        }
-
-        [UnityTest]
-        public IEnumerator ServerRpcWithSenderOnClient()
-        {
-            clientComponent.ServerWithSender(1);
-
-            yield return null;
-            yield return null;
-
-            Assert.That(serverComponent.ServerWithSenderCalls.Count, Is.EqualTo(1));
-            Assert.That(serverComponent.ServerWithSenderCalls[0].arg1, Is.EqualTo(1));
-            Assert.That(serverComponent.ServerWithSenderCalls[0].sender, Is.EqualTo(serverPlayer));
-        }
-
-        [Test]
-        public void ServerRpcWithSenderOnServer()
-        {
-            var exception = Assert.Throws<InvalidOperationException>(() =>
-            {
-                serverComponent.ServerWithSender(1);
-            });
-
-            Assert.That(exception, Has.Message.EqualTo("Server RPC can only be called when client is active"));
-        }
-
-        [UnityTest]
-        public IEnumerator ServerRpcWithNetworkIdentity()
-        {
-            clientComponent.ServerWithNI(clientIdentity);
-
-            yield return null;
-            yield return null;
-
-            Assert.That(serverComponent.ServerWithNICalls.Count, Is.EqualTo(1));
-            Assert.That(serverComponent.ServerWithNICalls[0], Is.SameAs(serverIdentity));
-        }
-
-        [UnityTest]
-        public IEnumerator ClientRpc()
-        {
-            serverComponent.Client2Args(1, "hello");
-
-            yield return null;
-            yield return null;
-
-            Assert.That(clientComponent.Client2ArgsCalls.Count, Is.EqualTo(1));
-            Assert.That(clientComponent.Client2ArgsCalls[0].arg1, Is.EqualTo(1));
-            Assert.That(clientComponent.Client2ArgsCalls[0].arg2, Is.EqualTo("hello"));
-        }
-
-        [UnityTest]
-        public IEnumerator ClientConnRpc()
-        {
-            serverComponent.ClientTarget(serverPlayer, 1, "hello");
-
-            yield return null;
-            yield return null;
-
-            Assert.That(clientComponent.ClientTargetCalls.Count, Is.EqualTo(1));
-            Assert.That(clientComponent.ClientTargetCalls[0].player, Is.EqualTo(clientPlayer));
-            Assert.That(clientComponent.ClientTargetCalls[0].arg1, Is.EqualTo(1));
-            Assert.That(clientComponent.ClientTargetCalls[0].arg2, Is.EqualTo("hello"));
-        }
-
-        [UnityTest]
-        public IEnumerator ClientOwnerRpc()
-        {
-            serverComponent.ClientOwner(1, "hello");
-
-            yield return null;
-            yield return null;
-
-            Assert.That(clientComponent.ClientOwnerCalls.Count, Is.EqualTo(1));
-            Assert.That(clientComponent.ClientOwnerCalls[0].arg1, Is.EqualTo(1));
-            Assert.That(clientComponent.ClientOwnerCalls[0].arg2, Is.EqualTo("hello"));
         }
 
         [UnityTest]
---
+++ Assets/Tests/Runtime/ClientServer/ClientServerRPCTestsMultipleComponents.cs
@@ -0,0 +1,177 @@
+using System.Collections;
+using Cysharp.Threading.Tasks;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Mirage.Tests.Runtime.ClientServer.RpcTests
+{
+    public class ClientServerRPCTestsMultipleComponents : ClientServerSetup
+    {
+        private const int COMPONENT_COUNT = 4;
+        private MockRpcComponent[] clientComponent;
+        private MockRpcComponent[] serverComponent;
+
+        protected override void ExtraPrefabSetup(NetworkIdentity prefab)
+        {
+            base.ExtraPrefabSetup(prefab);
+
+            for (var i = 0; i < COMPONENT_COUNT; i++)
+            {
+                prefab.gameObject.AddComponent<MockRpcComponent>();
+            }
+        }
+
+        protected override UniTask LateSetup()
+        {
+            clientComponent = new MockRpcComponent[COMPONENT_COUNT];
+            serverComponent = new MockRpcComponent[COMPONENT_COUNT];
+
+            for (var i = 0; i < COMPONENT_COUNT; i++)
+            {
+                serverComponent[i] = (MockRpcComponent)serverIdentity.NetworkBehaviours[i];
+                clientComponent[i] = (MockRpcComponent)clientIdentity.NetworkBehaviours[i];
+            }
+
+            return base.LateSetup();
+        }
+
+
+        [UnityTest]
+        public IEnumerator ServerRpc([Range(0, 3)] int index)
+        {
+            clientComponent[index].Server2Args(1, "hello");
+
+            yield return null;
+            yield return null;
+
+            for (var i = 0; i < COMPONENT_COUNT; i++)
+            {
+                if (i == index)
+                {
+                    Assert.That(serverComponent[i].Server2ArgsCalls.Count, Is.EqualTo(1), $"index {i}");
+                    Assert.That(serverComponent[i].Server2ArgsCalls[0].arg1, Is.EqualTo(1));
+                    Assert.That(serverComponent[i].Server2ArgsCalls[0].arg2, Is.EqualTo("hello"));
+                }
+                else
+                {
+                    Assert.That(serverComponent[i].Server2ArgsCalls.Count, Is.EqualTo(0), $"index {i}");
+                }
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator ServerRpcWithSenderOnClient([Range(0, 3)] int index)
+        {
+            clientComponent[index].ServerWithSender(1);
+
+            yield return null;
+            yield return null;
+
+            for (var i = 0; i < COMPONENT_COUNT; i++)
+            {
+                if (i == index)
+                {
+                    Assert.That(serverComponent[i].ServerWithSenderCalls.Count, Is.EqualTo(1), $"index {i}");
+                    Assert.That(serverComponent[i].ServerWithSenderCalls[0].arg1, Is.EqualTo(1));
+                    Assert.That(serverComponent[i].ServerWithSenderCalls[0].sender, Is.EqualTo(serverPlayer));
+                }
+                else
+                {
+                    Assert.That(serverComponent[i].ServerWithSenderCalls.Count, Is.EqualTo(0), $"index {i}");
+                }
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator ServerRpcWithNetworkIdentity([Range(0, 3)] int index)
+        {
+            clientComponent[index].ServerWithNI(clientIdentity);
+
+            yield return null;
+            yield return null;
+
+            for (var i = 0; i < COMPONENT_COUNT; i++)
+            {
+                if (i == index)
+                {
+                    Assert.That(serverComponent[i].ServerWithNICalls.Count, Is.EqualTo(1), $"index {i}");
+                    Assert.That(serverComponent[i].ServerWithNICalls[0], Is.SameAs(serverIdentity));
+                }
+                else
+                {
+                    Assert.That(serverComponent[i].ServerWithNICalls.Count, Is.EqualTo(0), $"index {i}");
+                }
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator ClientRpc([Range(0, 3)] int index)
+        {
+            serverComponent[index].Client2Args(1, "hello");
+
+            yield return null;
+            yield return null;
+
+            for (var i = 0; i < COMPONENT_COUNT; i++)
+            {
+                if (i == index)
+                {
+                    Assert.That(clientComponent[i].Client2ArgsCalls.Count, Is.EqualTo(1), $"index {i}");
+                    Assert.That(clientComponent[i].Client2ArgsCalls[0].arg1, Is.EqualTo(1));
+                    Assert.That(clientComponent[i].Client2ArgsCalls[0].arg2, Is.EqualTo("hello"));
+                }
+                else
+                {
+                    Assert.That(clientComponent[i].Client2ArgsCalls.Count, Is.EqualTo(0), $"index {i}");
+                }
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator ClientConnRpc([Range(0, 3)] int index)
+        {
+            serverComponent[index].ClientTarget(serverPlayer, 1, "hello");
+
+            yield return null;
+            yield return null;
+
+            for (var i = 0; i < COMPONENT_COUNT; i++)
+            {
+                if (i == index)
+                {
+                    Assert.That(clientComponent[i].ClientTargetCalls.Count, Is.EqualTo(1), $"index {i}");
+                    Assert.That(clientComponent[i].ClientTargetCalls[0].player, Is.EqualTo(clientPlayer));
+                    Assert.That(clientComponent[i].ClientTargetCalls[0].arg1, Is.EqualTo(1));
+                    Assert.That(clientComponent[i].ClientTargetCalls[0].arg2, Is.EqualTo("hello"));
+                }
+                else
+                {
+                    Assert.That(clientComponent[i].ClientTargetCalls.Count, Is.EqualTo(0), $"index {i}");
+                }
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator ClientOwnerRpc([Range(0, 3)] int index)
+        {
+            serverComponent[index].ClientOwner(1, "hello");
+
+            yield return null;
+            yield return null;
+
+            for (var i = 0; i < COMPONENT_COUNT; i++)
+            {
+                if (i == index)
+                {
+                    Assert.That(clientComponent[i].ClientOwnerCalls.Count, Is.EqualTo(1), $"index {i}");
+                    Assert.That(clientComponent[i].ClientOwnerCalls[0].arg1, Is.EqualTo(1));
+                    Assert.That(clientComponent[i].ClientOwnerCalls[0].arg2, Is.EqualTo("hello"));
+                }
+                else
+                {
+                    Assert.That(clientComponent[i].ClientOwnerCalls.Count, Is.EqualTo(0), $"index {i}");
+                }
+            }
+        }
+    }
+}
