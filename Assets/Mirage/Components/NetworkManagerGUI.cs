8b97f36399a8c1e1165e47011f9334df3bdcf28b
Coburn <SoftwareGuy@users.noreply.github.com>
2023-06-17 00:13:24 +1000

    feat(NetworkManagerGUI): overhaul the debug controls (#1149)
    The current NetworkManagerGUI that is used by Mirage is rather outdated. Buttons are hard to click on if you have a touch screen, the white text blends in with the scene, etc. Overall, it was begging for an update.
    
    This PR introduces an almost-completely refactored NetworkManagerGUI that is using the GUI.Window functionality to give it a nice background and title. In the future, one could turn it into a draggable GUI Window, but for now it's more elegant but keeping things simple.
    
    Empty space will be used in later updates (current connection count, etc).

--- Assets/Mirage/Components/NetworkManagerGUI.cs
+++ Assets/Mirage/Components/NetworkManagerGUI.cs
@@ -1,4 +1,5 @@
 using System;
+using Mirage.SocketLayer;
 using UnityEngine;
 
 namespace Mirage
@@ -32,7 +33,6 @@ namespace Mirage
 
         private void Awake()
         {
-            // Coburn, 2023-02-02: 
             // If automatic configuration of NetworkManager is enabled, then attempt to grab the
             // NetworkManager that this script is attached to.
             if (AutoConfigureNetworkManager)
@@ -57,111 +57,25 @@ namespace Mirage
 
         private void OnGUI()
         {
-            // Coburn, 2023-02-02: Apparently NMGUI can/may lose reference to NetworkManager for reasons unknown
-            // (maybe due to being in and out of DDOL and scene changes?). To prevent a NRE being spewed every OnGUI
-            // update, short-circuit here to prevent log spam.
+            // Depending how the scene is set up, references can be broken. If the NM reference is null/missing, then
+            // short-circuit here to prevent log spam.
             if (NetworkManager == null)
             {
                 return;
             }
 
+            // Draw the window on the screen.
             GUIUtility.ScaleAroundPivot(Vector2.one * Scale, GetPivotFromAnchor(GUIAnchor));
-
-            if (!NetworkManager.Server.Active && !NetworkManager.Client.Active)
-            {
-                StartButtons(GetRectFromAnchor(GUIAnchor, 71));
-            }
-            else
-            {
-                StatusLabels(GetRectFromAnchor(GUIAnchor, 100));
-            }
+            GUI.Window(WINDOW_ID, GetRectFromAnchor(GUIAnchor, WINDOW_HEIGHT), DrawNetworkManagerWindow, "Mirage Networking");
         }
 
-        private void StartButtons(Rect position)
-        {
-            GUILayout.BeginArea(position);
 
-            // Coburn, 2023-02-02, possible TODO: Remove the server buttons for WebGL since it can't be used as server
-            if (Application.platform == RuntimePlatform.WebGLPlayer)
-            {
-                GUILayout.Box("WebGL cannot host");
-            }
-            else
-            {
-                if (GUILayout.Button("Host (Server + Client)"))
-                {
-                    ClickHost();
-                }
-            }
-
-            GUILayout.BeginHorizontal();
-
-            if (GUILayout.Button("Client"))
-            {
-                ClickClient();
-            }
-            NetworkAddress = GUILayout.TextField(NetworkAddress);
-
-            GUILayout.EndHorizontal();
-
-            if (Application.platform == RuntimePlatform.WebGLPlayer)
-            {
-                GUILayout.Box("WebGL cannot be a server");
-            }
-            else
-            {
-                if (GUILayout.Button("Server Only"))
-                {
-                    ClickServerOnly();
-                }
-            }
-
-            GUILayout.EndArea();
-        }
-
-        private void StatusLabels(Rect position)
-        {
-            GUILayout.BeginArea(position);
-
-            if (NetworkManager.Server.Active)
-            {
-                GUILayout.Label("Server: active");
-                GUILayout.Label($"Socket: {NetworkManager.Server.SocketFactory.GetType().Name}");
-
-                if (NetworkManager.Client.IsConnected)
-                {
-                    if (GUILayout.Button("Stop Host"))
-                    {
-                        NetworkManager.Server.Stop();
-                    }
-                }
-                else
-                {
-                    if (GUILayout.Button("Stop Server"))
-                    {
-                        NetworkManager.Server.Stop();
-                    }
-                }
-            }
-
-            if (NetworkManager.Client.IsConnected)
-            {
-                GUILayout.Label($"Client: address={NetworkAddress}");
-
-                if (GUILayout.Button("Stop Client"))
-                {
-                    NetworkManager.Client.Disconnect();
-                }
-            }
-            else if (NetworkManager.Client.Active)
-            {
-                GUILayout.Label($"Connecting to {NetworkAddress}...");
-                //TODO: Implement cancel button when it's possible.
-            }
-
-            GUILayout.EndArea();
-        }
-
+        /// <summary>
+        /// Returns a Rect from the anchored position.
+        /// </summary>
+        /// <param name="anchor">Which part of the screen is this located?</param>
+        /// <param name="height">How tall the Rect will be</param>
+        /// <returns></returns>
         private static Rect GetRectFromAnchor(TextAnchor anchor, int height)
         {
             switch (anchor)
@@ -187,6 +101,11 @@ namespace Mirage
             }
         }
 
+        /// <summary>
+        /// Gets a Pivot from the Anchored position.
+        /// </summary>
+        /// <param name="anchor">Which part of the screen is this located?</param>
+        /// <returns>Vector2 with the pivot X and Y.</returns>
         private static Vector2 GetPivotFromAnchor(TextAnchor anchor)
         {
             switch (anchor)
@@ -212,23 +131,186 @@ namespace Mirage
             }
         }
 
+        /// <summary>
+        /// Simply throws an exception if the NetworkManager reference was null.
+        /// </summary>
+        /// <exception cref="NullReferenceException"></exception>
+        internal void CheckNetworkManagerReference()
+        {
+            if (NetworkManager == null)
+            {
+                throw new NullReferenceException("NetworkManager reference is NULL. Fix it, then try again.");
+            }
+        }
+
+        /// <summary>
+        /// Called when user clicks the Host button. It starts Host (Server + Client) mode.
+        /// </summary>
         private void ClickHost()
         {
+            CheckNetworkManagerReference();
             NetworkManager.Server.StartServer(NetworkManager.Client);
         }
 
+        /// <summary>
+        /// Called when user clicks Start Server button. Starts only the server, no server-mode client.
+        /// </summary>
         private void ClickServerOnly()
         {
+            CheckNetworkManagerReference();
             NetworkManager.Server.StartServer();
         }
 
+        /// <summary>
+        /// Called when the user clicks Connect. It connects to the server.
+        /// </summary>
         private void ClickClient()
         {
+            CheckNetworkManagerReference();
             NetworkManager.Client.Connect(NetworkAddress);
         }
 
+        /// <summary>
+        /// Draws the NetworkManager control window.
+        /// </summary>
+        /// <param name="id">Parameter reference passed from GUI.DrawWindow(int)</param>
+        internal void DrawNetworkManagerWindow(int id)
+        {
+            // If the server is active...
+            if (NetworkManager.Server.Active)
+            {
+                DrawServerControls();
+            }
+            else if (NetworkManager.Client.Active)
+            {
+                // The client is active.
+                DrawClientControls();
+            }
+            else
+            {
+                // Nothing is active.
+                DrawIdleControls();
+            }
+        }
+
+        /// <summary>
+        /// This draws the idle controls. Idle in the sense that
+        /// neither the Server or Client is running.
+        /// </summary>
+        internal void DrawIdleControls()
+        {
+            // Begin the idle controls.
+            GUILayout.BeginVertical(GUILayout.ExpandHeight(true));
+
+            // Server mode controls (not available on WebGL).
+#if !UNITY_WEBGL            
+            GUILayout.Label("Server Mode");
+
+            if (GUILayout.Button("Server Only", GUILayout.Height(WINDOW_BUTTON_HEIGHT)))
+            {
+                ClickServerOnly();
+            }
+
+            if (GUILayout.Button("Host Mode (Server + Client)", GUILayout.Height(WINDOW_BUTTON_HEIGHT)))
+            {
+                ClickHost();
+            }
+#endif
+            // Client mode
+            GUILayout.Label("Client Mode");
+            NetworkAddress = GUILayout.TextField(NetworkAddress);
+
+            if (GUILayout.Button("Connect", GUILayout.Height(WINDOW_BUTTON_HEIGHT)))
+            {
+                ClickClient();
+            }
+
+            // Spacer
+            GUILayout.FlexibleSpace();
+
+            // Versioning
+            GUILayout.Label($"Mirage Networking v{Version.Current}\n" +
+                $"Unity v{Application.unityVersion}");
+            GUILayout.EndVertical();
+        }
+
+        /// <summary>
+        /// Draws controls and labels to give information about
+        /// the server instance that's running.
+        /// </summary>
+        internal void DrawServerControls()
+        {
+            GUILayout.BeginVertical(GUILayout.ExpandHeight(true));
+            GUILayout.Label("Server is active.");
+
+            if (NetworkManager.Client.IsConnected)
+                GUILayout.Label("Host mode is active.");
+
+            if (GUILayout.Button(NetworkManager.Client.IsConnected ? "Stop Host Mode" : "Stop Server", GUILayout.Height(WINDOW_BUTTON_HEIGHT)))
+            {
+                NetworkManager.Server.Stop();
+            }
+
+            // Additional texts.
+            if (NetworkManager.Server.SocketFactory is IHasAddress hasAddress)
+                GUILayout.Label($"Listen Address: {hasAddress.Address}");
+
+            if (NetworkManager.Server.SocketFactory is IHasPort hasPort)
+                GUILayout.Label($"Listen Port: {hasPort.Port}");
+
+            GUILayout.Label($"Backend: {NetworkManager.Server.SocketFactory.GetType().Name}");
+
+            GUILayout.EndVertical();
+        }
+
+        /// <summary>
+        /// Draws controls and labels to give information about the client instance
+        /// that's running.
+        /// </summary>
+        internal void DrawClientControls()
+        {
+            double latency;
+
+            GUILayout.BeginVertical();
+
+            GUILayout.Label("Client mode is active.");
+
+            if (NetworkManager.Client.IsConnected)
+            {
+                latency = NetworkManager.Client.World.Time.Rtt;
+
+                if (GUILayout.Button("Stop Client", GUILayout.Height(WINDOW_BUTTON_HEIGHT)))
+                {
+                    NetworkManager.Client.Disconnect();
+                }
+
+                if (NetworkManager.Client.SocketFactory is IHasAddress hasAddress)
+                    GUILayout.Label($"Server: {hasAddress.Address}");
+
+                if (NetworkManager.Client.SocketFactory is IHasPort hasPort)
+                    GUILayout.Label($"Port: {hasPort.Port}");
+
+                // Round this to an integer as something like 7.0101025 ms looks incredibly odd.
+                // We have to cast down to a float because Unity RoundToInt doesn't allow double -> int
+                GUILayout.Label($"Latency: {Mathf.RoundToInt((float)(latency * 1000))} ms");
+            }
+            else
+            {
+                // No controls, just say we're connecting.
+                GUILayout.Label($"Connecting to '{NetworkAddress}'.");
+            }
+
+            GUILayout.Label($"Backend: {NetworkManager.Client.SocketFactory.GetType().Name}");
+            GUILayout.EndVertical();
+        }
+
+        // Constants.
         private const int WIDTH = 200;
         private const int PADDING_X = 10;
         private const int PADDING_Y = 10;
+
+        private const int WINDOW_ID = 0;
+        private const int WINDOW_HEIGHT = 240;
+        private const int WINDOW_BUTTON_HEIGHT = 30;
     }
 }
