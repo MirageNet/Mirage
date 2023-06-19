0b763cf6209f2570750140795171549ab6781a16
James Frowen <jamesfrowendev@gmail.com>
2023-06-19 22:33:13 +0100

    feat: adding script that will make sure object is always visible
    default visibility will check for character and scene ready. this Components can be used to override that

---
+++ Assets/Mirage/Components/Visibility/AlwaysNetworkVisibility.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Mirage.Visibility
+{
+    /// <summary>
+    /// Always shows an object, even if player is loading scene
+    /// <para>This can be used to make sure that Managers in DontDestoryOnLoad dont get despawned</para>
+    /// </summary>
+    public class AlwaysNetworkVisibility : NetworkVisibility
+    {
+        public override bool OnCheckObserver(INetworkPlayer player)
+        {
+            return true;
+        }
+
+        public override void OnRebuildObservers(HashSet<INetworkPlayer> observers, bool initialize)
+        {
+            observers.UnionWith(Server.Players);
+        }
+    }
+}
