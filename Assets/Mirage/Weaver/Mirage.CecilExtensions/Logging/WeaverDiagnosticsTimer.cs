984a13c0bf31a0bfccd791463389680ffcf740dc
James Frowen <jamesfrowendev@gmail.com>
2023-07-16 16:39:47 +0100

    refactor: moving timer

--- Assets/Mirage/Weaver/Mirage.CecilExtensions/Logging/WeaverDiagnosticsTimer.cs
+++ Assets/Mirage/Weaver/Mirage.CecilExtensions/Logging/WeaverDiagnosticsTimer.cs
@@ -3,9 +3,16 @@ using System.IO;
 using ConditionalAttribute = System.Diagnostics.ConditionalAttribute;
 using Stopwatch = System.Diagnostics.Stopwatch;
 
-namespace Mirage.Weaver
+namespace Mirage.CodeGen
 {
-    internal class WeaverDiagnosticsTimer
+    /// <summary>
+    /// Timer used to see how long different parts of weaver take
+    /// Call Start when starting all code. Use <see cref="Sample"/> and using scope to record part of the code.
+    /// <para>
+    /// WEAVER_DEBUG_TIMER must be added to Compile defines for this class to do anything
+    /// </para>
+    /// </summary>
+    public class WeaverDiagnosticsTimer
     {
         public bool writeToFile;
         private StreamWriter writer;
