bcba3a3839d5fc3fada8311ea1ef5de4c6ca75a8
James Frowen <jamesfrowendev@gmail.com>
2023-07-16 15:18:17 +0100

    refactor: assembly versions

--- Assets/Mirage/Weaver/AssemblyInfo.cs
+++ Assets/Mirage/Weaver/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
 using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("145.1.0")]
 
 [assembly: InternalsVisibleTo("Mirage.Tests")]
--- Assets/Mirage/Weaver/Mirage.CecilExtensions/AssemblyInfo.cs
+++ Assets/Mirage/Weaver/Mirage.CecilExtensions/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.0")] 
+[assembly: AssemblyVersion("145.1.0")]
