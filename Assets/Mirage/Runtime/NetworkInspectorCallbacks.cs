a9f1b81c7dc5bdba412067472bbed2495b4b9e25
James Frowen <jamesfrowendev@gmail.com>
2024-03-25 22:05:05 +0000

    refactor: removing convert code from NetworkInspectorCallbacks

--- Assets/Mirage/Runtime/NetworkInspectorCallbacks.cs
+++ Assets/Mirage/Runtime/NetworkInspectorCallbacks.cs
@@ -26,57 +26,5 @@ namespace Mirage
             Identity.OnStopClient.AddListener(_onStopClient.Invoke);
             Identity.OnStopServer.AddListener(_onStopServer.Invoke);
         }
-
-
-#if UNITY_EDITOR
-        public void Convert()
-        {
-            _onStartServer = GetAndClear<AddLateEventUnity>(nameof(_onStartServer));
-            _onStartClient = GetAndClear<AddLateEventUnity>(nameof(_onStartClient));
-            _onStartLocalPlayer = GetAndClear<AddLateEventUnity>(nameof(_onStartLocalPlayer));
-            _onAuthorityChanged = GetAndClear<BoolAddLateEvent>(nameof(_onAuthorityChanged));
-            _onOwnerChanged = GetAndClear<NetworkPlayerAddLateEvent>(nameof(_onOwnerChanged));
-            _onStopClient = GetAndClear<AddLateEventUnity>(nameof(_onStopClient));
-            _onStopServer = GetAndClear<AddLateEventUnity>(nameof(_onStopServer));
-
-            UnityEditor.EditorUtility.SetDirty(this);
-            UnityEditor.EditorUtility.SetDirty(Identity);
-
-            if (Identity.gameObject.scene.IsValid())
-            {
-                UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(Identity.gameObject.scene);
-            }
-        }
-
-        private T GetAndClear<T>(string field) where T : new()
-        {
-            var type = typeof(NetworkIdentity);
-            var flags = System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic;
-            var info = type.GetField(field, flags);
-            var value = (T)info.GetValue(Identity);
-            info.SetValue(Identity, new T());
-            return value;
-        }
-#endif
     }
 }
-
-#if UNITY_EDITOR
-namespace Mirage.EditorScripts
-{
-    [UnityEditor.CustomEditor(typeof(NetworkInspectorCallbacks))]
-    public class NetworkInspectorCallbacksEditor : UnityEditor.Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
-
-            var target = (NetworkInspectorCallbacks)base.target;
-            if (GUILayout.Button("Move Identity events"))
-            {
-                target.Convert();
-            }
-        }
-    }
-}
-#endif
