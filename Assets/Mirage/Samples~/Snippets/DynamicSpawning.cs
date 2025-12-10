7d93dfc93bd79ba8d86b63470a277ae173bc2645
James Frowen <jamesfrowendev@gmail.com>
2025-12-10 12:07:22 +0000

    docs: improving DynamicSpawning example

--- Assets/Mirage/Samples~/Snippets/DynamicSpawning.cs
+++ Assets/Mirage/Samples~/Snippets/DynamicSpawning.cs
@@ -9,6 +9,10 @@ namespace Mirage.Snippets.Spawning
         public ServerObjectManager ServerObjectManager;
         public ClientObjectManager ClientObjectManager;
 
+        // used to check if a prefabHash is for a pre-spawned object
+        // we use a high bit to avoid collisions with other hashes
+        private const int PRE_SPAWN_HASH_BIT = 1 << 30;
+
         // store handler in field so that you dont need to allocate a new one for each DynamicSpawn call
         private SpawnHandler _handler;
         private List<NetworkIdentity> _preSpawnedObjects = new List<NetworkIdentity>();
@@ -22,8 +26,9 @@ namespace Mirage.Snippets.Spawning
             // send spawn message
             for (var i = 0; i < _preSpawnedObjects.Count; i++)
             {
+                var prefabHash = PRE_SPAWN_HASH_BIT | i;
                 // send index as prefabHash
-                ServerObjectManager.Spawn(_preSpawnedObjects[i], prefabHash: i);
+                ServerObjectManager.Spawn(_preSpawnedObjects[i], prefabHash: prefabHash);
             }
         }
 
@@ -58,18 +63,25 @@ namespace Mirage.Snippets.Spawning
 
         private bool IsPreSpawnedId(int prefabHash)
         {
-            // prefabHash starts with 16 bits of 0, then it an id we are using for spawning
-            // this chance of this happening randomly is very low    
-            // you can do more validation on the hash based on use case
-            return (prefabHash & 0xFFFF) == 0;
+            // check if high bit is set (if this is not set, it is for sure not part of our list)
+            if ((prefabHash & PRE_SPAWN_HASH_BIT) == 0)
+                return false;
+
+            // if high bit is set, then the rest of the bits are the index
+            var index = prefabHash & ~PRE_SPAWN_HASH_BIT;
+
+            // NOTE: prefabHash could still randomly be from prefab at this point, check its against the _preSpawnedObjects count
+            //       it is highly unlikely that a random hash will be small and under _preSpawnedObjects
+            // check if index is in range of our pre-spawned list
+            return index < _preSpawnedObjects.Count;
         }
 
         // finds object based on hash and returns it
         public NetworkIdentity FindPreSpawnedObject(SpawnMessage spawnMessage)
         {
             var prefabHash = spawnMessage.PrefabHash.Value;
-            // we stored index in last 16 bits on hash
-            var index = prefabHash >> 16;
+            // remove high bit to get index
+            var index = prefabHash & ~PRE_SPAWN_HASH_BIT;
 
             var identity = _preSpawnedObjects[index];
             return identity;
