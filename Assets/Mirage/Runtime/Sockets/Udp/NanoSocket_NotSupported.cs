6e6519999c091638bbd90b1cb3473022759021eb
James Frowen <jamesfrowendev@gmail.com>
2026-01-12 15:32:18 +0000

    fix: fixing EXCLUDE_NANOSOCKETS code for new IConnectionHandle

--- Assets/Mirage/Runtime/Sockets/Udp/NanoSocket_NotSupported.cs
+++ Assets/Mirage/Runtime/Sockets/Udp/NanoSocket_NotSupported.cs
@@ -17,17 +17,34 @@ namespace Mirage.Sockets.Udp
         public static bool Supported => false;
 
         public NanoSocket(int bufferSize) => throw new NotSupportedException();
-        public void Bind(IEndPoint endPoint) => throw new NotSupportedException();
-        public void Connect(IEndPoint endPoint) => throw new NotSupportedException();
+        public void Bind(IBindEndPoint endPoint) => throw new NotSupportedException();
+        public IConnectionHandle Connect(IConnectEndPoint endPoint) => throw new NotSupportedException();
         public void Close() => throw new NotSupportedException();
         public bool Poll() => throw new NotSupportedException();
-        public int Receive(byte[] buffer, out IEndPoint endPoint) => throw new NotSupportedException();
-        public void Send(IEndPoint endPoint, byte[] packet, int length) => throw new NotSupportedException();
+        public int Receive(Span<byte> outBuffer, out IConnectionHandle handle) => throw new NotSupportedException();
+        public void Send(IConnectionHandle handle, ReadOnlySpan<byte> span) => throw new NotSupportedException();
+        void ISocket.Tick() => throw new NotSupportedException();
+        void ISocket.Flush() => throw new NotSupportedException();
+        void ISocket.SetTickEvents(int maxPacketSize, OnData onData, OnDisconnect onDisconnect) => throw new NotSupportedException();
+
     }
-    public sealed class NanoEndPoint : IEndPoint
+    public sealed class NanoConnectionHandle : IConnectionHandle, IBindEndPoint, IConnectEndPoint, IEquatable<NanoConnectionHandle>
     {
-        public NanoEndPoint(string host, ushort port) => throw new NotSupportedException();
-        public IEndPoint CreateCopy() => throw new NotSupportedException();
+        public NanoConnectionHandle(string host, ushort port) => throw new NotSupportedException();
+
+        bool IConnectionHandle.IsStateful => throw new NotSupportedException();
+        bool IConnectionHandle.SupportsGracefulDisconnect => throw new NotSupportedException();
+        void IConnectionHandle.Disconnect(string gracefulDisconnectReason) => throw new NotSupportedException();
+        ISocketLayerConnection IConnectionHandle.SocketLayerConnection
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+        public IConnectionHandle CreateCopy() => throw new NotSupportedException();
+        public bool Equals(NanoConnectionHandle other) => throw new NotSupportedException();
+        public override bool Equals(object obj) => throw new NotSupportedException();
+        public override int GetHashCode() => throw new NotSupportedException();
+        public override string ToString() => throw new NotSupportedException();
     }
 }
 #endif
