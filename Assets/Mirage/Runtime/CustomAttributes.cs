9a0eec2b41d0da027a6a39033200cd6350e9396a
James Frowen <jamesfrowendev@gmail.com>
2026-05-30 21:52:32 +0100

    feat: restrict SyncVarAttribute usage to properties

--- Assets/Mirage/Runtime/CustomAttributes.cs
+++ Assets/Mirage/Runtime/CustomAttributes.cs
@@ -7,7 +7,7 @@ namespace Mirage
     /// SyncVars are used to synchronize a variable from the server to all clients automatically.
     /// <para>Value must be changed on server, not directly by clients.  Hook parameter allows you to define a client-side method to be invoked when the client gets an update from the server.</para>
     /// </summary>
-    [AttributeUsage(AttributeTargets.Field)]
+    [AttributeUsage(AttributeTargets.Property)]
     public class SyncVarAttribute : PropertyAttribute
     {
         ///<summary>A function that should be called on the client when the value changes.</summary>
